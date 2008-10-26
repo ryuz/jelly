ba27851b398d84f89ae3a1cc707ecd1b35bd0a32
ryuji <ryuji@760bbadc-7816-4ce1-9c2b-5e91787a9cc6>
2008-10-26 01:37:07 +0000

    git-svn-id: http://10.72.141.11:8000/svn/jelly/trunk@28 760bbadc-7816-4ce1-9c2b-5e91787a9cc6

--- soft/gdb-stub/string.h
+++
@@ -1,9 +0,0 @@
-#ifndef __STRING_H
-#define __STRING_H
-
-extern void *memcpy (void *__dest, const void *__src, unsigned int __n);
-extern char *strcpy (char *dest, __const char *__src);
-extern int strlen (__const char *__s);
-extern void * memset(void *__s, int __c, unsigned int __n);
-
-#endif /* __STRING_H */
