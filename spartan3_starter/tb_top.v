1c7839d8b7d0177cf6288b5b7843e9f4a24b5998
ryuji <ryuji@760bbadc-7816-4ce1-9c2b-5e91787a9cc6>
2008-10-28 00:14:39 +0000

    git-svn-id: http://10.72.141.11:8000/svn/jelly/trunk@33 760bbadc-7816-4ce1-9c2b-5e91787a9cc6

--- spartan3_starter/tb_top.v
+++ spartan3_starter/tb_top.v
@@ -7,7 +7,7 @@ module tb_top;
 	
 	
 	initial begin
-		$dumpfile("tb_plasma.vcd");
+		$dumpfile("tb_top.vcd");
 		$dumpvars(0, tb_top);
 	end
 	
@@ -28,9 +28,8 @@ module tb_top;
 	end
 	
 	
-	
 	reg			uart_rx;
-
+	
 	wire				sram_ce0_n;
 	wire				sram_ce1_n;
 	wire				sram_we_n;
@@ -53,9 +52,13 @@ module tb_top;
 			.asram_oe_n		(sram_oe_n),
 			.asram_bls_n	(sram_bls_n),
 			.asram_a		(sram_a),
-			.asram_d		(sram_d)
+			.asram_d		(sram_d),
+			
+			.led			(),
+			.ext			()
 		);
 	
+	// SRAM
 	sram i_sram0 (.ce_n(sram_ce0_n), .we_n(sram_we_n | sram_bls_n[0]), .oe_n(sram_oe_n), .addr(sram_a), .data(sram_d[7:0]));
 	sram i_sram1 (.ce_n(sram_ce0_n), .we_n(sram_we_n | sram_bls_n[1]), .oe_n(sram_oe_n), .addr(sram_a), .data(sram_d[15:8]));
 	sram i_sram2 (.ce_n(sram_ce1_n), .we_n(sram_we_n | sram_bls_n[2]), .oe_n(sram_oe_n), .addr(sram_a), .data(sram_d[23:16]));
@@ -63,6 +66,37 @@ module tb_top;
 	
 	
 	
+	// PC trace
+	integer pc_trace;
+	initial begin
+		pc_trace = $fopen("pc_trace.txt");
+	end
+	always @ ( posedge i_top.i_cpu_top.i_cpu_core.clk ) begin
+		if ( !i_top.i_cpu_top.i_cpu_core.interlock & !i_top.i_cpu_top.i_cpu_core.ex_out_stall ) begin
+			$fdisplay(pc_trace, "%t : %h %h",
+						$time, i_top.i_cpu_top.i_cpu_core.ex_out_pc, i_top.i_cpu_top.i_cpu_core.ex_out_instruction);
+		end
+	end
+
+	
+	// UART monitor
+	always @ ( posedge i_top.i_uart0.clk ) begin
+		if ( i_top.i_uart0.tx_fifo_wr_en ) begin
+			$display("%t UART-TX:%h %c", $time, i_top.i_uart0.tx_fifo_wr_data, i_top.i_uart0.tx_fifo_wr_data);
+		end
+	end
+
+	// Interrupt monitor
+	always @ ( posedge i_top.i_cpu_top.clk ) begin
+		if ( i_top.i_cpu_top.interrupt_req ) begin
+			$display("%t  interrupt_req",  $time);
+		end
+		if ( i_top.i_cpu_top.interrupt_ack ) begin
+			$display("%t  interrupt_ack",  $time);
+		end
+	end
+	
+
 	initial begin
 					uart_rx = 1'b1;
 		#UART_RATE 	uart_rx = 1'b1;
@@ -129,38 +163,9 @@ module tb_top;
 		#UART_RATE 	uart_rx = 1'b1;		// 4
 		#UART_RATE 	uart_rx = 1'b0;		// 5
 		#UART_RATE 	uart_rx = 1'b1;		// 6
-		#UART_RATE 	uart_rx = 1'b0;		// 7 : MSB		
+		#UART_RATE 	uart_rx = 1'b0;		// 7 : MSB
 		#UART_RATE 	uart_rx = 1'b1;		// stop
 	end
 	
-	// PC trace
-	integer	pc_trace;
-	initial begin
-		pc_trace = $fopen("pc_trace.txt");
-	end
-	always @ ( posedge i_top.i_cpu_top.i_cpu_core.clk ) begin
-		if ( !i_top.i_cpu_top.i_cpu_core.ex_out_stall ) begin
-			$fdisplay(pc_trace, "%t %h", $time, i_top.i_cpu_top.i_cpu_core.ex_out_pc);
-		end
-	end
-	
-	
-	// UART monitor
-	always @ ( posedge i_top.i_uart0.clk ) begin
-		if ( i_top.i_uart0.tx_fifo_wr_en ) begin
-			$display("%t UART-TX:%h %c", $time, i_top.i_uart0.tx_fifo_wr_data, i_top.i_uart0.tx_fifo_wr_data);
-		end
-	end
-
-	// Interrupt monitor
-	always @ ( posedge i_top.i_cpu_top.clk ) begin
-		if ( i_top.i_cpu_top.interrupt_req ) begin
-			$display("%t  interrupt_req",  $time);
-		end
-		if ( i_top.i_cpu_top.interrupt_ack ) begin
-			$display("%t  interrupt_ack",  $time);
-		end
-	end
-		
 endmodule
 
