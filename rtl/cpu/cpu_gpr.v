cf5fc52e45a360db27ce79f9a0eb7042e7b1784c
ryuji <ryuji@760bbadc-7816-4ce1-9c2b-5e91787a9cc6>
2008-10-30 22:40:44 +0000

    git-svn-id: http://10.72.141.11:8000/svn/jelly/trunk@47 760bbadc-7816-4ce1-9c2b-5e91787a9cc6

--- rtl/cpu/cpu_gpr.v
+++ rtl/cpu/cpu_gpr.v
@@ -18,8 +18,11 @@ module cpu_gpr
 				r0_en, r0_addr, r0_data,
 				r1_en, r1_addr, r1_data
 			);
+	
+	parameter	TYPE       = 0;		// 0: LUT, 1: double dp-ram, 2: double-speed dp-ram
 	parameter	DATA_WIDTH = 32;
 	parameter	ADDR_WIDTH = 5;
+	localparam	REG_SIZE   = (1 << ADDR_WIDTH);
 	
 	input						reset;
 	input						clk;
@@ -30,7 +33,7 @@ module cpu_gpr
 	input						w0_en;
 	input	[ADDR_WIDTH-1:0]	w0_addr;
 	input	[DATA_WIDTH-1:0]	w0_data;
-	
+
 	input						w1_en;
 	input	[ADDR_WIDTH-1:0]	w1_addr;
 	input	[DATA_WIDTH-1:0]	w1_data;
@@ -42,6 +45,7 @@ module cpu_gpr
 	input						r1_en;
 	input	[ADDR_WIDTH-1:0]	r1_addr;
 	output	[DATA_WIDTH-1:0]	r1_data;
+	
 	
 	
 	reg		clk_dly;
@@ -56,95 +60,210 @@ module cpu_gpr
 	end
 	
 	
-	// dualport ram
-	wire						ram_en0;
-	wire						ram_we0;
-	wire	[ADDR_WIDTH-1:0]	ram_addr0;
-	wire	[DATA_WIDTH-1:0]	ram_din0;
-	wire	[DATA_WIDTH-1:0]	ram_dout0;
-	wire						ram_en1;
-	wire						ram_we1;
-	wire	[ADDR_WIDTH-1:0]	ram_addr1;
-	wire	[DATA_WIDTH-1:0]	ram_din1;
-	wire	[DATA_WIDTH-1:0]	ram_dout1;
-	
-	ram_dualport_xilinx
-			#(
-				.DATA_WIDTH		(DATA_WIDTH),
-				.ADDR_WIDTH		(ADDR_WIDTH),
-				.MEM_SIZE		((1 << (ADDR_WIDTH)))
-			)
-		i_ram_dualport
-			(
-				.clk0			(clk_x2),
-				.en0			(ram_en0),
-				.we0			(ram_we0),
-				.addr0			(ram_addr0),
-				.din0			(ram_din0),
-				.dout0			(ram_dout0),
-				
-				.clk1			(clk_x2),
-				.en1			(ram_en1),
-				.we1			(ram_we1),
-				.addr1			(ram_addr1),
-				.din1			(ram_din1),
-				.dout1			(ram_dout1)
-			);
-	
-	assign ram_en0   = (phase == 1'b0) ? r0_en   : w0_en;
-	assign ram_we0   = (phase == 1'b0) ? 1'b0    : 1'b1;
-	assign ram_addr0 = (phase == 1'b0) ? r0_addr : w0_addr;
-	assign ram_din0  = w0_data;
-
-	assign ram_en1   = (phase == 1'b0) ? r1_en   : w1_en;
-	assign ram_we1   = (phase == 1'b0) ? 1'b0    : 1'b1;
-	assign ram_addr1 = (phase == 1'b0) ? r1_addr : w1_addr;
-	assign ram_din1  = w1_data;
-	
-	
-	reg		[DATA_WIDTH-1:0]	r0_data;	
-	always @ ( posedge clk or posedge reset ) begin
-		if ( reset ) begin
-			r0_data <= 0;
+	generate
+	if ( TYPE == 1 ) begin
+		// ------------------------
+		//  Dual DP-RAM
+		// ------------------------
+		
+		genvar i;
+		for ( i = 0; i < 2; i = i + 1 ) begin :dpram
+			// dualport ram
+			wire						ram_en0;
+			wire						ram_we0;
+			wire	[ADDR_WIDTH-1:0]	ram_addr0;
+			wire	[DATA_WIDTH-1:0]	ram_din0;
+			wire	[DATA_WIDTH-1:0]	ram_dout0;
+			wire						ram_en1;
+			wire						ram_we1;
+			wire	[ADDR_WIDTH-1:0]	ram_addr1;
+			wire	[DATA_WIDTH-1:0]	ram_din1;
+			wire	[DATA_WIDTH-1:0]	ram_dout1;
+			
+			ram_dualport
+					#(
+						.DATA_WIDTH		(DATA_WIDTH),
+						.ADDR_WIDTH		(ADDR_WIDTH),
+						.MEM_SIZE		((1 << (ADDR_WIDTH)))
+					)
+				i_ram_dualport
+					(
+						.clk0			(clk),
+						.en0			(ram_en0),
+						.we0			(ram_we0),
+						.addr0			(ram_addr0),
+						.din0			(ram_din0),
+						.dout0			(ram_dout0),
+						
+						.clk1			(clk),
+						.en1			(ram_en1),
+						.we1			(ram_we1),
+						.addr1			(ram_addr1),
+						.din1			(ram_din1),
+						.dout1			(ram_dout1)
+					);
+			
+			// write
+			assign ram_en0  = w0_en;
+			assign ram_we0  = 1'b1;
+			assign ram_addr = w0_addr;
+			assign ram_din0 = w0_data;
+			
+			// read
+			reg							through;
+			reg		[DATA_WIDTH-1:0]	write_data;
+			wire	[DATA_WIDTH-1:0]	read_data;
+			always @ ( posedge clk or posedge reset ) begin
+				through    <= ram_en0 & ram_we0 & (ram_addr0 == ram_addr1);
+				write_data <= ram_din0;
+			end
+			assign read_data = through ? write_data : ram_dout1;
+			
+			if ( i == 0 ) begin
+				assign ram_en1   = r0_en;
+				assign ram_we1   = 1'b0;
+				assign ram_addr1 = r0_addr;
+				assign r0_data   = read_data;
+			end
+			else begin
+				assign ram_en1   = r1_en;
+				assign ram_we1   = 1'b0;
+				assign ram_addr1 = r1_addr;
+				assign r1_data   = read_data;
+			end
 		end
-		else begin
+	end
+	else if ( TYPE == 2 ) begin
+		// ------------------------
+		//  x2 clock DP-RAM
+		// ------------------------
+		
+		// write port1
+/*		wire						w1_en;
+		wire	[ADDR_WIDTH-1:0]	w1_addr;
+		wire	[DATA_WIDTH-1:0]	w1_data;
+		assign w1_en   = 1'b0;
+		assign w1_addr = {ADDR_WIDTH{1'b0}};
+		assign w1_data = {DATA_WIDTH{1'b0}};
+*/		
+		// dualport ram
+		wire						ram_en0;
+		wire						ram_we0;
+		wire	[ADDR_WIDTH-1:0]	ram_addr0;
+		wire	[DATA_WIDTH-1:0]	ram_din0;
+		wire	[DATA_WIDTH-1:0]	ram_dout0;
+		wire						ram_en1;
+		wire						ram_we1;
+		wire	[ADDR_WIDTH-1:0]	ram_addr1;
+		wire	[DATA_WIDTH-1:0]	ram_din1;
+		wire	[DATA_WIDTH-1:0]	ram_dout1;
+		
+		ram_dualport
+				#(
+					.DATA_WIDTH		(DATA_WIDTH),
+					.ADDR_WIDTH		(ADDR_WIDTH),
+					.MEM_SIZE		((1 << (ADDR_WIDTH)))
+				)
+			i_ram_dualport
+				(
+					.clk0			(clk_x2),
+					.en0			(ram_en0),
+					.we0			(ram_we0),
+					.addr0			(ram_addr0),
+					.din0			(ram_din0),
+					.dout0			(ram_dout0),
+					
+					.clk1			(clk_x2),
+					.en1			(ram_en1),
+					.we1			(ram_we1),
+					.addr1			(ram_addr1),
+					.din1			(ram_din1),
+					.dout1			(ram_dout1)
+				);
+		
+		assign ram_en0   = (phase == 1'b0) ? r0_en   : w0_en;
+		assign ram_we0   = (phase == 1'b0) ? 1'b0    : 1'b1;
+		assign ram_addr0 = (phase == 1'b0) ? r0_addr : w0_addr;
+		assign ram_din0  = w0_data;
+		
+		assign ram_en1   = (phase == 1'b0) ? r1_en   : w1_en;
+		assign ram_we1   = (phase == 1'b0) ? 1'b0    : 1'b1;
+		assign ram_addr1 = (phase == 1'b0) ? r1_addr : w1_addr;
+		assign ram_din1  = w1_data;
+		
+		
+		reg		[DATA_WIDTH-1:0]	r0_data;	
+		always @ ( posedge clk or posedge reset ) begin
+			if ( reset ) begin
+				r0_data <= 0;
+			end
+			else begin
+				if ( !interlock ) begin
+					if ( r0_en ) begin
+						if ( w0_en & (r0_addr == w0_addr) ) begin
+							r0_data <= w0_data;
+						end
+						else if ( w1_en & (r0_addr == w1_addr) ) begin
+							r0_data <= w1_data;
+						end
+						else begin
+							r0_data <= ram_dout0;
+						end
+					end
+				end
+			end
+		end
+		
+		reg		[DATA_WIDTH-1:0]	r1_data;
+		always @ ( posedge clk or posedge reset ) begin
+			if ( reset ) begin
+				r1_data <= 0;
+			end
+			else begin
+				if ( !interlock ) begin
+					if ( r1_en ) begin
+						if ( w0_en & (r1_addr == w0_addr) ) begin
+							r1_data <= w0_data;
+						end
+						else if ( w1_en & (r1_addr == w1_addr) ) begin
+							r1_data <= w1_data;
+						end
+						else begin
+							r1_data <= ram_dout1;
+						end
+					end
+				end
+			end
+		end
+	end
+	else begin
+		// ------------------------
+		//  LUT
+		// ------------------------
+		reg		[DATA_WIDTH-1:0]	reg_gpr		[0:REG_SIZE-1];
+		reg		[DATA_WIDTH-1:0]	reg_read0;
+		reg		[DATA_WIDTH-1:0]	reg_read1;
+		
+		always @ ( posedge clk ) begin
+			if ( w0_en ) begin
+				reg_gpr[w0_addr] = w0_data;
+			end
+			
 			if ( !interlock ) begin
 				if ( r0_en ) begin
-					if ( w0_en & (r0_addr == w0_addr) ) begin
-						r0_data <= w0_data;
-					end
-					else if ( w1_en & (r0_addr == w1_addr) ) begin
-						r0_data <= w1_data;
-					end
-					else begin
-						r0_data <= ram_dout0;
-					end
+					reg_read0 = reg_gpr[r0_addr];
 				end
-			end
-		end
-	end
-	
-	reg		[DATA_WIDTH-1:0]	r1_data;
-	always @ ( posedge clk or posedge reset ) begin
-		if ( reset ) begin
-			r1_data <= 0;
-		end
-		else begin
-			if ( !interlock ) begin
+				
 				if ( r1_en ) begin
-					if ( w0_en & (r1_addr == w0_addr) ) begin
-						r1_data <= w0_data;
-					end
-					else if ( w1_en & (r1_addr == w1_addr) ) begin
-						r1_data <= w1_data;
-					end
-					else begin
-						r1_data <= ram_dout1;
-					end
+					reg_read1 = reg_gpr[r1_addr];
 				end
 			end
 		end
+		
+		assign r0_data = reg_read0;
+		assign r1_data = reg_read1;
 	end
+	endgenerate
 	
 endmodule
 
