58b4e855a9362274d27c25a872c76327b04f74fe
ryuji <ryuji@760bbadc-7816-4ce1-9c2b-5e91787a9cc6>
2009-10-26 22:53:05 +0000

    git-svn-id: http://10.72.141.11:8000/svn/jelly/trunk@253 760bbadc-7816-4ce1-9c2b-5e91787a9cc6

---
+++ rtl/common/demultiplexer.v
@@ -0,0 +1,45 @@
+// ---------------------------------------------------------------------------
+//  Common components
+//   demultiplexer
+//
+//                                 Copyright (C) 2009 by Ryuji Fuchikami
+//                                 http://homepage3.nifty.com/ryuz/
+// ---------------------------------------------------------------------------
+
+
+`timescale 1ns / 1ps
+
+
+
+// demultiplexer
+module jelly_demultiplexer
+		#(
+			parameter	SEL_WIDTH = 2,
+			parameter	NUM       = (1 << SEL_WIDTH),
+			parameter	IN_WIDTH  = 8,
+			parameter	OUT_WIDTH = (OUT_WIDTH * NUM),
+		)
+		(
+			input	wire						endian,
+			input	wire	[SEL_WIDTH-1:0]		sel,
+			input	wire	[IN_WIDTH-1:0]		din,
+			output	reg		[OUT_WIDTH-1:0]		dout
+		);
+	
+	integer i;
+	integer j;
+	always @* begin
+		dout = {OUT_WIDTH{1'b0}};
+		for ( i = 0; i < NUM; i = i + 1 ) begin
+			if ( i == (sel ^ {SEL_WIDTH{endian}}) ) begin
+				for ( j = 0; j < IN_WIDTH; j = j + 1 ) begin
+					dout[IN_WIDTH*i + j] = din[j];
+				end
+			end
+		end
+	end
+	
+endmodule
+
+
+// end of file
---
+++ rtl/common/multiplexer.v
@@ -0,0 +1,45 @@
+// ---------------------------------------------------------------------------
+//  Common components
+//   multiplexer 
+//
+//                                 Copyright (C) 2009 by Ryuji Fuchikami
+//                                 http://homepage3.nifty.com/ryuz/
+// ---------------------------------------------------------------------------
+
+
+`timescale 1ns / 1ps
+
+
+
+// multiplexer
+module jelly_multiplexer
+		#(
+			parameter	SEL_WIDTH = 2,
+			parameter	NUM       = (1 << SEL_WIDTH),
+			parameter	OUT_WIDTH = 8,
+			parameter	IN_WIDTH  = (OUT_WIDTH * NUM),
+		)
+		(
+			input	wire						endian,
+			input	wire	[SEL_WIDTH-1:0]		sel,
+			input	wire	[IN_WIDTH-1:0]		din,
+			output	reg		[OUT_WIDTH-1:0]		dout
+		);
+	
+	integer i;
+	integer j;
+	always @* begin
+		dout = {OUT_WIDTH{1'b0}};
+		for ( i = 0; i < NUM; i = i + 1 ) begin
+			if ( i == (sel ^ {SEL_WIDTH{endian}}) ) begin
+				for ( j = 0; j < OUT_WIDTH; j = j + 1 ) begin
+					dout[j] = din[OUT_WIDTH*i + j];
+				end
+			end
+		end
+	end
+		
+endmodule
+
+
+// end of file
