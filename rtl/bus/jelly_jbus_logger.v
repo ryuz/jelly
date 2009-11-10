cf4b2d1ae6d56b6c59d0ecfb2390faa72848d7ca
ryuji <ryuji@760bbadc-7816-4ce1-9c2b-5e91787a9cc6>
2009-11-10 13:03:43 +0000

    git-svn-id: http://10.72.141.11:8000/svn/jelly/trunk@346 760bbadc-7816-4ce1-9c2b-5e91787a9cc6

--- rtl/bus/jelly_jbus_logger.v
+++ rtl/bus/jelly_jbus_logger.v
@@ -19,7 +19,7 @@ module jelly_jbus_logger
 			parameter	FILE_NAME       = "",
 			parameter	DISPLAY         = 1,
 			parameter	MESSAGE         = "[jbus trace]",
-			parameter	CHECK_DATA      = 1,
+			parameter	CHECK_DATA      = 0,
 			parameter	CHECK_ADR_MASK  = 32'hf000_0000,
 			parameter	CHECK_ADR_VALUE = 32'h0000_0000,
 			parameter	CHECK_MEM_SIZE  = 256*1024
