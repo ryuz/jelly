394ca4451aba37d0512c01a9e2e0696addd84c3b
ryuji <ryuji@760bbadc-7816-4ce1-9c2b-5e91787a9cc6>
2010-05-26 14:56:35 +0000

    git-svn-id: http://10.72.141.11:8000/svn/jelly/trunk@423 760bbadc-7816-4ce1-9c2b-5e91787a9cc6

---
+++ rtl/i2c/jelly_i2c.v
@@ -0,0 +1,124 @@
+// ---------------------------------------------------------------------------
+//  Jelly  -- the soft-core processor system
+//    I2C
+//
+//                                  Copyright (C) 2008-2010 by Ryuji Fuchikami
+//                                      http://homepage3.nifty.com/ryuz
+// ---------------------------------------------------------------------------
+
+
+
+`timescale       1ns / 1ps
+`default_nettype none
+
+// uart
+module jelly_i2c
+		#(
+			parameter							DIVIDER_WIDTH = 16
+		)
+		(
+			input	wire						reset,
+			input	wire						clk,
+			
+			input	wire	[DIVIDER_WIDTH-1:0]	clk_dvider,
+			
+			output	wire						i2c_scl_t,
+			input	wire						i2c_scl_i,
+			output	wire						i2c_sda_t,
+			input	wire						i2c_sda_i,
+			
+			input	wire						ctl_start,
+			input	wire						ctl_end,
+			input	wire						ctl_send,
+			input	wire						ctl_recv,
+			input	wire	[7:0]				send_data,
+			output	wire	[7:0]				recv_data,
+			
+			output	wire						busy,			
+		);
+
+	reg		[DIVIDER_WIDTH-1:0]		reg_clk_counter;
+	reg								reg_clk_trig;
+	reg								reg_scl_t;
+	reg								reg_scl_i;
+	reg								reg_sda_t;
+	reg								reg_sda_i;
+	reg		[7:0]					reg_send_data;
+	reg		[7:0]					reg_read_data;
+	reg		[1:0]					reg_busy;
+	reg		[1:0]					reg_state;
+	reg		[3:0]					reg_counter;
+	
+	parameter	[1:0]	ST_START = 2'd0, ST_END = 2'd1, ST_START = 2'd2, ST_START = 2'd3;
+	
+	always @( posedge clk ) begin
+		if ( reset ) begin
+			
+		end
+		else begin
+			// input
+			reg_scl_i <= 2c_scl_i;
+			reg_sda_i <= 2c_sad_i; 
+			
+			// wait pullup voltage
+			if ( !(reg_scl_t == 1'b1 && reg_scl_i == 1'b0) ) begin
+				// counter
+				reg_clk_trig    <= (reg_clk_counter == 0);
+				reg_clk_counter <= (reg_clk_counter == 0) ? clk_dvider : reg_clk_counter - 1;
+				
+				if ( reg_clk_trig ) begin
+					if ( !reg_busy ) begin
+						if ( ctl_start ) begin
+							reg_busy      <= 1'b1;
+							reg_state     <= ST_START;
+							reg_send_data <= 8'hxx;
+							reg_counter   <= 0;
+						end
+						else if ( ctl_start ) begin
+							reg_busy      <= 1'b1;
+							reg_state     <= ST_END;
+							reg_send_data <= 8'hxx;
+							reg_counter   <= 0;
+						end
+						else if ( ctl_send ) begin
+							reg_busy      <= 1'b1;
+							reg_state     <= ST_SEND;
+							reg_send_data <= send_data;
+							reg_counter   <= 0;
+						end
+						else if ( ctl_recv ) begin
+							reg_busy      <= 1'b1;
+							reg_state     <= ST_START;
+							reg_send_data <= 8'hxx;
+							reg_counter   <= 0;
+						end
+					end
+					else begin
+						reg_counter <= reg_counter + 1;
+						case ( reg_state )
+						ST_START:
+							begin
+								case ()
+							end
+						
+						ST_END:
+						ST_START:
+						ST_START:
+						endcase
+					end
+				end
+				else begin
+				end
+			end
+		end
+	end
+	
+	
+endmodule
+
+
+`default_nettype wire
+
+
+// end of file
+
---
+++ rtl/i2c/jelly_i2c_core.v
@@ -0,0 +1,115 @@
+// ---------------------------------------------------------------------------
+//  Jelly  -- the soft-core processor system
+//    UART
+//
+//                                  Copyright (C) 2008-2009 by Ryuji Fuchikami
+//                                      http://homepage3.nifty.com/ryuz
+// ---------------------------------------------------------------------------
+
+
+
+`timescale 1ns / 1ps
+
+
+// uart
+module jelly_i2c_core
+		#(
+			parameter							TX_FIFO_PTR_WIDTH = 4,
+			parameter							RX_FIFO_PTR_WIDTH = 4,
+			         
+			parameter							WB_ADR_WIDTH  = 2,
+			parameter							WB_DAT_WIDTH  = 32,
+			parameter							WB_SEL_WIDTH  = (WB_DAT_WIDTH / 8)
+		)
+		(
+			input	wire						reset,
+			input	wire						clk,
+			
+			// UART
+			input	wire						uart_clk,
+			output	wire						uart_tx,
+			input	wire						uart_rx,
+			
+			output	wire						irq_rx,
+			output	wire						irq_tx,
+			
+			// control
+			input	wire	[WB_ADR_WIDTH-1:0]	wb_adr_i,
+			output	wire	[WB_DAT_WIDTH-1:0]	wb_dat_o,
+			input	wire	[WB_DAT_WIDTH-1:0]	wb_dat_i,
+			input	wire						wb_we_i,
+			input	wire	[WB_SEL_WIDTH-1:0]	wb_sel_i,
+			input	wire						wb_stb_i,
+			output	wire						wb_ack_o
+		);
+
+	localparam	TX_FIFO_SIZE = (1 << TX_FIFO_PTR_WIDTH);
+	localparam	RX_FIFO_SIZE = (1 << RX_FIFO_PTR_WIDTH);
+	
+	
+	// -------------------------
+	//   Core
+	// -------------------------
+	
+	wire							tx_en;
+	wire	[7:0]					tx_data;
+	wire							tx_ready;
+
+	wire							rx_en;
+	wire	[7:0]					rx_data;
+	wire							rx_ready;
+	
+	wire	[TX_FIFO_PTR_WIDTH:0]	tx_fifo_free_num;
+	wire	[RX_FIFO_PTR_WIDTH:0]	rx_fifo_data_num;
+	
+	jelly_uart_core
+			#(
+				.TX_FIFO_PTR_WIDTH	(TX_FIFO_PTR_WIDTH),
+				.RX_FIFO_PTR_WIDTH	(RX_FIFO_PTR_WIDTH)
+			)
+		i_uart_core
+			(
+				.reset				(reset),
+				.clk				(clk),
+				
+				.uart_clk			(uart_clk),
+				.uart_tx			(uart_tx),
+				.uart_rx			(uart_rx),
+				
+				.tx_en				(tx_en),
+				.tx_data			(tx_data),
+				.tx_ready			(tx_ready),
+				
+				.rx_en				(rx_en),
+				.rx_data			(rx_data),
+				.rx_ready			(rx_ready),
+				
+				.tx_fifo_free_num	(tx_fifo_free_num),
+				.rx_fifo_data_num	(rx_fifo_data_num)
+			);
+	
+	
+	// irq
+	assign irq_tx = (tx_fifo_free_num == TX_FIFO_SIZE);
+	assign irq_rx = rx_en;
+	
+	
+	// -------------------------
+	//  register
+	// -------------------------
+	
+	// TX
+	assign tx_en    = wb_stb_i & wb_we_i & (wb_adr_i == 0);
+	assign tx_data  = wb_dat_i[7:0];
+	
+	// RX
+	assign rx_ready = wb_stb_i & !wb_we_i & (wb_adr_i == 0);
+	
+	
+	assign wb_dat_o = (wb_stb_i && (wb_adr_i == 0)) ? rx_data           : 32'h00000000
+					| (wb_stb_i && (wb_adr_i == 1)) ? {tx_ready, rx_en} : 32'h00000000;
+	assign wb_ack_o = 1'b1;
+	
+	
+endmodule
+
