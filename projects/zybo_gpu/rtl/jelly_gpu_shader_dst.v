30a1b3a604e57dc70baba6fb5c4ef4e9336181c4
Ryuji Fuchikami <ryuz@users.sourceforge.jp>
2015-08-15 10:51:09 +0900

    update

--- projects/zybo_gpu/rtl/jelly_gpu_shader_dst.v
+++ projects/zybo_gpu/rtl/jelly_gpu_shader_dst.v
@@ -12,8 +12,8 @@
 `default_nettype none
 
 
-// シェーダー演算ソース側制御
-module jelly_gpu_shader_src
+// シェーダー演算デスティネーション側制御
+module jelly_gpu_shader_dst
 		#(
 			parameter	PC_WIDTH     = 9,
 			parameter	IMEM_LATENCY = 2,
@@ -33,13 +33,10 @@ module jelly_gpu_shader_src
 			input	wire	[31:0]			imem_rdata,
 			
 			// register file
-			output	wire					dmem0_en,
-			output	wire	[7:0]			dmem0_addr,
-			input	wire	[31:0]			dmem0_rdata,
-			
-			output	wire					dmem1_en,
-			output	wire	[7:0]			dmem1_addr,
-			input	wire	[31:0]			dmem1_rdata,
+			output	wire					dmem_we,
+			output	wire					dmem_we,
+			output	wire	[7:0]			dmem_addr,
+			input	wire	[31:0]			dmem_rdata,
 			
 			// sync
 			output	wire					sync_out,
@@ -47,19 +44,19 @@ module jelly_gpu_shader_src
 			
 			// ALU
 			output	wire	[1:0]			alu_select,
-			output	wire	[1:0]			alu_control,
-			output	wire	[31:0]			alu_src0,
-			output	wire	[31:0]			alu_src1,
-			output	wire					alu_valid,
-			input	wire					alu_ready
+			input	wire	[31:0]			alu_dst,
+			input	wire					alu_valid,
+			output	wire					alu_ready
 		);
 	
 	localparam	[3:0]	OPCODE_END      = 4'h0;
 	localparam	[3:0]	OPCODE_NOP      = 4'h1;
 	localparam	[3:0]	OPCODE_ALU      = 4'h2;
+	localparam	[3:0]	OPCODE_HI       = 4'h4;
+	localparam	[3:0]	OPCODE_LO       = 4'h5;
 	localparam	[3:0]	OPCODE_SYNC     = 4'hf;
 	
-	wire						interlock = (alu_valid && !alu_ready) || (sync_out != sync_in) || !cke;
+	wire						interlock = (!alu_valid && alu_ready) || (sync_out != sync_in) || !cke;
 	
 	wire						sig_end;
 	
@@ -98,8 +95,8 @@ module jelly_gpu_shader_src
 	wire	[3:0]		sig_if_opcode      = sig_if_instruction[3:0];
 	wire	[1:0]		sig_if_alu_sel     = sig_if_instruction[5:4];
 	wire	[1:0]		sig_if_alu_ctl     = sig_if_instruction[7:6];
-	wire	[7:0]		sig_if_src0        = sig_if_instruction[23:16];
-	wire	[7:0]		sig_if_src1        = sig_if_instruction[31:24];
+	wire	[7:0]		sig_if_dst         = sig_if_instruction[15:8];
+	wire	[15:0]		sig_if_immediate   = sig_if_instruction[31:16];
 	
 	reg		[IMEM_LATENCY-1:0]	reg_if_valid;
 	always @(posedge clk) begin
@@ -117,17 +114,9 @@ module jelly_gpu_shader_src
 	end
 	
 	
-	// decode & dmem read
-	assign	dmem0_en   = !interlock;
-	assign	dmem0_addr = sig_if_src0;
-	assign	dmem1_en   = !interlock;
-	assign	dmem1_addr = sig_if_src1;
 	
-	wire	[31:0]	sig_dec_src0 = dmem0_rdata;
-	wire	[31:0]	sig_dec_src1 = dmem1_rdata;
-	
-	
-	reg		[DMEM_LATENCY-1:0]	reg_dec_valid;
+	// decode
+	reg						reg_dec_valid;
 	always @(posedge clk) begin
 		if ( reset ) begin
 			reg_dec_valid <= {DMEM_LATENCY{1'b0}};
@@ -142,25 +131,27 @@ module jelly_gpu_shader_src
 		end
 	end
 	
-	
 	reg		[1:0]		reg_dec_alu_sel;
-	reg		[1:0]		reg_dec_alu_ctl;
 	reg					reg_dec_alu_valid;
+	reg		[31:0]		reg_dec_imm_data;
+	reg					reg_dec_imm_valid;
 	reg					reg_dec_sync;
 	reg					reg_dec_end;
 	
 	always @(posedge clk) begin
 		if ( reset ) begin
 			reg_dec_alu_sel   <= {2{1'bx}};
-			reg_dec_alu_ctl   <= {2{1'bx}};
 			reg_dec_alu_valid <= 1'b0;
+			reg_dec_imm_data  <= {32{1'bx}};
+			reg_dec_imm_valid <= 1'b0;
 			reg_dec_sync      <= 1'b0;
 			reg_dec_end       <= 1'b0;
 		end
 		else if ( !interlock ) begin
+			reg_dec_dst       <= {8{1'bx}};
 			reg_dec_alu_sel   <= {2{1'bx}};
-			reg_dec_alu_ctl   <= {2{1'bx}};
 			reg_dec_alu_valid <= 1'b0;
+			reg_dec_imm_valid <= 1'b0;
 			reg_dec_sync      <= 1'b0;
 			reg_dec_end       <= 1'b0;
 			
@@ -168,87 +159,62 @@ module jelly_gpu_shader_src
 				case ( sig_if_opcode )
 				OPCODE_ALU:
 					begin
+						reg_dec_dst       <= sig_if_dst;
 						reg_dec_alu_sel   <= sig_if_alu_sel;
-						reg_dec_alu_ctl   <= sig_if_alu_ctl;
 						reg_dec_alu_valid <= 1'b1;
+					end
+					
+				OPCODE_HI:
+					begin
+						reg_dec_imm_data[31:16] <= sig_if_immediate;
+					end
+					
+				OPCODE_LO:
+					begin
+						reg_dec_dst             <= sig_if_dst;
+						reg_dec_imm_data[15:0]  <= sig_if_immediate;
+						reg_dec_imm_valid       <= 1'b1;
 					end
 					
 				OPCODE_SYNC:
 					begin
-						reg_dec_sync      <= 1'b1;
+						reg_dec_sync <= 1'b1;
 					end
 					
 				OPCODE_END:
 					begin
-						reg_dec_end       <= 1'b1;
+						reg_dec_end <= 1'b1;
 					end
 				endcase
 			end
 		end
 	end
 	
-	assign sig_end     = reg_dec_end;
-	assign sync_out    = reg_dec_sync;
-	
-	wire	[1:0]		sig_dec_alu_sel;
-	wire	[1:0]		sig_dec_alu_ctl;
-	wire				sig_dec_alu_valid;
-	
-	jelly_data_delay
-			#(
-				.LATENCY		(DMEM_LATENCY-1),
-				.DATA_WIDTH		(5),
-				.DATA_INIT		(5'bxx_xx_0)
-			)
-		i_data_delay
-			(
-				.reset			(reset),
-				.clk			(clk),
-				.cke			(!interlock),
-				
-				.in_data		({reg_dec_alu_sel, reg_dec_alu_ctl, reg_dec_alu_valid}),
-				
-				.out_data		({sig_dec_alu_sel, sig_dec_alu_ctl, sig_dec_alu_valid})
-			);
+	assign sig_end  = reg_dec_end;
+	assign sync_out = reg_dec_sync;
 	
 	
 	// execute
-	reg		[1:0]		reg_exe_select;
-	reg		[1:0]		reg_exe_control;
-	reg		[31:0]		reg_exe_src0;
-	reg		[31:0]		reg_exe_src1;
+	assign alu_select = reg_dec_alu_sel;
+	assign alu_ready  = reg_dec_alu_valid;
+	
+	reg		[7:0]		reg_exe_dst;
+	reg		[31:0]		reg_exe_data;
 	reg					reg_exe_valid;
 	always @(posedge clk) begin
 		if ( reset ) begin
-			reg_exe_select  <= {2{1'bx}};
-			reg_exe_control <= {2{1'bx}};
-			reg_exe_src0    <= {32{1'bx}};
-			reg_exe_src1    <= {32{1'bx}};
-			reg_exe_valid   <= 1'b0;
+			reg_exe_dst   <= {8{1'bx}};
+			reg_exe_data  <= {32{1'bx}};
+			reg_exe_valid <= 1'b0;
 		end
-		else begin
-			if ( alu_ready ) begin
-				reg_exe_valid <= 1'b0;
-			end
-			
-			if ( !interlock ) begin
-				reg_exe_select  <= sig_dec_alu_sel;
-				reg_exe_control <= sig_dec_alu_ctl;
-				reg_exe_src0    <= sig_dec_src0;
-				reg_exe_src1    <= sig_dec_src1;
-				reg_exe_valid   <= sig_dec_alu_valid;
-			
-			end
+		else if ( !interlock ) begin
+			reg_exe_dst   <= reg_dec_dst;
+			reg_exe_data  <= reg_dec_imm_valid ? reg_dec_imm_valid : alu_dst;
+			reg_exe_valid <= reg_dec_alu_valid || reg_dec_imm_valid;
 		end
 	end
 	
-	assign	alu_select  = reg_exe_select;
-	assign	alu_control = reg_exe_control;
-	assign	alu_src0    = reg_exe_src0;
-	assign	alu_src1    = reg_exe_src1;
-	assign	alu_valid   = reg_exe_valid;
-	
-	assign busy = (reg_pc_valid || reg_dec_alu_valid || reg_dec_sync || reg_exe_valid);
+	assign busy = (reg_pc_valid || reg_dec_alu_valid || reg_dec_imm_valid || reg_dec_sync || reg_exe_valid);
 	
 endmodule
 
