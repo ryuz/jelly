57ed8729ac90d7028e2d091601cbfd87a48826a4
Ryuji Fuchikami <ryuz@users.sourceforge.jp>
2016-11-24 22:25:44 +0900

    add scatter testbench

--- projects/testbench/sim/tb_scatter.v
+++ projects/testbench/sim/tb_scatter.v
@@ -17,22 +17,33 @@ module tb_scatter();
 	reg		reset = 1'b1;
 	always #(RATE*100)	reset = 1'b0;
 	
-	parameter	PORT_NUM   = 16;
+	
+	// 4サイクルかかる処理を4並列で実行して、スループットを１にする
+	
+	parameter	PORT_NUM   = 4;
 	parameter	DATA_WIDTH = 16;
-	parameter	LINE_SIZE  = 640;
+	parameter	LINE_SIZE  = 256;
 	parameter	UNIT_SIZE  = (LINE_SIZE + (PORT_NUM-1)) / PORT_NUM;
 	
 	reg		[DATA_WIDTH-1:0]			src_data;
 	reg									src_valid;
 	wire								src_ready;
 	
-	wire	[PORT_NUM*DATA_WIDTH-1:0]	port_data;
-	wire	[PORT_NUM-1:0]				port_valid;
-	wire	[PORT_NUM-1:0]				port_ready;
+	wire	[PORT_NUM*DATA_WIDTH-1:0]	port_s_data;
+	wire	[PORT_NUM-1:0]				port_s_valid;
+	wire	[PORT_NUM-1:0]				port_s_ready;
+	
+	wire	[PORT_NUM*DATA_WIDTH-1:0]	port_m_data;
+	wire	[PORT_NUM-1:0]				port_m_valid;
+	wire	[PORT_NUM-1:0]				port_m_ready;
 	
 	wire	[DATA_WIDTH-1:0]			sink_data;
 	wire								sink_valid;
 	reg									sink_ready = 1;
+	
+	always @(posedge clk) begin
+		sink_ready <= 1; // {$random};
+	end
 	
 	jelly_data_scatter
 			#(
@@ -50,11 +61,33 @@ module tb_scatter();
 				.s_valid		(src_valid),
 				.s_ready		(src_ready),
 				
-				.m_data			(port_data),
-				.m_valid		(port_valid),
-				.m_ready		(port_ready)
+				.m_data			(port_s_data),
+				.m_valid		(port_s_valid),
+				.m_ready		(port_s_ready)
 			);
 	
+	genvar	i;
+	generate
+	for ( i = 0; i < PORT_NUM; i= i + 1 ) begin : loop_dummy
+		dummy_proc
+				#(
+					.DATA_WIDTH		(DATA_WIDTH)
+				)
+			i_dummy_proc
+				(
+					.reset			(reset),
+					.clk			(clk),
+					
+					.s_data			(port_s_data [i*DATA_WIDTH +: DATA_WIDTH]),
+					.s_valid		(port_s_valid[i]),
+					.s_ready		(port_s_ready[i]),
+					
+					.m_data			(port_m_data [i*DATA_WIDTH +: DATA_WIDTH]),
+					.m_valid		(port_m_valid[i]),
+					.m_ready		(port_m_ready[i])
+				);
+	end
+	endgenerate
 	
 	jelly_data_gather
 			#(
@@ -68,9 +101,9 @@ module tb_scatter();
 				.reset			(reset),
 				.clk			(clk),
 				
-				.s_data			(port_data),
-				.s_valid		(port_valid),
-				.s_ready		(port_ready),
+				.s_data			(port_m_data),
+				.s_valid		(port_m_valid),
+				.s_ready		(port_m_ready),
 				
 				.m_data			(sink_data),
 				.m_valid		(sink_valid),
@@ -86,12 +119,65 @@ module tb_scatter();
 			if ( src_valid && src_ready ) begin
 				src_data <= src_data + 1;
 			end
-			src_valid <= 1;
+			
+			if ( !src_valid || src_ready ) begin
+				src_valid <= 1; // {$random};
+			end
+		end
+	end
+	
+	
+	reg		[DATA_WIDTH-1:0]	exp_data;
+	always @(posedge clk) begin
+		if ( reset ) begin
+			exp_data <= 0;
+		end
+		else begin
+			if ( sink_valid && sink_ready ) begin
+				if ( sink_data != exp_data ) begin
+					$display("Error!");
+				end
+	//			$display("%h", sink_data);
+				exp_data <= exp_data + 1;
+			end
 		end
 	end
 	
 	
 endmodule
+
+
+
+module dummy_proc
+		#(
+			parameter	DATA_WIDTH = 16
+		)
+		(
+			input	wire						reset,
+			input	wire						clk,
+			
+			input	wire	[DATA_WIDTH-1:0]	s_data,
+			input	wire						s_valid,
+			output	wire						s_ready,
+			
+			output	wire	[DATA_WIDTH-1:0]	m_data,
+			output	wire						m_valid,
+			input	wire						m_ready
+		);
+	
+	reg		[1:0]	reg_busy = 0;
+	always @(posedge clk) begin
+//		reg_busy <= {$random()};
+		reg_busy <= reg_busy + 1;
+	end
+	wire			busy = (reg_busy != 0);
+	
+	assign m_data  = s_data;
+	assign m_valid = s_valid && !busy;
+	assign s_ready = m_ready && !busy;
+	
+endmodule
+
 
 
 `default_nettype wire
