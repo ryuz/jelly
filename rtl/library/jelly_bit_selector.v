b2cff30d345ba76f7a164702ee88c1028c4e6053
Ryuji Fuchikami <ryuz@users.sourceforge.jp>
2017-01-29 17:21:37 +0900

    add bit shifter

---
+++ rtl/library/jelly_bit_selector.v
@@ -0,0 +1,77 @@
+// ---------------------------------------------------------------------------
+//  Jelly  -- the system on fpga system
+//
+//                                 Copyright (C) 2008-2017 by Ryuji Fuchikami
+//                                 http://ryuz.my.coocan.jp/
+//                                 https://github.com/ryuz/jelly.git
+// ---------------------------------------------------------------------------
+
+
+
+`timescale 1ns / 1ps
+`default_nettype none
+
+
+
+// multiplexer
+module jelly_bit_selector
+		#(
+			parameter	SEL_WIDTH     = 4,
+			parameter	BIT_WIDTH     = (1 << SEL_WIDTH),
+			
+			parameter	USE_PRIMITIVE = 0,
+			parameter	DEVICE        = "RTL"
+		)
+		(
+			input	wire	[SEL_BITS-1:0]		sel,
+			input	wire	[BIT_WIDTH-1:0]		din,
+			output	wire						dout
+		);
+	
+	localparam	SEL_BITS  = SEL_WIDTH > 0 ? SEL_WIDTH : 1;
+	
+	generate
+	if ( SEL_WIDTH <= 0 ) begin : blk_bypass
+		assign dout = din;
+	end
+	else if ( USE_PRIMITIVE && (BIT_WIDTH <= 4) ) begin : blk_mul4
+		wire	[1:0]	bit4_sel = sel;
+		wire	[3:0]	bit4_din = din;
+		jelly_multiplexer4
+				#(
+					.DEVICE		(DEVICE)
+				)
+			i_multiplexer4
+				(
+					.o			(dout),
+					.i			(bit4_din),
+					.s			(bit4_sel)
+				);
+	end
+	else if ( USE_PRIMITIVE && (BIT_WIDTH <= 4) ) begin : blk_mul16
+		wire	[3:0]	bit16_sel = sel;
+		wire	[15:0]	bit16_din = din;
+		jelly_multiplexer16
+				#(
+					.DEVICE		(DEVICE)
+				)
+			i_multiplexer16
+				(
+					.o			(dout),
+					.i			(bit16_din),
+					.s			(bit16_sel)
+				);
+	end
+	else begin : blk_rtl
+		assign dout = din[sel];
+	end
+	endgenerate
+	
+endmodule
+
+
+
+`default_nettype wire
+
+
+// end of file
---
+++ rtl/library/jelly_bus_shift.v
@@ -0,0 +1,79 @@
+// ---------------------------------------------------------------------------
+//  Jelly  -- the system on fpga system
+//
+//                                 Copyright (C) 2008-2017 by Ryuji Fuchikami
+//                                 http://ryuz.my.coocan.jp/
+//                                 https://github.com/ryuz/jelly.git
+// ---------------------------------------------------------------------------
+
+
+
+`timescale 1ns / 1ps
+`default_nettype none
+
+
+// shift
+module jelly_bus_shift
+		#(
+			parameter	SHIFT_WIDTH   = 4,
+			parameter	NUM           = (1 << SHIFT_WIDTH),
+			parameter	DATA_WIDTH    = 8,
+			
+			parameter	LEFT          = 0,
+			parameter	ARITHMETIC    = 0,		// <- Žg‚í‚È‚¢H
+			parameter	ROTATION      = 0,
+			
+			parameter	USE_PRIMITIVE = 0,
+			parameter	DEVICE        = "7SERIES" // "RTL"
+		)
+		(
+			input	wire	[SHIFT_WIDTH-1:0]		shift,
+			input	wire	[NUM*DATA_WIDTH-1:0]	in_data,
+			output	wire	[NUM*DATA_WIDTH-1:0]	out_data
+		);
+	
+	genvar		i, j;
+	
+	generate
+	for ( i = 0; i < DATA_WIDTH; i = i+1 ) begin : loop_shift
+		wire	[NUM-1:0]	in_bus;
+		wire	[NUM-1:0]	out_bus;
+		
+		for ( j = 0; j < NUM; j = j+1 ) begin : loop_in_bus
+			assign in_bus[j] = in_data[j*DATA_WIDTH + i];
+		end
+		
+		jelly_bit_shift
+				#(
+					.SHIFT_WIDTH	(SHIFT_WIDTH),
+					.DATA_WIDTH		(NUM),
+					
+					.LEFT			(LEFT),
+					.ARITHMETIC		(ARITHMETIC),
+					.ROTATION		(ROTATION),
+					
+					.USE_PRIMITIVE	(USE_PRIMITIVE),
+					.DEVICE			(DEVICE)
+				)
+			i_bit_shift
+				(
+					.shift			(shift),
+					.in_data		(in_bus),
+					.out_data		(out_bus)
+				);
+		
+		for ( j = 0; j < NUM; j = j+1 ) begin : loop_out_bus
+			assign out_data[j*DATA_WIDTH + i] = out_bus[j];
+		end
+	end
+	endgenerate
+	
+	
+endmodule
+
+
+
+`default_nettype wire
+
+
+// end of file
