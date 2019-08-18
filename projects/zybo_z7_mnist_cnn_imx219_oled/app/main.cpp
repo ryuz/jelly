e1f0ec3ad051367a84295240658b09d1fd9bad64
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2019-08-18 17:21:02 +0900

    update

--- projects/zybo_z7_mnist_cnn_imx219_oled/app/main.cpp
+++ projects/zybo_z7_mnist_cnn_imx219_oled/app/main.cpp
@@ -16,21 +16,104 @@
 
 
 
+#define DVITX_WIDTH                         1280
+#define DVITX_HEIGHT                        720
+#define DVITX_STRIDE                        8192
+
+
+#define IMG_MEM_ADDR						0x30000000
+
+#define VIN_NORM_BASE_ADDR					0x00011000
+#define	VIN_NORM_REG_CONTROL      			(VIN_NORM_BASE_ADDR + 0x00*4)
+#define	VIN_NORM_REG_BUSY         			(VIN_NORM_BASE_ADDR + 0x01*4)
+#define	VIN_NORM_REG_INDEX        			(VIN_NORM_BASE_ADDR + 0x02*4)
+#define	VIN_NORM_REG_SKIP         			(VIN_NORM_BASE_ADDR + 0x03*4)
+#define	VIN_NORM_REG_FRM_TIMER_EN 			(VIN_NORM_BASE_ADDR + 0x04*4)
+#define	VIN_NORM_REG_FRM_TIMEOUT  			(VIN_NORM_BASE_ADDR + 0x05*4)
+#define	VIN_NORM_REG_PARAM_WIDTH  			(VIN_NORM_BASE_ADDR + 0x08*4)
+#define	VIN_NORM_REG_PARAM_HEIGHT 			(VIN_NORM_BASE_ADDR + 0x09*4)
+#define	VIN_NORM_REG_PARAM_FILL   			(VIN_NORM_BASE_ADDR + 0x0a*4)
+#define	VIN_NORM_REG_PARAM_TIMEOUT			(VIN_NORM_BASE_ADDR + 0x0b*4)
+
+#define VDMAW_BASE_ADDR						0x00010000
+#define VDMAW_REG_ID						(VDMAW_BASE_ADDR + 0x0000)
+#define VDMAW_REG_VERSION					(VDMAW_BASE_ADDR + 0x0004)
+#define VDMAW_REG_CTL_CONTROL				(VDMAW_BASE_ADDR + 0x0010)
+#define VDMAW_REG_CTL_STATUS				(VDMAW_BASE_ADDR + 0x0014)
+#define VDMAW_REG_CTL_INDEX 				(VDMAW_BASE_ADDR + 0x001c)
+#define VDMAW_REG_PARAM_ADDR				(VDMAW_BASE_ADDR + 0x0020)
+#define VDMAW_REG_PARAM_STRIDE				(VDMAW_BASE_ADDR + 0x0024)
+#define VDMAW_REG_PARAM_WIDTH				(VDMAW_BASE_ADDR + 0x0028)
+#define VDMAW_REG_PARAM_HEIGHT				(VDMAW_BASE_ADDR + 0x002c)
+#define VDMAW_REG_PARAM_SIZE				(VDMAW_BASE_ADDR + 0x0030)
+#define VDMAW_REG_PARAM_AWLEN				(VDMAW_BASE_ADDR + 0x003c)
+#define VDMAW_REG_MONITOR_ADDR				(VDMAW_BASE_ADDR + 0x0040)
+#define VDMAW_REG_MONITOR_STRIDE			(VDMAW_BASE_ADDR + 0x0044)
+#define VDMAW_REG_MONITOR_WIDTH 			(VDMAW_BASE_ADDR + 0x0048)
+#define VDMAW_REG_MONITOR_HEIGHT			(VDMAW_BASE_ADDR + 0x004c)
+#define VDMAW_REG_MONITOR_SIZE				(VDMAW_BASE_ADDR + 0x0050)
+#define VDMAW_REG_MONITOR_AWLEN 			(VDMAW_BASE_ADDR + 0x005c)
+
+#define VDMAR_BASE_ADDR 					0x00020000
+#define VDMAR_REG_ID						(VDMAR_BASE_ADDR + 0x0000)
+#define VDMAR_REG_VERSION					(VDMAR_BASE_ADDR + 0x0004)
+#define VDMAR_REG_CTL_CONTROL				(VDMAR_BASE_ADDR + 0x0010)
+#define VDMAR_REG_CTL_STATUS				(VDMAR_BASE_ADDR + 0x0014)
+#define VDMAR_REG_CTL_INDEX 				(VDMAR_BASE_ADDR + 0x001c)
+#define VDMAR_REG_PARAM_ADDR				(VDMAR_BASE_ADDR + 0x0020)
+#define VDMAR_REG_PARAM_STRIDE				(VDMAR_BASE_ADDR + 0x0024)
+#define VDMAR_REG_PARAM_WIDTH				(VDMAR_BASE_ADDR + 0x0028)
+#define VDMAR_REG_PARAM_HEIGHT				(VDMAR_BASE_ADDR + 0x002c)
+#define VDMAR_REG_PARAM_SIZE				(VDMAR_BASE_ADDR + 0x0030)
+#define VDMAR_REG_PARAM_ARLEN				(VDMAR_BASE_ADDR + 0x003c)
+#define VDMAR_REG_MONITOR_ADDR				(VDMAR_BASE_ADDR + 0x0040)
+#define VDMAR_REG_MONITOR_STRIDE			(VDMAR_BASE_ADDR + 0x0044)
+#define VDMAR_REG_MONITOR_WIDTH 			(VDMAR_BASE_ADDR + 0x0048)
+#define VDMAR_REG_MONITOR_HEIGHT			(VDMAR_BASE_ADDR + 0x004c)
+#define VDMAR_REG_MONITOR_SIZE				(VDMAR_BASE_ADDR + 0x0050)
+#define VDMAR_REG_MONITOR_ARLEN 			(VDMAR_BASE_ADDR + 0x005c)
+
+#define	VOUT_VSGEN_BASE_ADDR				0x00021000
+#define	VOUT_VSGEN_REG_ID                	(VOUT_VSGEN_BASE_ADDR + 0x0000)
+#define	VOUT_VSGEN_REG_VERSION           	(VOUT_VSGEN_BASE_ADDR + 0x0004)
+#define	VOUT_VSGEN_REG_CTL_CONTROL       	(VOUT_VSGEN_BASE_ADDR + 0x0010)
+#define	VOUT_VSGEN_REG_CTL_STATUS        	(VOUT_VSGEN_BASE_ADDR + 0x0014)
+#define	VOUT_VSGEN_REG_PARAM_HTOTAL      	(VOUT_VSGEN_BASE_ADDR + 0x0020)
+#define	VOUT_VSGEN_REG_PARAM_HSYNC_POL   	(VOUT_VSGEN_BASE_ADDR + 0x002c)
+#define	VOUT_VSGEN_REG_PARAM_HDISP_START 	(VOUT_VSGEN_BASE_ADDR + 0x0030)
+#define	VOUT_VSGEN_REG_PARAM_HDISP_END   	(VOUT_VSGEN_BASE_ADDR + 0x0034)
+#define	VOUT_VSGEN_REG_PARAM_HSYNC_START 	(VOUT_VSGEN_BASE_ADDR + 0x0038)
+#define	VOUT_VSGEN_REG_PARAM_HSYNC_END   	(VOUT_VSGEN_BASE_ADDR + 0x003c)
+#define	VOUT_VSGEN_REG_PARAM_VTOTAL      	(VOUT_VSGEN_BASE_ADDR + 0x0040)
+#define	VOUT_VSGEN_REG_PARAM_VSYNC_POL   	(VOUT_VSGEN_BASE_ADDR + 0x004c)
+#define	VOUT_VSGEN_REG_PARAM_VDISP_START 	(VOUT_VSGEN_BASE_ADDR + 0x0050)
+#define	VOUT_VSGEN_REG_PARAM_VDISP_END   	(VOUT_VSGEN_BASE_ADDR + 0x0054)
+#define	VOUT_VSGEN_REG_PARAM_VSYNC_START 	(VOUT_VSGEN_BASE_ADDR + 0x0058)
+#define	VOUT_VSGEN_REG_PARAM_VSYNC_END   	(VOUT_VSGEN_BASE_ADDR + 0x005c)
+
+
+
+
 void oled_init(UioMmap* p);
 int  oled_main();
 
-void capture_still_image(UioMmap& um_pl_peri, int width, int height, int frame_num);    // DDR3-SDRAM経由で画像をキャプチャ
-void camera_setup(I2cAccess& i2c, int w, int h);                                        // カメラ初期化
-
-
+void camera_setup(I2cAccess& i2c, int w, int h);                                                                 // カメラ初期化
+void capture_still_image(UioMmap& um_pl_peri, int offset, int width, int height, int stride, int frame_num);    // DDR3-SDRAM経由で画像をキャプチャ
+void start_viewer_dma   (UioMmap& um_pl_peri, int offset, int width, int height, int stride, int frame_num);
+void write_back_image(void* mem_addr);
 
 // メイン関数
 int main(int argc, char *argv[])
 {
-    bool gui = false;
+    bool gui  = false;
+    bool stop = false;
     if ( argc >= 2 && strcmp(argv[1], "gui") == 0 ) {
         gui = true;
     }
+    if ( argc >= 2 && strcmp(argv[1], "stop") == 0 ) {
+        stop = true;
+    }
+    
     
     // UIOオープン
     UioMmap um_pl_peri("my_pl_peri", 0x00200000);
@@ -46,14 +129,45 @@ int main(int argc, char *argv[])
     }
     
     
+    // 一旦すべてのDMA等を止める
+    um_pl_peri.WriteWord32(VDMAR_REG_CTL_CONTROL, 0x0);
+    um_pl_peri.WriteWord32(VDMAW_REG_CTL_CONTROL, 0x0);
+    while ( um_pl_peri.ReadWord32(VDMAR_REG_CTL_STATUS) != 0 ) {
+        usleep(1000);
+    }
+    while ( um_pl_peri.ReadWord32(VDMAW_REG_CTL_STATUS) != 0 ) {
+        usleep(1000);
+    }
+    
+    // normalizer stop
+    um_pl_peri.WriteWord32(0x00011000, 0);
+    usleep(1000);
+    while ( um_pl_peri.ReadWord32(0x00011004) != 0 ) {
+        usleep(1000);
+    }
+    
+    // 止めるだけならここで終了
+    if ( stop ) {
+        printf("stop\n");
+        return 0;
+    }
+    
+    
     // OLED初期化
     oled_init(&um_pl_peri);
     oled_main();
     
     
-    
+    // カメラサイズ
     int width  = 640;
     int height = 132;
+    
+    // DVIへの表示位置
+    int dvi_x  = (DVITX_WIDTH  - width ) / 2;
+    int dvi_y  = (DVITX_HEIGHT - height) / 2;
+    
+    int stride = DVITX_STRIDE;
+    int offset = DVITX_STRIDE * dvi_y + dvi_x * 4;
     void* mem_addr = um_pl_mem.GetAddress();
     
     
@@ -75,13 +189,6 @@ int main(int argc, char *argv[])
     camera_setup(i2c, width, height);
     
     
-    
-    // normalizer stop
-    um_pl_peri.WriteWord32(0x00011000, 0);
-    usleep(1000);
-    while ( um_pl_peri.ReadWord32(0x00011004) != 0 ) {
-        usleep(1000);
-    }
     
     // demosaic param_phase
     um_pl_peri.WriteWord32(0x00012000, 0);
@@ -118,14 +225,37 @@ int main(int argc, char *argv[])
     um_pl_peri.WriteWord32(0x00015000, dnn0_lpf);
     um_pl_peri.WriteWord32(0x00015040, dnn1_lpf);
     
+    // 背景画像展開
+    write_back_image(mem_addr);
+    
+    
+    // DVI-TX start DMA read
+    um_pl_peri.WriteWord32(VDMAR_REG_PARAM_ADDR,   0x30000000);
+    um_pl_peri.WriteWord32(VDMAR_REG_PARAM_STRIDE, DVITX_STRIDE);
+    um_pl_peri.WriteWord32(VDMAR_REG_PARAM_WIDTH,  DVITX_WIDTH);
+    um_pl_peri.WriteWord32(VDMAR_REG_PARAM_HEIGHT, DVITX_HEIGHT);
+    um_pl_peri.WriteWord32(VDMAR_REG_PARAM_SIZE,   DVITX_WIDTH*DVITX_HEIGHT);
+    um_pl_peri.WriteWord32(VDMAR_REG_PARAM_ARLEN,  31);
+    um_pl_peri.WriteWord32(VDMAR_REG_CTL_CONTROL,  0x3);
+    
+    um_pl_peri.WriteWord32(VOUT_VSGEN_REG_CTL_CONTROL, 1);
+    
+    
     
     int     frame_num = 1;
     int     key = -1;
     do {
         // 画像取り込み
-        capture_still_image(um_pl_peri, width, height, frame_num);
+        capture_still_image(um_pl_peri, offset, width, height, stride, frame_num);
         cv::Mat img(height*frame_num, width, CV_8UC4);
-        memcpy(img.data, (void *)mem_addr, width * height * 4 * frame_num);
+        {
+            char* p = (char*)mem_addr;
+            p += offset;
+            for ( int i = 0; i < height * frame_num; ++i ) {
+                memcpy(&img.data[width*4*i], p, width*4);
+                p += stride;
+            }
+        }
         
         // GUI表示
         if ( gui ) {
@@ -187,7 +317,7 @@ int main(int argc, char *argv[])
         // 録画
         if ( key == 'r' ) {
             printf("record\n");
-            capture_still_image(um_pl_peri, width, height, 100);
+            capture_still_image(um_pl_peri, 0, width, height, width*4, 100);
             char* p = (char*)mem_addr;
             for ( int i = 0; i< 100; i++ ) {
                 char fname[64];
@@ -198,27 +328,49 @@ int main(int argc, char *argv[])
                 cv::cvtColor(imgRec, imgRgb, CV_BGRA2BGR);
                 cv::imwrite(fname, imgRgb);
             }
+            
+            
+            // 背景画像展開
+            write_back_image(mem_addr);
         }
     } while ( gui && ((key = (cv::waitKey(10) & 0xff)) != 0x1b) );
     
+    
+    // DVI用にDMAを動きっぱなしにする
+    start_viewer_dma(um_pl_peri, offset, width, height, stride, frame_num);
     
     return 0;
 }
 
 
 
+void write_back_image(void* mem_addr)
+{
+    unsigned char* p = (unsigned char*)mem_addr;
+    
+    // 背景画像展開
+    cv::Mat img = cv::imread("back.png");
+    if ( !img.empty() ) {
+        cv::cvtColor(img, img, CV_RGB2RGBA);
+        for ( int i = 0; i < DVITX_HEIGHT; ++i ) {
+            memcpy(&p[i*DVITX_STRIDE], &img.data[img.cols*4*i], img.cols*4);
+        }
+    }
+}
+
+
 
 // DDR3-SDRAM経由で画像をキャプチャ
-void capture_still_image(UioMmap& um_pl_peri, int width, int height, int frame_num)
+void capture_still_image(UioMmap& um_pl_peri, int offset, int width, int height, int stride, int frame_num)
 {
     // DMA start (one shot)
-    um_pl_peri.WriteWord32(0x00010020, 0x30000000);
-    um_pl_peri.WriteWord32(0x00010024, width*4);                // stride
+    um_pl_peri.WriteWord32(0x00010020, 0x30000000 + offset);
+    um_pl_peri.WriteWord32(0x00010024, stride);                 // stride
     um_pl_peri.WriteWord32(0x00010028, width);                  // width
     um_pl_peri.WriteWord32(0x0001002c, height);                 // height
     um_pl_peri.WriteWord32(0x00010030, width*height*frame_num); // size
     um_pl_peri.WriteWord32(0x0001003c, 31);                     // awlen
-    um_pl_peri.WriteWord32(0x00010010, 0x07);
+    um_pl_peri.WriteWord32(0x00010010, 0x07);   // one-shot
     
     // normalizer start
     um_pl_peri.WriteWord32(0x00011010, 1);
@@ -228,15 +380,41 @@ void capture_still_image(UioMmap& um_pl_peri, int width, int height, int frame_n
     um_pl_peri.WriteWord32(0x00011028, 0xfff);
     um_pl_peri.WriteWord32(0x0001102c, 0x10000);
     um_pl_peri.WriteWord32(0x00011000, 3);
-    usleep(100000);
-    
+//  usleep(100000);
     
     // wait for DMA end
-    usleep(10000);
+    usleep(1000);
     while ( um_pl_peri.ReadWord32(0x00010014) != 0 ) {
         usleep(10000);
     }
 }
+
+
+void start_viewer_dma(UioMmap& um_pl_peri, int offset, int width, int height, int stride, int frame_num)
+{
+    while ( um_pl_peri.ReadWord32(0x00010014) != 0 ) {
+        usleep(10000);
+    }
+    
+    // DMA start (one shot)
+    um_pl_peri.WriteWord32(VDMAW_REG_PARAM_ADDR, 0x30000000 + offset);
+    um_pl_peri.WriteWord32(VDMAW_REG_PARAM_STRIDE, stride);                 // stride
+    um_pl_peri.WriteWord32(VDMAW_REG_PARAM_WIDTH, width);                  // width
+    um_pl_peri.WriteWord32(VDMAW_REG_PARAM_HEIGHT, height);                 // height
+    um_pl_peri.WriteWord32(VDMAW_REG_PARAM_SIZE, width*height*frame_num); // size
+    um_pl_peri.WriteWord32(VDMAW_REG_PARAM_AWLEN, 3);                      // awlen
+    um_pl_peri.WriteWord32(VDMAW_REG_CTL_CONTROL, 0x03);
+    
+    // normalizer start
+    um_pl_peri.WriteWord32(0x00011010, 1);
+    um_pl_peri.WriteWord32(0x00011014, 100000000);
+    um_pl_peri.WriteWord32(0x00011020, width);
+    um_pl_peri.WriteWord32(0x00011024, height);
+    um_pl_peri.WriteWord32(0x00011028, 0xfff);
+    um_pl_peri.WriteWord32(0x0001102c, 0x10000);
+    um_pl_peri.WriteWord32(0x00011000, 3);
+}
+
 
 
 // カメラ初期化
