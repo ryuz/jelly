879a34cdeccdc266c536d3ea709a703475cbfa7f
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2020-06-03 06:31:40 +0900

    add zybo_z7_bldc

--- projects/ultra96v2_udmabuf_sample/sim/tb_top.v
+++ projects/ultra96v2_udmabuf_sample/sim/tb_top.v
@@ -45,23 +45,25 @@ module tb_top();
     localparam  WB_DAT_WIDTH = (8 << WB_DAT_SIZE);
     localparam  WB_SEL_WIDTH = (1 << WB_DAT_SIZE);
     
-    wire                            wb_rst_i = i_top.wb_peri_rst_i;
-    wire                            wb_clk_i = i_top.wb_peri_clk_i;
+    // force connect to top-net
+    wire                            wb_rst_i = i_top.wb_rst_i;
+    wire                            wb_clk_i = i_top.wb_clk_i;
     reg     [WB_ADR_WIDTH-1:0]      wb_adr_o;
-    wire    [WB_DAT_WIDTH-1:0]      wb_dat_i = i_top.wb_peri_dat_o;
+    wire    [WB_DAT_WIDTH-1:0]      wb_dat_i = i_top.wb_dat_o;
     reg     [WB_DAT_WIDTH-1:0]      wb_dat_o;
     reg                             wb_we_o;
     reg     [WB_SEL_WIDTH-1:0]      wb_sel_o;
     reg                             wb_stb_o = 0;
-    wire                            wb_ack_i = i_top.wb_peri_ack_o;
+    wire                            wb_ack_i = i_top.wb_ack_o;
     
     initial begin
-        force i_top.wb_peri_adr_i = wb_adr_o;
-        force i_top.wb_peri_dat_i = wb_dat_o;
-        force i_top.wb_peri_we_i  = wb_we_o;
-        force i_top.wb_peri_sel_i = wb_sel_o;
-        force i_top.wb_peri_stb_i = wb_stb_o;
+        force i_top.wb_adr_i = wb_adr_o;
+        force i_top.wb_dat_i = wb_dat_o;
+        force i_top.wb_we_i  = wb_we_o;
+        force i_top.wb_sel_i = wb_sel_o;
+        force i_top.wb_stb_i = wb_stb_o;
     end
+    
     
     
     reg     [WB_DAT_WIDTH-1:0]      reg_wb_dat;
@@ -75,14 +77,14 @@ module tb_top();
     
     
     task wb_write(
-                input [WB_ADR_WIDTH+WB_DAT_SIZE-1:0]    adr,
-                input [WB_DAT_WIDTH-1:0]                dat,
-                input [WB_SEL_WIDTH-1:0]                sel
+                input [WB_ADR_WIDTH-1:0]    adr,
+                input [WB_DAT_WIDTH-1:0]    dat,
+                input [WB_SEL_WIDTH-1:0]    sel
             );
     begin
         $display("WISHBONE_WRITE(adr:%h dat:%h sel:%b)", adr, dat, sel);
         @(negedge wb_clk_i);
-            wb_adr_o = (adr >> WB_DAT_SIZE);
+            wb_adr_o = adr;
             wb_dat_o = dat;
             wb_sel_o = sel;
             wb_we_o  = 1'b1;
@@ -100,11 +102,11 @@ module tb_top();
     endtask
     
     task wb_read(
-                input [WB_ADR_WIDTH+WB_DAT_SIZE-1:0]    adr
+                input [WB_ADR_WIDTH-1:0]    adr
             );
     begin
         @(negedge wb_clk_i);
-            wb_adr_o = (adr >> WB_DAT_SIZE);
+            wb_adr_o = adr;
             wb_dat_o = {WB_DAT_WIDTH{1'bx}};
             wb_sel_o = {WB_SEL_WIDTH{1'b1}};
             wb_we_o  = 1'b0;
@@ -131,30 +133,8 @@ module tb_top();
     initial begin
     @(negedge wb_rst_i);
     #10000;
-        $display(" --- dma0 --- ");
+        $display(" --- cam0 imx --- ");
         wb_read (8*8); // CORE_ID
-        
-        wb_read (4*8);
-        wb_write(4*8, 64'h0123456789abcdef, 8'h0f); // ADR_WDATA0
-        wb_read (4*8);
-        wb_write(4*8, 64'h01234567FFFFFFFF, 8'hf0); // ADR_WDATA0
-        wb_read (4*8);
-        
-        $display("write start");
-        wb_write(4*8, 64'h0123456789abcdef, 8'hff); // ADR_WDATA0
-        wb_write(5*8, 64'hfedcba9876543210, 8'hff); // ADR_WDATA1
-        wb_write(3*8, 64'h0000_0100, 8'hff);    // ADR_ADDR
-        wb_write(1*8, 64'h0000_0001, 8'hff);    // ADR_WSTART
-    #10000;
-    
-        wb_write(3*8, 64'h0000_0100, 8'hff);    // ADR_ADDR
-        wb_write(2*8, 64'h0000_0001, 8'hff);    // ADR_RSTART
-        wb_read (0*8);    // ADR_STATUS
-        
-    #10000;
-        wb_read (0*8);    // ADR_STATUS
-        wb_read (6*8);    // ADR_RDATA0
-        wb_read (7*8);    // ADR_RDATA1
     
     
     #10000;
---
+++ projects/zybo_z7_bldc/sim/tb_sin_3phase.v
@@ -0,0 +1,67 @@
+// ---------------------------------------------------------------------------
+//
+//                                  Copyright (C) 2015-2020 by Ryuji Fuchikami
+//                                  http://ryuz.my.coocan.jp/
+// ---------------------------------------------------------------------------
+
+
+`timescale 1ns / 1ps
+`default_nettype none
+
+
+module tb_sin_3phase();
+    localparam RATE = 1000.0/125.0;
+    
+    initial begin
+        $dumpfile("tb_sin_3phase.vcd");
+        $dumpvars(0, tb_sin_3phase);
+        
+    #100000
+        $finish;
+    end
+    
+    reg     reset = 1'b1;
+    initial #(RATE*100) reset = 1'b0;
+    
+    reg     clk = 1'b1;
+    always #(RATE/2.0) clk = ~clk;
+    
+    wire            cke = 1;
+    
+    reg     [9:0]   in_phase = 0;
+    
+    wire    [11:0]  out_x;
+    wire    [11:0]  out_y;
+    wire    [11:0]  out_z;
+    
+    sin_3phase_tbl
+        i_sin_3phase_tbl
+            (
+                .reset      (reset),
+                .clk        (clk),
+                .cke        (cke),
+                
+                .in_phase   (in_phase),
+                
+                .out_x      (out_x),
+                .out_y      (out_y),
+                .out_z      (out_z)
+            );
+    
+    always @(posedge clk) begin
+        if ( reset ) begin
+            in_phase <= 0;
+        end
+        else if ( cke ) begin
+            in_phase <= in_phase + 1;
+        end
+    end
+    
+    
+endmodule
+
+
+`default_nettype wire
+
+
+// end of file
