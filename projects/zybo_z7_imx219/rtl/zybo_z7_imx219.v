9defd57421b6b01598e3c6ae6a0975cd238cfd7e
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2020-09-05 22:07:23 +0900

    update

--- projects/zybo_z7_imx219/rtl/zybo_z7_imx219.v
+++ projects/zybo_z7_imx219/rtl/zybo_z7_imx219.v
@@ -25,7 +25,7 @@ module zybo_z7_imx219
             input   wire    [3:0]   push_sw,
             input   wire    [3:0]   dip_sw,
             output  wire    [3:0]   led,
-            inout   wire    [7:0]   pmod_a,
+            output  wire    [7:0]   pmod_a,
             inout   wire    [7:0]   pmod_b,
             inout   wire    [7:0]   pmod_c,
             inout   wire    [7:0]   pmod_d,
