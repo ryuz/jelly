f31a25b766351355fc8557daf7524cd7756b1d76
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2020-12-09 20:58:36 +0900

    update app

--- projects/ultra96v2_imx219_display_port/app/main.cpp
+++ projects/ultra96v2_imx219_display_port/app/main.cpp
@@ -44,15 +44,55 @@ int main(int argc, char *argv[])
     int     aoi_y       = -1;
     bool    flip_h      = false;
     bool    flip_v      = false;
-    int     frame_rate  = 1000;
-    int     exposure    = 10;
+    int     frame_rate  = 60;
+    int     exposure    = 20;
     int     a_gain      = 20;
-    int     d_gain      = 10;
+    int     d_gain      = 1;
     int     bayer_phase = 1;
     int     view_scale  = 1;
-
+    int     view_x      = -1;
+    int     view_y      = -1;
+    bool    colmat_en   = false;
+    int     gamma       = 22;
+    int     gauss_level = 0;
+    int     canny_th    = 127;
+    int     diff_th     = 15;
+    cv::Mat imgBack;
+    
     for ( int i = 1; i < argc; ++i ) {
-        if ( strcmp(argv[i], "full") == 0 ) {
+        if ( strcmp(argv[i], "1000fps") == 0 ) {
+            pixel_clock = 139200000.0;
+            binning     = true;
+            width       = 640;
+            height      = 132;
+            aoi_x       = -1;
+            aoi_y       = -1;
+            flip_h      = false;
+            flip_v      = false;
+            frame_rate  = 1000;
+            exposure    = 1;
+            a_gain      = 20;
+            d_gain      = 10;
+            bayer_phase = 1;
+            view_scale  = 1;
+        }
+        else if ( strcmp(argv[i], "720p") == 0 ) {
+            pixel_clock = 91000000.0;
+            binning     = true;
+            width       = 1280;
+            height      = 720;
+            aoi_x       = -1;
+            aoi_y       = -1;
+            flip_h      = false;
+            flip_v      = false;
+            frame_rate  = 60;
+            exposure    = 20;
+            a_gain      = 20;
+            d_gain      = 0;
+            bayer_phase = 1;
+            view_scale  = 1;
+        }
+        else if ( strcmp(argv[i], "full") == 0 ) {
             pixel_clock = 91000000;
             binning    = false;
             width      = 3280;
@@ -64,22 +104,6 @@ int main(int argc, char *argv[])
             a_gain     = 20;
             d_gain     = 0;
             view_scale = 4;
-        }
-        else if ( strcmp(argv[i], "1000fps") == 0 ) {
-            pixel_clock = 139200000.0;
-            binning     = true;
-            width       = 640;
-            height      = 132;
-            aoi_x       = -1;
-            aoi_y       = -1;
-            flip_h      = false;
-            flip_v      = false;
-            frame_rate  = 1000;
-            exposure    = 10;
-            a_gain      = 20;
-            d_gain      = 10;
-            bayer_phase = 1;
-            view_scale  = 1;
         }
         else if ( strcmp(argv[i], "-pixel_clock") == 0 && i+1 < argc) {
             ++i;
@@ -129,12 +153,17 @@ int main(int argc, char *argv[])
             ++i;
             view_scale = strtol(argv[i], nullptr, 0);
         }
+        else if ( strcmp(argv[i], "-back_image") == 0 && i+1 < argc) {
+            ++i;
+            imgBack = cv::imread(argv[i]);
+            cv::resize(imgBack, imgBack, cv::Size(1280, 720));
+        }
         else {
             std::cout << "unknown option : " << argv[i] << std::endl;
             return 1;
         }
     }
-    
+
     width &= ~0xf;
     width  = std::max(width, 16);
     height = std::max(height, 2);
@@ -155,14 +184,6 @@ int main(int argc, char *argv[])
     std::cout << "udmabuf4 phys addr : 0x" << std::hex << dmabuf_addr << std::endl;
     std::cout << "udmabuf4 size      : 0x" << std::hex << dmabuf_size << std::endl;
 
-    auto img = cv::imread("test.jpg");
-    if ( !img.empty() ) {
-        cv::Mat imgView;
-        cv::resize(img, imgView, cv::Size(1920, 1080));
-        udmabuf_acc.WriteImage2d(3, 1920, 1080, imgView.data, 0);
-    }
-    
-
     // PL peripheral bus
     std::cout << "\nuio open" << std::endl;
     jelly::UioAccessor uio_acc("uio_pl_peri", 0x08000000);
@@ -175,11 +196,11 @@ int main(int argc, char *argv[])
     auto reg_fmtr    = uio_acc.GetAccessor(0x00100000);  // ビデオサイズ正規化
     auto reg_demos   = uio_acc.GetAccessor(0x00200000);  // デモザイク
     auto reg_colmat  = uio_acc.GetAccessor(0x00210000);  // カラーマトリックス
-//    auto reg_gamma   = uio_acc.GetAccessor(0x00220000);  // ガンマ補正
-//    auto reg_gauss   = uio_acc.GetAccessor(0x00240000);  // ガウシアンフィルタ
-//    auto reg_canny   = uio_acc.GetAccessor(0x00250000);  // Cannyフィルタ
-//    auto reg_imgdma  = uio_acc.GetAccessor(0x00260000);  // FIFO dma
-//    auto reg_bindiff = uio_acc.GetAccessor(0x00270000);  // 前画像との差分バイナライズ
+    auto reg_gamma   = uio_acc.GetAccessor(0x00220000);  // ガンマ補正
+    auto reg_gauss   = uio_acc.GetAccessor(0x00240000);  // ガウシアンフィルタ
+    auto reg_canny   = uio_acc.GetAccessor(0x00250000);  // Cannyフィルタ
+    auto reg_imgdma  = uio_acc.GetAccessor(0x00260000);  // FIFO dma
+    auto reg_bindiff = uio_acc.GetAccessor(0x00270000);  // 前画像との差分バイナライズ
     auto reg_sel     = uio_acc.GetAccessor(0x002f0000);  // 出力切り替え
     auto reg_bufmng  = uio_acc.GetAccessor(0x00300000);  // Buffer manager
     auto reg_bufalc  = uio_acc.GetAccessor(0x00310000);  // Buffer allocator
@@ -193,11 +214,11 @@ int main(int argc, char *argv[])
     std::cout << "fmtr    : " << std::hex << reg_fmtr.ReadReg(0) << std::endl;
     std::cout << "demos   : " << std::hex << reg_demos.ReadReg(0) << std::endl;
     std::cout << "colmat  : " << std::hex << reg_colmat.ReadReg(0) << std::endl;
- //   std::cout << "gamma   : " << std::hex << reg_gamma.ReadReg(0) << std::endl;
- //   std::cout << "gauss   : " << std::hex << reg_gauss.ReadReg(0) << std::endl;
- //   std::cout << "canny   : " << std::hex << reg_canny.ReadReg(0) << std::endl;
- //   std::cout << "imgdma  : " << std::hex << reg_imgdma.ReadReg(0) << std::endl;
- //   std::cout << "bindiff : " << std::hex << reg_bindiff.ReadReg(0) << std::endl;
+    std::cout << "gamma   : " << std::hex << reg_gamma.ReadReg(0) << std::endl;
+    std::cout << "gauss   : " << std::hex << reg_gauss.ReadReg(0) << std::endl;
+    std::cout << "canny   : " << std::hex << reg_canny.ReadReg(0) << std::endl;
+    std::cout << "imgdma  : " << std::hex << reg_imgdma.ReadReg(0) << std::endl;
+    std::cout << "bindiff : " << std::hex << reg_bindiff.ReadReg(0) << std::endl;
     std::cout << "sel     : " << std::hex << reg_sel.ReadReg(0) << std::endl;
     std::cout << "bufmng  : " << std::hex << reg_bufmng.ReadReg(0) << std::endl;
     std::cout << "bufalc  : " << std::hex << reg_bufalc.ReadReg(0) << std::endl;
@@ -209,6 +230,29 @@ int main(int argc, char *argv[])
     // DMA
     jelly::VideoDmaControl  vdmaw(reg_vdmaw, 3, 3, true);
     jelly::VideoDmaControl  vdmar(reg_vdmar, 3, 3, true);
+
+
+    // memmeap iamge fifo
+    jelly::UdmabufAccessor udmabuf5_acc("udmabuf5");
+    if ( !udmabuf5_acc.IsMapped() ) {
+        std::cout << "udmabuf5 : open error or mmap error" << std::endl;
+        return 1;
+    }
+
+    reg_imgdma.WriteReg(REG_DAM_FIFO_CTL_CONTROL, 0x0);
+    usleep(100);
+
+    auto fifobuf_phys_adr = udmabuf5_acc.GetPhysAddr();
+    auto fifobuf_mem_size = udmabuf5_acc.GetSize();
+    std::cout << "udmabuf5 phys addr : 0x" << std::hex << fifobuf_phys_adr << std::endl;
+    std::cout << "udmabuf5 size      : " << std::dec << fifobuf_mem_size << std::endl;
+    reg_imgdma.WriteReg(REG_DAM_FIFO_PARAM_ADDR,     fifobuf_phys_adr);
+    reg_imgdma.WriteReg(REG_DAM_FIFO_PARAM_SIZE,     fifobuf_mem_size);
+    reg_imgdma.WriteReg(REG_DAM_FIFO_PARAM_AWLEN,    0x0f);
+    reg_imgdma.WriteReg(REG_DAM_FIFO_PARAM_WTIMEOUT, 0xff);
+    reg_imgdma.WriteReg(REG_DAM_FIFO_PARAM_ARLEN,    0x0f);
+    reg_imgdma.WriteReg(REG_DAM_FIFO_PARAM_RTIMEOUT, 0xff);
+    reg_imgdma.WriteReg(REG_DAM_FIFO_CTL_CONTROL,    0x3);
 
 
     // DisplayPort 設定
@@ -234,6 +278,14 @@ int main(int argc, char *argv[])
     int h_start = dp_hstart-dp_hswidth-17;
     int v_start = dp_vstart-dp_vswidth-1;
 
+
+    // 背景書き込み
+    if ( imgBack.empty() ) {
+        imgBack = cv::Mat::zeros(360, 640, CV_8UC3);
+    }
+    cv::Mat imgView;
+    cv::resize(imgBack, imgView, cv::Size(dp_hres, dp_vres));
+    udmabuf_acc.WriteImage2d(3, dp_hres, dp_vres, imgView.data, 0);
 
 
     // カメラ電源ON
@@ -263,7 +315,7 @@ int main(int argc, char *argv[])
     // Camera DMA write start
     vdmaw.SetBufferAddr(dmabuf_addr);
     vdmaw.SetImageSize(width, height);
-    vdmaw.SetImageStep(1920*3);
+    vdmaw.SetImageStep(dp_hres*3);
     vdmaw.Start();
 
     // camera video format regularizer
@@ -279,14 +331,14 @@ int main(int argc, char *argv[])
 
     // DisplayPort DMA read start
     vdmar.SetBufferAddr(dmabuf_addr);
-    vdmar.SetImageSize(1920, 1080);
+    vdmar.SetImageSize(dp_hres, dp_vres);
     vdmar.Start();
 
 
 #if 1
     // VSync adjust de
-    reg_vsgen.WriteReg(REG_VIDEO_ADJDE_PARAM_HSIZE, dp_hres-1);
-    reg_vsgen.WriteReg(REG_VIDEO_ADJDE_PARAM_VSIZE, dp_vres-1);
+    reg_vsgen.WriteReg(REG_VIDEO_ADJDE_PARAM_HSIZE,  dp_hres-1);
+    reg_vsgen.WriteReg(REG_VIDEO_ADJDE_PARAM_VSIZE,  dp_vres-1);
     reg_vsgen.WriteReg(REG_VIDEO_ADJDE_PARAM_HSTART, v_start);
     reg_vsgen.WriteReg(REG_VIDEO_ADJDE_PARAM_VSTART, dp_vstart-1);
     reg_vsgen.WriteReg(REG_VIDEO_ADJDE_PARAM_HPOL,   (dp_polarity & 1) ? 1 : 0);
@@ -309,41 +361,92 @@ int main(int argc, char *argv[])
     reg_vsgen.WriteReg(REG_VIDEO_VSGEN_CTL_CONTROL,          1);
 #endif
 
+    cv::imshow("DisplayPort", imgBack);
+    
     int key;
+    int gamma_prev = 0;
     while ( (key = cv::waitKey(10)) != 0x1b ) {
         cv::Mat img(height, width, CV_8UC3);
         udmabuf_acc.ReadImage2d(3, width, height, img.data, 0, 0, 0, 0, 1920*3);
-        cv::imshow("img", img);
+        cv::imshow("Camera", img);
+        cv::createTrackbar("fps",       "Camera", &frame_rate, 1000);
+        cv::createTrackbar("exposure",  "Camera", &exposure, 1000);
+        cv::createTrackbar("a_gain",    "Camera", &a_gain, 20);
+        cv::createTrackbar("d_gain",    "Camera", &d_gain, 24);
+        cv::createTrackbar("bayer",     "Camera", &bayer_phase, 3);
+        cv::createTrackbar("gamm" ,     "Camera", &gamma, 30);
+        cv::createTrackbar("gauss" ,    "Camera", &gauss_level, 3);
+        cv::createTrackbar("canny_th" , "Camera", &canny_th, 255);
+        cv::createTrackbar("diff_th" ,  "Camera", &diff_th, 255);
 
-//      cv::createTrackbar("h", "img", &h_start, 200);
-//      cv::createTrackbar("v", "img", &v_start, 100);
-
-        cv::createTrackbar("fps",      "img", &frame_rate, 1000);
-        cv::createTrackbar("phase",    "img", &bayer_phase, 3);
-        cv::createTrackbar("exposure", "img", &exposure, 1000);
-        cv::createTrackbar("a_gain",   "img", &a_gain, 20);
-        cv::createTrackbar("d_gain",   "img", &d_gain, 24);
-
-
+        // imx219
         imx219.SetFrameRate(frame_rate);
         imx219.SetExposureTime(exposure / 1000.0);
         imx219.SetGain(a_gain);
         imx219.SetDigitalGain(d_gain);
         imx219.SetFlip(flip_h, flip_v);
 
+        // demosaic
+        reg_demos.WriteReg(REG_IMG_DEMOSAIC_PARAM_PHASE, bayer_phase);
+        reg_demos.WriteReg(REG_IMG_DEMOSAIC_CTL_CONTROL, 3);
+
+        // gamma
+        gamma = std::max(1, gamma);
+        if ( gamma != gamma_prev ) {
+            float g = gamma / 10.0f;
+            int gamma_tbl_addr  = (int)reg_gamma.ReadReg(REG_IMG_GAMMA_CFG_TBL_ADDR);
+            int gamma_tbl_size  = (int)reg_gamma.ReadReg(REG_IMG_GAMMA_CFG_TBL_SIZE);
+            int gamma_tbl_width = (int)reg_gamma.ReadReg(REG_IMG_GAMMA_CFG_TBL_WIDTH);
+            for ( int i = 0; i < gamma_tbl_size; ++i) {
+                int v = (int)(std::pow((float)i / (float)(gamma_tbl_size-1), 1.0f/g)*((1<<gamma_tbl_width)-1));
+                for ( int c = 0; c < 3; ++c) {
+                    reg_gamma.WriteReg(gamma_tbl_addr*(c+1)+i, v);
+                }
+            }
+            reg_gamma.WriteReg(REG_IMG_GAMMA_PARAM_ENABLE, 0x7);
+            reg_gamma.WriteReg(REG_IMG_GAMMA_CTL_CONTROL, 0x3);
+
+            gamma_prev = gamma;
+        }
+
+        // canny
+        reg_canny.WriteReg(REG_IMG_CANNY_PARAM_TH, canny_th*canny_th);
+        reg_canny.WriteReg(REG_IMG_CANNY_CTL_CONTROL, 0x3);
+
+        // diff binarize
+        reg_bindiff.WriteReg(REG_IMG_BINARIZER_PARAM_TH, diff_th);
+        reg_bindiff.WriteReg(REG_IMG_BINARIZER_CTL_CONTROL, 0x3);
+        
+
+        // display port
+        cv::createTrackbar("h", "DisplayPort", &h_start, 200);
+        cv::createTrackbar("v", "DisplayPort", &v_start, 100);
         reg_vsgen.WriteReg(REG_VIDEO_ADJDE_PARAM_HSTART, h_start);
         reg_vsgen.WriteReg(REG_VIDEO_ADJDE_PARAM_VSTART, v_start);
         reg_vsgen.WriteReg(REG_VIDEO_ADJDE_CTL_CONTROL, 3);
 
-        reg_demos.WriteReg(REG_IMG_DEMOSAIC_PARAM_PHASE, bayer_phase);
-        reg_demos.WriteReg(REG_IMG_DEMOSAIC_CTL_CONTROL, 3);
-
         switch ( key ) {
         case 'h':   flip_h = !flip_h; bayer_phase ^= 1; break;
         case 'v':   flip_v = !flip_v; bayer_phase ^= 2; break;
+
+        case 'd':   cv::imwrite("dump.png", img); break;
+
+        case '0':   reg_sel.WriteReg(REG_IMG_SELECTOR_CTL_SELECT, 0); break;
+        case '1':   reg_sel.WriteReg(REG_IMG_SELECTOR_CTL_SELECT, 1); break;
+        case '2':   reg_sel.WriteReg(REG_IMG_SELECTOR_CTL_SELECT, 2); break;
+        case '3':   reg_sel.WriteReg(REG_IMG_SELECTOR_CTL_SELECT, 3); break;
+        case '4':   reg_sel.WriteReg(REG_IMG_SELECTOR_CTL_SELECT, 4); break;
+        case '5':   reg_sel.WriteReg(REG_IMG_SELECTOR_CTL_SELECT, 5); break;
+        case '6':   reg_sel.WriteReg(REG_IMG_SELECTOR_CTL_SELECT, 6); break;
+        case '7':   reg_sel.WriteReg(REG_IMG_SELECTOR_CTL_SELECT, 7); break;
+        case '8':   reg_sel.WriteReg(REG_IMG_SELECTOR_CTL_SELECT, 8); break;
+        case '9':   reg_sel.WriteReg(REG_IMG_SELECTOR_CTL_SELECT, 9); break;
         }
     }
     
+    // FIFO 停止
+    reg_imgdma.WriteReg(REG_DAM_FIFO_CTL_CONTROL, 0x0);
+    usleep(100);
 
     // DMA 停止
     vdmaw.Stop();
