7a0cf675d5d238768f66ff154810da7827d13497
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-11-09 18:49:34 +0900

    update tcl

--- projects/ultra96v2_udmabuf_sample/syn/tcl/create_project.tcl
+++ projects/ultra96v2_udmabuf_sample/syn/tcl/create_project.tcl
@@ -1,3 +1,4 @@
+# project setting
 puts [file dirname [info script]]
 
 set project_directory   [file dirname [info script]]
@@ -6,31 +7,56 @@ set project_name        "ultra96v2_udmabuf_sample"
 cd $project_directory
 
 
+# add borad repository path
+set boardRepoPathsLin [concat [file join $::env(HOME) {.Xilinx/Vivado/} [version -short] {xhub/board_store}] [get_param board.repoPaths]]
+set boardRepoPathsWin [concat [file join $::env(APPDATA) {Xilinx/Vivado/} [version -short] {xhub/board_store}] [get_param board.repoPaths]]
+if { [ file exists $boardRepoPathsLin ] == 1 } then {
+    set_param board.repoPaths $boardRepoPathsLin
+} elseif { [ file exists $boardRepoPathsWin ] == 1 } then {
+    set_param board.repoPaths $boardRepoPathsWin
+}
+
+
+# version
 set current_vivado_version [version -short]
 if       { [string first "2019.2" $current_vivado_version] != -1 } {
-    set vivado_version "vivado2019.2"
+    set vivado_version   "vivado2019.2"
+    set synth_1_flow     "Vivado Synthesis 2019"
+    set synth_1_strategy "Vivado Synthesis Defaults"
+    set impl_1_flow      "Vivado Implementation 2019"
+    set impl_1_strategy  "Vivado Implementation Defaults"
 } elseif { [string first "2020.1" $current_vivado_version] != -1 } {
-    set vivado_version "vivado2020.1"
-} elseif { [string first "2020.1" $current_vivado_version] != -1 } {
-    set vivado_version "vivado2021.2"
+    set vivado_version   "vivado2020.1"
+    set synth_1_flow     "Vivado Synthesis 2020"
+    set synth_1_strategy "Vivado Synthesis Defaults"
+    set impl_1_flow      "Vivado Implementation 2020"
+    set impl_1_strategy  "Vivado Implementation Defaults"
+} elseif { [string first "2021.2" $current_vivado_version] != -1 } {
+    set vivado_version   "vivado2021.2"
+    set synth_1_flow     "Vivado Synthesis 2021"
+    set synth_1_strategy "Vivado Synthesis Defaults"
+    set impl_1_flow      "Vivado Implementation 2021"
+    set impl_1_strategy  "Vivado Implementation Defaults"
 } else {
     puts "ERROR: mismatch vivado version."
     return 1
 }
 
+
+# create project
 create_project -force $project_name $project_directory
 
 #set board_part "avnet.com:ultra96v2:part0:1.1"
 set board_part [get_board_parts -quiet -latest_file_version "avnet.com:ultra96v2*"]
+set device_part "xczu3eg-sbva484-1-e"
 puts $board_part
-set device_parts "xczu3eg-sbva484-1-e"
 
-set     design_bd_tcl_file  [file join ".." $vivado_version "design_1.tcl"  ]
-set     design_pin_xdc_file [file join $project_directory "../../constrain/xdc/ultra96v2_udmabuf_sample.xdc" ]
-lappend ip_repo_path_list   [file join $project_directory ".." ".." ".." ".." ".." ".." "PTTY_AXI" "target" "xilinx" "ip"]
-lappend ip_repo_path_list   [file join $project_directory ".." ".." ".." ".." ".." ".." "LED_AXI"  "target" "xilinx" "ip"]
-lappend ip_repo_path_list   [file join $project_directory ".." ".." "ip"]
-puts $design_pin_xdc_file
+#set     design_bd_tcl_file  [file join ".." $vivado_version "design_1.tcl"  ]
+#set     design_pin_xdc_file [file join $project_directory "../../constrain/xdc/ultra96v2_udmabuf_sample.xdc" ]
+#puts $design_pin_xdc_file
+
+set_property "part"           $device_part     [current_project]
+set_property "board_part"     $board_part      [current_project]
 
 if       {[info exists board_part ] && [string equal $board_part  "" ] == 0} {
     set_property "board_part"     $board_part      [current_project]
@@ -45,30 +71,22 @@ set_property "default_lib"        "xil_defaultlib" [current_project]
 set_property "simulator_language" "Mixed"          [current_project]
 set_property "target_language"    "verilog"        [current_project]
 
-#
 # Create fileset "sources_1"
-#
 if {[string equal [get_filesets -quiet sources_1] ""]} {
     create_fileset -srcset sources_1
 }
-#
+
 # Create fileset "constrs_1"
-#
 if {[string equal [get_filesets -quiet constrs_1] ""]} {
     create_fileset -constrset constrs_1
 }
-#
+
 # Create fileset "sim_1"
-#
 if {[string equal [get_filesets -quiet sim_1] ""]} {
     create_fileset -simset sim_1
 }
 
-#
-# Create run "synth_1" and set property
-#
-set synth_1_flow     "Vivado Synthesis 2019"
-set synth_1_strategy "Vivado Synthesis Defaults"
+# create run "synth_1" and set property
 if {[string equal [get_runs -quiet synth_1] ""]} {
     create_run -name synth_1 -flow $synth_1_flow -strategy $synth_1_strategy -constrset constrs_1
 } else {
@@ -76,11 +94,8 @@ if {[string equal [get_runs -quiet synth_1] ""]} {
     set_property strategy $synth_1_strategy [get_runs synth_1]
 }
 current_run -synthesis [get_runs synth_1]
-#
-# Create run "impl_1" and set property
-#
-set impl_1_flow      "Vivado Implementation 2019"
-set impl_1_strategy  "Vivado Implementation Defaults"
+
+# create run "impl_1" and set property
 if {[string equal [get_runs -quiet impl_1] ""]} {
     create_run -name impl_1 -flow $impl_1_flow -strategy $impl_1_strategy -constrset constrs_1 -parent_run synth_1
 } else {
@@ -88,45 +103,25 @@ if {[string equal [get_runs -quiet impl_1] ""]} {
     set_property strategy $impl_1_strategy  [get_runs impl_1]
 }
 current_run -implementation [get_runs impl_1]
-#
-# Set IP Repository
-#
-if {[info exists ip_repo_path_list] && [llength $ip_repo_path_list] > 0 } {
-    set_property ip_repo_paths $ip_repo_path_list [current_fileset]
-    update_ip_catalog
-}
 
-#
-# Create block design
-#
-if {[info exists design_bd_tcl_file]} {
-    # 
-    # Read block design file
-    #
-    source $design_bd_tcl_file
-    #
-    # Save block design
-    #
-    regenerate_bd_layout
-    save_bd_design
-    #
-    # Generate wrapper files
-    #
-    set design_bd_name  [get_bd_designs]
-#   make_wrapper -files [get_files $design_bd_name.bd] -top -import
-}
-#
+# Set IP Repository
+#if {[info exists ip_repo_path_list] && [llength $ip_repo_path_list] > 0 } {
+#    set_property ip_repo_paths $ip_repo_path_list [current_fileset]
+#    update_ip_catalog
+#}
+
+
+# create block design
+source [file join ".." $vivado_version "design_1.tcl"  ]
+regenerate_bd_layout
+save_bd_design
+    
+
 # Import timing files
-#
-if {[info exists design_timing_xdc_file]} {
-    add_files    -fileset constrs_1 -norecurse $design_timing_xdc_file
-}
-#
-# Import pin files
-#
-if {[info exists design_pin_xdc_file]} {
-    add_files    -fileset constrs_1 -norecurse $design_pin_xdc_file
-}
+#if {[info exists design_timing_xdc_file]} {
+#    add_files    -fileset constrs_1 -norecurse $design_timing_xdc_file
+#}
+
 
 proc add_verilog_file {fileset_name library_name file_name} {
     set file    [file normalize $file_name]
@@ -137,12 +132,19 @@ proc add_verilog_file {fileset_name library_name file_name} {
     set_property "library"   $library_name $file_obj
 }
 
+# add source file
 add_verilog_file sources_1 WORK ../../rtl/ultra96v2_udmabuf_sample.v
 add_verilog_file sources_1 WORK ../../rtl/test_dma.v
 add_verilog_file sources_1 WORK ../../rtl/test_dma_core.v
 add_verilog_file sources_1 WORK ../../../../rtl/bus/jelly_axi4l_to_wishbone.v
 add_verilog_file sources_1 WORK ../../../../rtl/peripheral/jelly_interval_timer.v
 
-
 set_property top ultra96v2_udmabuf_sample [current_fileset]
+
+
+# add constrain file
+add_files    -fileset constrs_1 -norecurse "../../constrain/xdc/ultra96v2_udmabuf_sample.xdc"
+add_files    -fileset constrs_1 -norecurse "../../constrain/xdc/debug.xdc"
+set_property target_constrs_file "../../constrain/xdc/debug.xdc" [current_fileset -constrset]
+
 
