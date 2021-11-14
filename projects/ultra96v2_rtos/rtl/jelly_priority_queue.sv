5beb7c65d6c65b59db337c23a7099725c061b3ce
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-11-14 20:25:13 +0900

    update

--- projects/ultra96v2_rtos/rtl/jelly_priority_queue.sv
+++ projects/ultra96v2_rtos/rtl/jelly_priority_queue.sv
@@ -38,78 +38,94 @@ module jelly_priority_queue
 
     
     typedef struct packed {
-        logic                    move;
-        logic                    sort;
         logic   [ID_WIDTH-1:0]   id;
         logic   [PRI_WIDTH-1:0]  pri;
         logic                    valid;
     } object_t;
     
+    typedef enum {
+        stay,
+        forward,
+        backward,
+        insert
+    } flag_t;
+
+    object_t                    in_obj;
+    assign in_obj.id    = in_id;
+    assign in_obj.pri   = in_pri;
+    assign in_obj.valid = in_valid;
 
     logic       [N_WIDTH-1:0]   reg_size;
+    flag_t      [N-1:0]         reg_flag;
+    object_t                    reg_obj;
     object_t    [N-1:0]         reg_array;
-    object_t    [N:0]           array;
-    always_comb begin : blk_array
-        automatic object_t tmp_obj = 'x;
 
-        array[N-1:0] = reg_array;
-        array[N]     = '0;
+    object_t    [N-1:0]         array;
+    logic       [N-1:0]         compare;
+
+    always_comb begin : blk_array
+        // array
+        automatic object_t  [N:0] tmp_array;
+        tmp_array[N-1:0] = reg_array;
+        tmp_array[N]     = '0;
         for ( int i = 0; i < N; ++i ) begin
-            if ( array[i].move ) begin
-                array[i] = array[i+1];
-            end
+            case ( reg_flag[i] )
+            stay:       array[i] = tmp_array[i];
+            forward:    array[i] = tmp_array[i+1];
+            backward:   array[i] = tmp_array[i-1];
+            insert:     array[i] = reg_obj;
+            endcase
         end
 
+        // priority
         for ( int i = 0; i < N; ++i ) begin
-            if ( array[i].sort && array[i+1].valid && array[i].pri >= array[i+1].pri ) begin
-                tmp_obj    = array[i+1];
-                array[i+1] = array[i];
-                array[i]   = tmp_obj;
-            end
-            else begin
-                array[i].sort = 1'b0;
-            end
+            compare[i] = (in_pri < array[i].pri || !array[i].valid);
         end
     end
 
+    
     always_ff @(posedge clk) begin
         if ( reset ) begin
             reg_size  <= '0;
             for ( int i = 0; i < N; ++i ) begin
-                reg_array[i].move  <= 1'b0;
-                reg_array[i].sort  <= 1'b0;
-                reg_array[i].id    <= 'x;
-                reg_array[i].pri   <= 'x;
-                reg_array[i].valid <= 1'b0;
+                reg_flag[i]  <= stay;
+                reg_array[i] <= '{id: 'x, pri: 'x, valid: 1'b0};
             end
         end
         else if ( cke ) begin
-            reg_array[N-1:0] <= array[N-1:0];
+            for ( int i = 0; i < N; ++i ) begin
+                reg_flag[i]  <= stay;
+                reg_array[i] <= array[i];
+            end
 
+            reg_obj <= in_obj;
             if ( in_valid ) begin
                 if (in_op == 1'b0) begin    // add
-                    automatic   object_t    obj;
-
                     reg_size <= reg_size + 1'b1;
-                    for ( int i = N-1; i > 1; --i ) begin
-                        reg_array[i] <= reg_array[i-1];
+
+                    for ( int i = 0; i < N; ++i ) begin
+                        if ( compare[i] ) begin
+                            if ( i == 0 ) begin
+                                reg_flag[i] <= insert;
+                            end
+                            else begin
+                                reg_flag[i] <= compare[i-1] ? backward : insert;
+                            end
+                        end
                     end
 
-                    obj.move  = 1'b0;
-                    obj.sort  = 1'b1;
-                    obj.id    = in_id;
-                    obj.pri   = in_pri;
-                    obj.valid = 1'b1;
-                    if ( reg_array[0].valid && obj.pri >= reg_array[0].pri ) begin
-                        reg_array[1] <= obj;
+                    /*
+                    if ( compare[0] ) begin
+                        reg_array[0] <= in_obj;
+                        reg_obj      <= reg_array[0];
+                        if ( N > 1 ) begin
+                            reg_flag[1] <= insert;
+                        end
                     end
-                    else begin
-                        obj.sort = 1'b0;
-                        reg_array[1] <= reg_array[0];
-                        reg_array[0] <= obj;
-                    end
+                    */
                 end
-                else if ( in_op == 1'b1 ) begin    // del
+
+                if (in_op == 1'b1) begin    // delete
                     automatic bit   del_flag;
 
                     reg_size <= reg_size - 1'b1;
@@ -117,27 +133,22 @@ module jelly_priority_queue
                     for ( int i = 0; i < N; ++i ) begin
                         if ( array[i].id == in_id ) begin
                             del_flag = 1'b1;
-                            reg_array[i].move  <= 1'b0;
-                            reg_array[i].sort  <= 1'b0;
-                            reg_array[i].id    <= 'x;
-                            reg_array[i].pri   <= 'x;
-                            reg_array[i].valid <= 1'b0;
                         end
 
                         if ( del_flag ) begin
-                            if ( i < 2 ) begin
-                                reg_array[i] <= array[i+1];
-                            end
-                            else begin
-                                reg_array[i].move <= 1'b1;
-                            end
+                            reg_flag[i] <= forward;
                         end
+                    end
+
+                    if ( array[0].id == in_id ) begin
+                        reg_array[0] <= array[1];
+                        reg_flag[0]  <= stay;
                     end
                 end
             end
         end
     end
-
+    
     assign top_id    = array[0].id;
     assign top_pri   = array[0].pri;
     assign top_valid = array[0].valid;
