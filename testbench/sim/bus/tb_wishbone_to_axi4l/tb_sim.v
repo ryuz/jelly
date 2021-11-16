df08ed8caa48452253c3c2129aee04e20daf359c
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-11-16 20:57:44 +0900

    add tb_wishbone_to_axi4l

---
+++ testbench/sim/bus/tb_wishbone_to_axi4l/tb_sim.v
@@ -0,0 +1,184 @@
+
+`timescale 1ns / 1ps
+`default_nettype none
+
+
+module tb_sim();
+
+    // -----------------------------
+    //  simulation setting
+    // -----------------------------
+
+    initial begin
+        $dumpfile("tb_sim.vcd");
+        $dumpvars(0, tb_sim);
+        
+    #2000000
+        $finish();
+    end
+
+    localparam RATE    = 10.0;
+
+    reg     reset = 1'b1;
+    always #(RATE*100)  reset = 1'b0;
+
+    reg     clk = 1'b1;
+    always #(RATE/2.0)  clk = ~clk;
+
+
+    // -----------------------------
+    //  simulation target
+    // -----------------------------
+
+    parameter WB_ADR_WIDTH = 30;
+    parameter WB_DAT_SIZE  = 2;
+    parameter WB_DAT_WIDTH = (8 << WB_DAT_SIZE);
+    parameter WB_SEL_WIDTH = WB_DAT_WIDTH / 8;
+
+    wire                        s_wb_rst_i = reset;
+    wire                        s_wb_clk_i;
+    wire    [WB_ADR_WIDTH-1:0]  s_wb_adr_i;
+    wire    [WB_DAT_WIDTH-1:0]  s_wb_dat_o;
+    wire    [WB_DAT_WIDTH-1:0]  s_wb_dat_i;
+    wire    [WB_SEL_WIDTH-1:0]  s_wb_sel_i;
+    wire                        s_wb_we_i;
+    wire                        s_wb_stb_i;
+    wire                        s_wb_ack_o;
+
+    tb_sim_main
+            #(
+                .WB_ADR_WIDTH       (WB_ADR_WIDTH),
+                .WB_DAT_SIZE        (WB_DAT_SIZE),
+                .WB_DAT_WIDTH       (WB_DAT_WIDTH),
+                .WB_SEL_WIDTH       (WB_SEL_WIDTH)
+            )
+        i_sim_main
+            (
+                .s_wb_rst_i         (s_wb_rst_i),
+                .s_wb_clk_i         (s_wb_clk_i),
+                .s_wb_adr_i         (s_wb_adr_i),
+                .s_wb_dat_o         (s_wb_dat_o),
+                .s_wb_dat_i         (s_wb_dat_i),
+                .s_wb_sel_i         (s_wb_sel_i),
+                .s_wb_we_i          (s_wb_we_i ),
+                .s_wb_stb_i         (s_wb_stb_i),
+                .s_wb_ack_o         (s_wb_ack_o)
+            );
+    
+    
+    // ----------------------------------
+    //  WISHBONE master
+    // ----------------------------------
+    
+    // force connect to top-net
+    wire                            wb_rst_i = s_wb_rst_i;
+    wire                            wb_clk_i = s_wb_clk_i;
+    reg     [WB_ADR_WIDTH-1:0]      wb_adr_o;
+    wire    [WB_DAT_WIDTH-1:0]      wb_dat_i = s_wb_dat_i;
+    reg     [WB_DAT_WIDTH-1:0]      wb_dat_o;
+    reg                             wb_we_o;
+    reg     [WB_SEL_WIDTH-1:0]      wb_sel_o;
+    reg                             wb_stb_o = 0;
+    wire                            wb_ack_i = s_wb_ack_o;
+    
+    assign s_wb_adr_i = wb_adr_o;
+    assign s_wb_dat_i = wb_dat_o;
+    assign s_wb_we_i  = wb_we_o;
+    assign s_wb_sel_i = wb_sel_o;
+    assign s_wb_stb_i = wb_stb_o;
+    
+    
+    
+    reg     [WB_DAT_WIDTH-1:0]      reg_wb_dat;
+    reg                             reg_wb_ack;
+    always @(posedge wb_clk_i) begin
+        if ( ~wb_we_o & wb_stb_o & wb_ack_i ) begin
+            reg_wb_dat <= wb_dat_i;
+        end
+        reg_wb_ack <= wb_ack_i;
+    end
+    
+    
+    task wb_write(
+                input [WB_ADR_WIDTH-1:0]    adr,
+                input [WB_DAT_WIDTH-1:0]    dat,
+                input [WB_SEL_WIDTH-1:0]    sel
+            );
+    begin
+        $display("WISHBONE_WRITE(adr:%h dat:%h sel:%b)", adr, dat, sel);
+        @(negedge wb_clk_i);
+            wb_adr_o = adr;
+            wb_dat_o = dat;
+            wb_sel_o = sel;
+            wb_we_o  = 1'b1;
+            wb_stb_o = 1'b1;
+        @(negedge wb_clk_i);
+            while ( reg_wb_ack == 1'b0 ) begin
+                @(negedge wb_clk_i);
+            end
+            wb_adr_o = {WB_ADR_WIDTH{1'bx}};
+            wb_dat_o = {WB_DAT_WIDTH{1'bx}};
+            wb_sel_o = {WB_SEL_WIDTH{1'bx}};
+            wb_we_o  = 1'bx;
+            wb_stb_o = 1'b0;
+    end
+    endtask
+    
+    task wb_read(
+                input [WB_ADR_WIDTH-1:0]    adr
+            );
+    begin
+        @(negedge wb_clk_i);
+            wb_adr_o = adr;
+            wb_dat_o = {WB_DAT_WIDTH{1'bx}};
+            wb_sel_o = {WB_SEL_WIDTH{1'b1}};
+            wb_we_o  = 1'b0;
+            wb_stb_o = 1'b1;
+        @(negedge wb_clk_i);
+            while ( reg_wb_ack == 1'b0 ) begin
+                @(negedge wb_clk_i);
+            end
+            wb_adr_o = {WB_ADR_WIDTH{1'bx}};
+            wb_dat_o = {WB_DAT_WIDTH{1'bx}};
+            wb_sel_o = {WB_SEL_WIDTH{1'bx}};
+            wb_we_o  = 1'bx;
+            wb_stb_o = 1'b0;
+            $display("WISHBONE_READ(adr:%h dat:%h)", adr, reg_wb_dat);
+    end
+    endtask
+    
+    
+    initial begin
+    #1000;
+        $display(" --- read test --- ");
+        wb_read (30'h00000001);
+        wb_read (30'h00000002);
+        wb_read (30'h00000003);
+        wb_read (30'h00000004);
+        wb_read (30'h11111111);
+        wb_read (30'h22222222);
+        wb_read (30'h12345678);
+        
+        $display(" --- write test --- ");
+        wb_write(30'h00000001, 32'h00000001, 8'hff);
+        wb_write(30'h00000002, 32'haaaaaaaa, 8'hf0);
+        wb_write(30'h00000003, 32'h55555555, 8'h0f);
+        wb_write(30'h00000004, 32'h12345678, 8'h12);
+        wb_write(30'h11111111, 32'h87654321, 8'ha5);
+        wb_write(30'h22222222, 32'haaaa5555, 8'h5a);
+        wb_write(30'h12345678, 32'h5555aaaa, 8'hff);
+        
+    #2000;
+        
+        $finish();
+    end
+    
+    
+    
+endmodule
+
+
+`default_nettype wire
+
+
+// end of file
---
+++ testbench/sim/bus/tb_wishbone_to_axi4l/tb_sim_main.v
@@ -0,0 +1,163 @@
+
+`timescale 1ns / 1ps
+`default_nettype none
+
+
+module tb_sim_main
+        #(
+            parameter WB_ADR_WIDTH = 30,
+            parameter WB_DAT_SIZE  = 2,
+            parameter WB_DAT_WIDTH = (8 << WB_DAT_SIZE),
+            parameter WB_SEL_WIDTH = WB_DAT_WIDTH / 8
+        )
+        (
+            input   wire                        s_wb_rst_i,
+            input   wire                        s_wb_clk_i,
+            input   wire    [WB_ADR_WIDTH-1:0]  s_wb_adr_i,
+            output  wire    [WB_DAT_WIDTH-1:0]  s_wb_dat_o,
+            input   wire    [WB_DAT_WIDTH-1:0]  s_wb_dat_i,
+            input   wire    [WB_SEL_WIDTH-1:0]  s_wb_sel_i,
+            input   wire                        s_wb_we_i,
+            input   wire                        s_wb_stb_i,
+            output  wire                        s_wb_ack_o
+        );
+    
+    parameter   AXI4L_ADDR_WIDTH = WB_ADR_WIDTH + WB_DAT_SIZE;
+    parameter   AXI4L_DATA_SIZE  = WB_DAT_SIZE;
+    parameter   AXI4L_DATA_WIDTH = WB_DAT_WIDTH;
+    parameter   AXI4L_STRB_WIDTH = WB_SEL_WIDTH;
+
+    wire                            axi4l_aresetn;
+    wire                            axi4l_aclk;
+    wire    [AXI4L_ADDR_WIDTH-1:0]  axi4l_awaddr;
+    wire    [2:0]                   axi4l_awprot;
+    wire                            axi4l_awvalid;
+    wire                            axi4l_awready;
+    wire    [AXI4L_STRB_WIDTH-1:0]  axi4l_wstrb;
+    wire    [AXI4L_DATA_WIDTH-1:0]  axi4l_wdata;
+    wire                            axi4l_wvalid;
+    wire                            axi4l_wready;
+    wire    [1:0]                   axi4l_bresp;
+    wire                            axi4l_bvalid;
+    wire                            axi4l_bready;
+    wire    [AXI4L_ADDR_WIDTH-1:0]  axi4l_araddr;
+    wire    [2:0]                   axi4l_arprot;
+    wire                            axi4l_arvalid;
+    wire                            axi4l_arready;
+    wire    [AXI4L_DATA_WIDTH-1:0]  axi4l_rdata;
+    wire    [1:0]                   axi4l_rresp;
+    wire                            axi4l_rvalid;
+    wire                            axi4l_rready;
+    
+
+    // WISHBONE => AXI4Lite converter
+    jelly_wishbone_to_axi4l
+            #(
+                .WB_ADR_WIDTH           (WB_ADR_WIDTH),
+                .WB_DAT_SIZE            (WB_DAT_SIZE),
+                .WB_DAT_WIDTH           (WB_DAT_WIDTH),
+                .WB_SEL_WIDTH           (WB_SEL_WIDTH),
+                .AXI4L_ADDR_WIDTH       (AXI4L_ADDR_WIDTH),
+                .AXI4L_DATA_WIDTH       (AXI4L_DATA_WIDTH),
+                .AXI4L_STRB_WIDTH       (AXI4L_STRB_WIDTH)
+            )
+        i_jelly_wishbone_to_axi4l
+            (
+                .s_wb_rst_i             (s_wb_rst_i),
+                .s_wb_clk_i             (s_wb_clk_i),
+                .s_wb_adr_i             (s_wb_adr_i),
+                .s_wb_dat_o             (s_wb_dat_o),
+                .s_wb_dat_i             (s_wb_dat_i),
+                .s_wb_sel_i             (s_wb_sel_i),
+                .s_wb_we_i              (s_wb_we_i ),
+                .s_wb_stb_i             (s_wb_stb_i),
+                .s_wb_ack_o             (s_wb_ack_o),
+                
+                .m_axi4l_aresetn        (axi4l_aresetn),
+                .m_axi4l_aclk           (axi4l_aclk),
+                .m_axi4l_awaddr         (axi4l_awaddr),
+                .m_axi4l_awprot         (axi4l_awprot ),
+                .m_axi4l_awvalid        (axi4l_awvalid),
+                .m_axi4l_awready        (axi4l_awready),
+                .m_axi4l_wstrb          (axi4l_wstrb),
+                .m_axi4l_wdata          (axi4l_wdata),
+                .m_axi4l_wvalid         (axi4l_wvalid),
+                .m_axi4l_wready         (axi4l_wready),
+                .m_axi4l_bresp          (axi4l_bresp),
+                .m_axi4l_bvalid         (axi4l_bvalid),
+                .m_axi4l_bready         (axi4l_bready),
+                .m_axi4l_araddr         (axi4l_araddr),
+                .m_axi4l_arprot         (axi4l_arprot),
+                .m_axi4l_arvalid        (axi4l_arvalid),
+                .m_axi4l_arready        (axi4l_arready),
+                .m_axi4l_rdata          (axi4l_rdata),
+                .m_axi4l_rresp          (axi4l_rresp),
+                .m_axi4l_rvalid         (axi4l_rvalid),
+                .m_axi4l_rready         (axi4l_rready)
+            );
+
+
+    wire                            m_wb_rst_o;
+    wire                            m_wb_clk_o;
+    wire    [WB_ADR_WIDTH-1:0]      m_wb_adr_o;
+    wire    [WB_DAT_WIDTH-1:0]      m_wb_dat_o;
+    wire    [WB_DAT_WIDTH-1:0]      m_wb_dat_i;
+    wire                            m_wb_we_o;
+    wire    [WB_SEL_WIDTH-1:0]      m_wb_sel_o;
+    wire                            m_wb_stb_o;
+    wire                            m_wb_ack_i;
+    jelly_axi4l_to_wishbone
+            #(
+                .AXI4L_ADDR_WIDTH       (AXI4L_ADDR_WIDTH),
+                .AXI4L_DATA_SIZE        (AXI4L_DATA_SIZE),
+                .AXI4L_DATA_WIDTH       (AXI4L_DATA_WIDTH),
+                .AXI4L_STRB_WIDTH       (AXI4L_STRB_WIDTH),
+                .WB_ADR_WIDTH           (WB_ADR_WIDTH),
+                .WB_DAT_WIDTH           (WB_DAT_WIDTH),
+                .WB_SEL_WIDTH           (WB_SEL_WIDTH)
+            )
+        i_axi4l_to_wishbone
+            (
+                .s_axi4l_aresetn        (axi4l_aresetn),
+                .s_axi4l_aclk           (axi4l_aclk),
+                .s_axi4l_awaddr         (axi4l_awaddr),
+                .s_axi4l_awprot         (axi4l_awprot ),
+                .s_axi4l_awvalid        (axi4l_awvalid),
+                .s_axi4l_awready        (axi4l_awready),
+                .s_axi4l_wstrb          (axi4l_wstrb),
+                .s_axi4l_wdata          (axi4l_wdata),
+                .s_axi4l_wvalid         (axi4l_wvalid),
+                .s_axi4l_wready         (axi4l_wready),
+                .s_axi4l_bresp          (axi4l_bresp),
+                .s_axi4l_bvalid         (axi4l_bvalid),
+                .s_axi4l_bready         (axi4l_bready),
+                .s_axi4l_araddr         (axi4l_araddr),
+                .s_axi4l_arprot         (axi4l_arprot),
+                .s_axi4l_arvalid        (axi4l_arvalid),
+                .s_axi4l_arready        (axi4l_arready),
+                .s_axi4l_rdata          (axi4l_rdata),
+                .s_axi4l_rresp          (axi4l_rresp),
+                .s_axi4l_rvalid         (axi4l_rvalid),
+                .s_axi4l_rready         (axi4l_rready),
+            
+                .m_wb_rst_o             (m_wb_rst_o),
+                .m_wb_clk_o             (m_wb_clk_o),
+                .m_wb_adr_o             (m_wb_adr_o),
+                .m_wb_dat_o             (m_wb_dat_o),
+                .m_wb_dat_i             (m_wb_dat_i),
+                .m_wb_we_o              (m_wb_we_o),
+                .m_wb_sel_o             (m_wb_sel_o),
+                .m_wb_stb_o             (m_wb_stb_o),
+                .m_wb_ack_i             (m_wb_ack_i)
+            );
+    
+    assign m_wb_ack_i = m_wb_stb_o;
+    assign m_wb_dat_i = m_wb_adr_o;
+
+endmodule
+
+
+`default_nettype wire
+
+
+// end of file
