6183ffc4e7d8338ca413f0e199fb3aa040e7e95a
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-11-16 23:53:25 +0900

    update

--- projects/ultra96v2_rtos/rtl/jelly_rtos.sv
+++ projects/ultra96v2_rtos/rtl/jelly_rtos.sv
@@ -62,27 +62,27 @@ module jelly_rtos
 
     jelly_priority_queue
             #(
-                .N          (TASKS),
-                .ID_WIDTH   (TSKID_WIDTH),
-                .PRI_WIDTH  (TSKPRI_WIDTH),
-                .N_WIDTH    (TSKID_WIDTH)
-            )
-        i_ready_queue
-            (
-                .reset      (reset),
-                .clk        (clk),
-                .cke        (cke),
+                .N              (TASKS),
+                .ID_WIDTH       (TSKID_WIDTH),
+                .PRI_WIDTH      (TSKPRI_WIDTH),
+                .N_WIDTH        (TSKID_WIDTH)
+            )   
+        i_ready_queue   
+            (   
+                .reset          (reset),
+                .clk            (clk),
+                .cke            (cke),
 
-                .in_op      (rdq_op),    // 0: add, 1: del
-                .in_id      (rdq_tskid),
-                .in_pri     (rdq_tskpri),
-                .in_valid   (in_valid),
+                .in_op          (rdq_op),    // 0: add, 1: del
+                .in_id          (rdq_tskid),
+                .in_pri         (rdq_tskpri),
+                .in_valid       (in_valid),
 
-                .top_id     (top_id),
-                .top_pri    (top_pri),
-                .top_valid  (top_valid),
+                .top_id         (top_id),
+                .top_pri        (top_pri),
+                .top_valid      (top_valid),
 
-                .size       ()
+                .size           ()
             );
 
 
@@ -90,15 +90,57 @@ module jelly_rtos
     //  semaphores
     // -----------------------------------------
 
+    logic   [SEMAPHORES-1:0][ID_WIDTH-1:0]      sem_wait_id;
+    logic   [SEMAPHORES-1:0][PRI_WIDTH-1:0]     sem_wait_pri;
+    logic                                       sem_wait_valid;
+
+    logic   [SEMAPHORES-1:0][ID_WIDTH-1:0]      sem_wakeup_id;
+    logic                                       sem_wakeup_valid;
+
+    logic   [SEMAPHORES-1:0][COUNTER_WIDTH-1:0] sem_counter;
+    logic   [SEMAPHORES-1:0][N_WIDTH-1:0]       sem_que_siz;
+
     logic   [SEMAPHORES-1:0][WB_DAT_WIDTH-1:0]  wb_sem_dat_o;
     logic   [SEMAPHORES-1:0]                    wb_sem_stb_i;
     logic   [SEMAPHORES-1:0]                    wb_sem_ack_o;
 
     generate
     for ( genvar i = 0; i < SEMAPHORES; ++i ) begin : loop_sem
-        
+        jelly_semaphore
+                #(
+                    .N              (TASKS),
+                    .N_WIDTH        (TSKID_WIDTH),
+                    .ID_WIDTH       (TSKID_WIDTH),
+                    .PRI_WIDTH      (TSKPRI_WIDTH),
+                    .COUNTER_WIDTH  (SEMCNT_WIDTH),
+                    .INIT_COUNTER   (0)
+                )
+            i_semaphore
+                (
+                    .reset          (reset),
+                    .clk            (clk),
+                    .cke            (cke),
+
+                    .signal         (sem_signal      [i]),
+
+                    .wait_id        (sem_wait_id     [i]),
+                    .wait_pri       (sem_wait_pri    [i]),
+                    .wait_valid     (sem_wait_valid  [i]),
+
+                    .wakeup_id      (sem_wakeup_id   [i]),
+                    .wakeup_valid   (sem_wakeup_valid[i]),
+
+                    .counter        (sem_counter     [i]),
+                    .que_size       (sem_que_size    [i])
+                );
     end
     endgenerate
+
+    
+    // -----------------------------------------
+    //  Wishbone
+    // -----------------------------------------
+
 
 
 endmodule
