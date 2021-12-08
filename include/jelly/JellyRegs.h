017b1d9d2cc544ddeece47ba57d2d376e61c235a
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-12-08 20:05:22 +0900

    update logger

--- include/jelly/JellyRegs.h
+++ include/jelly/JellyRegs.h
@@ -593,6 +593,24 @@
 
 
 
+
+/* ---------------------------------- */
+/*  Miscellaneous                     */
+/* ---------------------------------- */
+
+ #define REG_LOGGER_CORE_ID                     0x00
+ #define REG_LOGGER_CORE_VERSION                0x01
+ #define REG_LOGGER_CTL_CONTROL                 0x04
+ #define REG_LOGGER_CTL_STATUS                  0x05
+ #define REG_LOGGER_CTL_COUNT                   0x07
+ #define REG_LOGGER_READ_DATA                   0x10
+ #define REG_LOGGER_POL_TIMER0                  0x18
+ #define REG_LOGGER_POL_TIMER1                  0x19
+ #define REG_LOGGER_POL_DATA(x)                 (0x20+(x))
+
+
+
+
 #endif	/* __RYUZ__JELLY__REGS__H__ */
 
 
