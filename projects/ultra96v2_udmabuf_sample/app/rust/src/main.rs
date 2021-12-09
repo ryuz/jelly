ea93eb310049dc51f6fb6c34ecec582db3fb3a55
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-12-09 22:32:42 +0900

    update

--- projects/ultra96v2_udmabuf_sample/app/rust/src/main.rs
+++ projects/ultra96v2_udmabuf_sample/app/rust/src/main.rs
@@ -1,59 +1,52 @@
+#![allow(dead_code)]
 
-use uio::UioDevice;
-
-//mod mem_accessor;
-
+use std::{thread, time};
 use jelly_mem_access::*;
 
+const REG_DMA_STATUS : usize = 0;
+const REG_DMA_WSTART : usize = 1;
+const REG_DMA_RSTART : usize = 2;
+const REG_DMA_ADDR   : usize = 3;
+const REG_DMA_WDATA0 : usize = 4;
+const REG_DMA_WDATA1 : usize = 5;
+const REG_DMA_RDATA0 : usize = 6;
+const REG_DMA_RDATA1 : usize = 7;
+const REG_DMA_CORE_ID: usize = 8;
 
-fn open_uio(name: &str) -> Result<UioDevice, uio::UioError>
-{
-    for i in 0..99 {
-        let dev = uio::UioDevice::new(i)?;
-        let dev_name = dev.get_name()?;
-        if dev_name == name {
-            return Ok(dev);
-        }
-    }
-    Err(uio::UioError::Parse)
-}  
-
-
-fn test<T: MemRegion>(acc: MemAccesor<T, usize>) {
-    unsafe { println!("{:x}", acc.read_mem(0x040)); }
-    unsafe { println!("{:x}", acc.read_mem(0x840)); }
-
-    let acc2 = acc.clone(0x800, 0);
-    unsafe { println!("{:x}", acc2.read_mem(0x40)); }
-}
-
+const REG_TIM_CONTROL: usize = 0;
+const REG_TIM_COMPARE: usize = 1;
+const REG_TIM_COUNTER: usize = 3;
 
 
 fn main() {
-    println!("Hello, world!");
+    println!("Memory access test");
+    
+    // UIO を開く
+    let uio_acc = uio_accesor_from_name::<usize>("uio_pl_peri").unwrap();
 
-    /*
-    let dev = open_uio("uio_pl_peri").unwrap();
+    // メモリアドレスでアクセス
+    unsafe { println!("{:x}", uio_acc.read_mem(0x040)); }
+    unsafe { println!("{:x}", uio_acc.read_mem(0x840)); }
 
-    println!("uio_name : {}", dev.get_name().unwrap());
-    println!("uio_addr : 0x{:x}", dev.map_addr(0).unwrap());
-    println!("uio_size : 0x{:x}", dev.map_size(0).unwrap());
+    // UIOの中をさらにコアごとに割り当て
+    let dma0_acc = uio_acc.clone(0x00000, 0);
+    let dma1_acc = uio_acc.clone(0x00800, 0);
+    let led_acc  = uio_acc.clone(0x08000, 0);
+    let _tim_acc  = uio_acc.clone(0x10000, 0);
 
-    let addr = dev.map_mapping(0).unwrap();
-    let addr = addr as usize;
-    unsafe { println!("{:x}", std::ptr::read_volatile((addr + 0x040) as *mut u32)); }
-    unsafe { println!("{:x}", std::ptr::read_volatile((addr + 0x840) as *mut u32)); }
+    // レジスタのワードアドレスでアクセス
+    println!("DAM0 ID : 0x{:x}", unsafe{dma0_acc.read_reg(REG_DMA_CORE_ID)});
+    println!("DAM1 ID : 0x{:x}", unsafe{dma1_acc.read_reg(REG_DMA_CORE_ID)});
 
-    println!("<mmio>");
-    let acc = mmio_accesor_new::<usize>(addr, dev.map_size(0).unwrap());
-    unsafe { println!("{:x}", acc.read_mem(0x040)); }
-    unsafe { println!("{:x}", acc.read_mem(0x840)); }
+    for _ in 0..5 {
+        // LED ON
+        println!("LED : ON");
+        unsafe { led_acc.write_reg(0, 1); }
+        thread::sleep(time::Duration::from_millis(500));
 
-    test::<MmioRegion>(acc);
-    */
-
-    println!("<uio>");
-    let acc = uio_accesor_from_name::<usize>("uio_pl_peri").unwrap();
-    unsafe { println!("{:x}", acc.read_mem(0x040)); }
-    unsafe { println!("{:x}", acc.read_mem(0x840)); }
+        // LED OFF
+        println!("LED : OFF");
+        unsafe { led_acc.write_reg(0, 0); }
+        thread::sleep(time::Duration::from_millis(500));
+    }
 }
