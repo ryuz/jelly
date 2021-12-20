45e0ec84e23449ddbc6f8563bf02677bd618d947
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-12-20 23:24:20 +0900

    update

--- projects/ultra96v2_hls_test/hls/csynth.tcl
+++ projects/ultra96v2_hls_test/hls/csynth.tcl
@@ -1,16 +1,9 @@
 
 set hls_target $env(HLS_TARGET)
 set hls_solution $env(HLS_SOLUTION)
-set device_part $env(DEVICE_PART)
-set clock_period $env(CLOCK_PERIOD)
 
 open_project $hls_target
 open_solution $hls_solution
-
-set_top $hls_target
-set_part $device_part
-create_clock -period $clock_period -name default
-source "directives.tcl"
 
 csynth_design
 export_design -format ip_catalog
