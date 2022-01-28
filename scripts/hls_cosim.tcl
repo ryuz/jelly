a48aafac8e6fa1ecc65e1b039eba1928286ff4b9
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2022-01-29 08:34:11 +0900

    update tcl scripts

--- scripts/hls_cosim.tcl
+++ scripts/hls_cosim.tcl
@@ -1,11 +1,12 @@
 
 set hls_target $env(HLS_TARGET)
 set hls_solution $env(HLS_SOLUTION)
+set cosim_options [split $env(COSIM_OPTIONS) " "]
 
 open_project $hls_target
 open_solution $hls_solution
 
-cosim_design
+cosim_design {*}$cosim_options
 
 close_solution
 close_project
--- scripts/hls_csim.tcl
+++ scripts/hls_csim.tcl
@@ -1,11 +1,12 @@
 
 set hls_target $env(HLS_TARGET)
 set hls_solution $env(HLS_SOLUTION)
+set csim_options [split $env(CSIM_OPTIONS) " "]
 
 open_project $hls_target
 open_solution $hls_solution
 
-csim_design
+csim_design {*}$csim_options
 
 close_solution
 close_project
