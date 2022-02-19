b612e18c7efcb0535deecf38fbba8bed7e28d8e8
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2022-02-19 13:18:27 +0900

    update sim

--- projects/ultra96v2_imx219/sim/tb_top/verilator/tb_verilator.cpp
+++ projects/ultra96v2_imx219/sim/tb_top/verilator/tb_verilator.cpp
@@ -1,6 +1,6 @@
 #include <memory>
 #include <verilated.h>
-#include <opencv2/opencv.hpp>
+//#include <opencv2/opencv.hpp>
 #include "Vtb_sim_main.h"
 #include "Vtb_sim_main__Syms.h"
 #include "Vtb_sim_main___024root.h"
@@ -9,8 +9,9 @@
 #include "jelly/simulator/ClockNode.h"
 #include "jelly/simulator/VerilatorNode.h"
 #include "jelly/simulator/WishboneMasterNode.h"
-#include "jelly/simulator/Axi4sImageLoadNode.h"
-#include "jelly/simulator/Axi4sImageDumpNode.h"
+//#include "jelly/simulator/Axi4sImageLoadNode.h"
+//#include "jelly/simulator/Axi4sImageDumpNode.h"
+#include "jelly/JellyRegs.h"
 
 
 namespace jsim = jelly::simulator;
@@ -20,20 +21,6 @@ namespace jsim = jelly::simulator;
 #include <verilated_fst_c.h> 
 #include <verilated_vcd_c.h> 
 #endif
-
-
-// 画像の前処理
-cv::Mat PreProcImage(cv::Mat img, int frame_num)
-{
-    if ( img.empty() ) { return img; }
-
-    // 回転
-    auto center = cv::Point(img.cols/2, img.rows/2);
-    auto trans  = cv::getRotationMatrix2D(center, frame_num*10, 1);
-    cv::Mat img2;
-    cv::warpAffine(img, img2, trans, img.size());
-    return img2;
-}
 
 
 int main(int argc, char** argv)
@@ -58,112 +45,129 @@ int main(int argc, char** argv)
     auto mng = jsim::Manager::Create();
 
     mng->AddNode(jsim::VerilatorNode_Create(top, tfp));
+
     mng->AddNode(jsim::ResetNode_Create(&top->reset, 100));
-    mng->AddNode(jsim::ClockNode_Create(&top->clk, 1000.0/100.0));
-
-    mng->AddNode(jsim::ResetNode_Create(&top->rootp->tb_sim_main__DOT__i_top__DOT__i_design_1__DOT__reset, 100));
-    mng->AddNode(jsim::ClockNode_Create(&top->rootp->tb_sim_main__DOT__i_top__DOT__i_design_1__DOT__clk100, 1000.0/100.0));
-    mng->AddNode(jsim::ClockNode_Create(&top->rootp->tb_sim_main__DOT__i_top__DOT__i_design_1__DOT__clk200, 1000.0/200.0));
-    mng->AddNode(jsim::ClockNode_Create(&top->rootp->tb_sim_main__DOT__i_top__DOT__i_design_1__DOT__clk250, 1000.0/250.0));
-
-    mng->AddNode(jsim::ResetNode_Create(&top->rootp->tb_sim_main__DOT__i_top__DOT__i_mipi_dphy_cam__DOT__reset,  100));
-    mng->AddNode(jsim::ResetNode_Create(&top->rootp->tb_sim_main__DOT__i_top__DOT__i_mipi_dphy_cam__DOT__busy,  1000, false));
-    mng->AddNode(jsim::ClockNode_Create(&top->rootp->tb_sim_main__DOT__i_top__DOT__i_mipi_dphy_cam__DOT__hs_clk, 8.768));
-
-    jsim::Axi4sVideo axi4s_src =
-            {
-                &top->rootp->tb_sim_main__DOT__i_top__DOT__axi4s_cam_aresetn,
-                &top->rootp->tb_sim_main__DOT__i_top__DOT__axi4s_cam_aclk,
-                &top->rootp->tb_sim_main__DOT__i_top__DOT__axi4s2_csi2_tuser,
-                &top->rootp->tb_sim_main__DOT__i_top__DOT__axi4s2_csi2_tlast,
-                &top->rootp->tb_sim_main__DOT__i_top__DOT__axi4s2_csi2_tdata,
-                &top->rootp->tb_sim_main__DOT__i_top__DOT__axi4s2_csi2_tvalid,
-                &top->rootp->tb_sim_main__DOT__i_top__DOT__axi4s2_csi2_tready
-            };
-    auto image_src_load = jsim::Axi4sImageLoadNode_Create(axi4s_src, "../BOAT.bmp", jsim::fmt_gray);
-    mng->AddNode(image_src_load);
-    image_src_load->SetBlankX(64);
-    image_src_load->SetBlankY((256 + 64) * 8);
-    image_src_load->SetRandomWait(0.0);
-
-
-    mng->AddNode(image_src_load);
-
+    mng->AddNode(jsim::ClockNode_Create(&top->clk100, 1000.0/100.0));
+    mng->AddNode(jsim::ClockNode_Create(&top->clk200, 1000.0/200.0));
+    mng->AddNode(jsim::ClockNode_Create(&top->clk250, 1000.0/250.0));
 
     jsim::WishboneMaster wishbone_signals =
             {
-                &top->rootp->tb_sim_main__DOT__i_top__DOT__i_design_1__DOT__reset,
-                &top->rootp->tb_sim_main__DOT__i_top__DOT__i_design_1__DOT__clk100,
-                &top->rootp->tb_sim_main__DOT__i_top__DOT__i_design_1__DOT__wb_adr_i,
-                &top->rootp->tb_sim_main__DOT__i_top__DOT__i_design_1__DOT__wb_dat_o,
-                &top->rootp->tb_sim_main__DOT__i_top__DOT__i_design_1__DOT__wb_dat_i,
-                &top->rootp->tb_sim_main__DOT__i_top__DOT__i_design_1__DOT__wb_sel_i,
-                &top->rootp->tb_sim_main__DOT__i_top__DOT__i_design_1__DOT__wb_we_i,
-                &top->rootp->tb_sim_main__DOT__i_top__DOT__i_design_1__DOT__wb_stb_i,
-                &top->rootp->tb_sim_main__DOT__i_top__DOT__i_design_1__DOT__wb_ack_o
+                &top->reset,
+                &top->clk100,
+                &top->s_wb_adr_i,
+                &top->s_wb_dat_o,
+                &top->s_wb_dat_i,
+                &top->s_wb_sel_i,
+                &top->s_wb_we_i,
+                &top->s_wb_stb_i,
+                &top->s_wb_ack_o
             };
     auto wb = jsim::WishboneMasterNode_Create(wishbone_signals);
     mng->AddNode(wb);
-    
-    // WISHBONE 
-    wb->Wait(1000);
-    
-    wb->Display("read id");
-    wb->Read(0);
-    wb->Read(0);
 
+
+    const int X_NUM = 1024;
+    const int Y_NUM = 64;
+    
+    const int reg_gid    = (0x00000000 >> 3);
+    const int reg_fmtr   = (0x00100000 >> 3);
+    const int reg_demos  = (0x00120000 >> 3);
+    const int reg_colmat = (0x00120200 >> 3);
+    const int reg_wdma   = (0x00210000 >> 3);
+
+
+    wb->Wait(1000);
+    wb->Display("start");
+    
+    wb->Wait(1000);
+    wb->Display("read core ID");
+    wb->Read (reg_gid);     // gid
+    wb->Read (reg_fmtr);    // fmtr
+    wb->Read (reg_demos);   // demosaic
+    wb->Read (reg_colmat);  // col mat
+    wb->Read (reg_wdma);    // wdma
+
+    wb->Wait(1000);
+    wb->Display("set format regularizer");
+    wb->Read (reg_fmtr + REG_VIDEO_FMTREG_CORE_ID);                         // CORE ID
+    wb->Write(reg_fmtr + REG_VIDEO_FMTREG_PARAM_WIDTH,      X_NUM, 0xf);    // width
+    wb->Write(reg_fmtr + REG_VIDEO_FMTREG_PARAM_HEIGHT,     Y_NUM, 0xf);    // height
+    wb->Write(reg_fmtr + REG_VIDEO_FMTREG_PARAM_FILL,           0, 0xf);    // fill
+    wb->Write(reg_fmtr + REG_VIDEO_FMTREG_PARAM_TIMEOUT,     1024, 0xf);    // timeout
+    wb->Write(reg_fmtr + REG_VIDEO_FMTREG_CTL_CONTROL,          1, 0xf);    // enable
+    wb->Wait(1000);
+        
+    wb->Display("set write DMA");
+    wb->Read (reg_wdma + REG_VDMA_WRITE_CORE_ID);                         // CORE ID
+    wb->Write(reg_wdma + REG_VDMA_WRITE_PARAM_ADDR,          0x30000000, 0xf);  // address
+    wb->Write(reg_wdma + REG_VDMA_WRITE_PARAM_LINE_STEP,        X_NUM*4, 0xf);  // stride
+    wb->Write(reg_wdma + REG_VDMA_WRITE_PARAM_H_SIZE,           X_NUM-1, 0xf);   // width
+    wb->Write(reg_wdma + REG_VDMA_WRITE_PARAM_V_SIZE,           Y_NUM-1, 0xf);  // height
+    wb->Write(reg_wdma + REG_VDMA_WRITE_PARAM_F_SIZE,               1-1, 0xf);
+    wb->Write(reg_wdma + REG_VDMA_WRITE_PARAM_FRAME_STEP, X_NUM*Y_NUM*4, 0xff);
+    wb->Write(reg_wdma + REG_VDMA_WRITE_CTL_CONTROL,                  3, 0xf);  // update & enable
+    wb->Wait(1000);
+    wb->Read (reg_wdma + REG_VDMA_WRITE_CTL_STATUS);  // read status
+    wb->Read (reg_wdma + REG_VDMA_WRITE_CTL_STATUS);  // read status
+    wb->Read (reg_wdma + REG_VDMA_WRITE_CTL_STATUS);  // read status
+    wb->Read (reg_wdma + REG_VDMA_WRITE_CTL_STATUS);  // read status
+        
+        
 
     /*
-    jsim::Axi4sVideo axi4s_dst =
-            {
-                &top->aresetn,
-                &top->aclk,
-                &top->m_axi4s_dst_tuser,
-                &top->m_axi4s_dst_tlast,
-                &top->m_axi4s_dst_tdata,
-                &top->m_axi4s_dst_tvalid,
-                &top->m_axi4s_dst_tready
-            };
-
-    jsim::Axi4sVideo axi4s_angle =
-            {
-                &top->aresetn,
-                &top->aclk,
-                &top->m_axi4s_angle_tuser,
-                &top->m_axi4s_angle_tlast,
-                &top->m_axi4s_angle_tdata,
-                &top->m_axi4s_angle_tvalid,
-                (int*)nullptr
-            };
-
-    std::string s;
-    auto image_src_load = jsim::Axi4sImageLoadNode_Create(axi4s_src, "../BOAT.bmp", jsim::fmt_gray);
-    auto image_dst_dump = jsim::Axi4sImageDumpNode_Create(axi4s_dst, "img_%04d.png", jsim::fmt_gray, 256, 256);
-    auto image_angle_dump = jsim::Axi4sImageDumpNode_Create(axi4s_angle, "angle_%04d.png", jsim::fmt_color, 256, 256);
-
-    image_src_load->SetBlankX(64);
-    image_src_load->SetBlankY((256 + 64) * 8);
-    image_src_load->SetRandomWait(0.0);
-    image_dst_dump->SetRandomWait(0.0);
-
-    image_angle_dump->SetFrameLimit(2);
-    image_dst_dump->SetFrameLimit(2);//, true); // 2フレーム処理したら終了するように設定
+    const unsigned int ADR_FMTR    = (0x00100000 >> 3);  // ビデオサイズ正規化
+    const unsigned int ADR_DEMOS   = (0x00200000 >> 3);  // デモザイク
+    const unsigned int ADR_COLMAT  = (0x00210000 >> 3);  // カラーマトリックス
+    const unsigned int ADR_GAMMA   = (0x00220000 >> 3);  // ガンマ補正
+    const unsigned int ADR_GAUSS   = (0x00240000 >> 3);  // ガウシアンフィルタ
+    const unsigned int ADR_CANNY   = (0x00250000 >> 3);  // Cannyフィルタ
+    const unsigned int ADR_IMGDMA  = (0x00260000 >> 3);  // FIFO dma
+    const unsigned int ADR_BINDIFF = (0x00270000 >> 3);  // 前画像との差分バイナライズ
+    const unsigned int ADR_SEL     = (0x002f0000 >> 3);  // 出力切り替え
+    const unsigned int ADR_BUFMNG  = (0x00300000 >> 3);  // Buffer manager
+    const unsigned int ADR_BUFALC  = (0x00310000 >> 3);  // Buffer allocator
+    const unsigned int ADR_VDMAW   = (0x00320000 >> 3);  // Write-DMA
+    const unsigned int ADR_VDMAR   = (0x00340000 >> 3);  // Read-DMA
+    const unsigned int ADR_VSGEN   = (0x00360000 >> 3);  // Video out sync generator
+    const unsigned int ADR_HLS     = (0x00400000 >> 3);
     
+    const unsigned int IMG_X_NUM = 1024;
+    const unsigned int IMG_Y_NUM = 64;
 
-    mng->AddNode(image_src_load);
-    mng->AddNode(image_dst_dump);
-    mng->AddNode(image_angle_dump);
+    // WISHBONE 
+    wb->Wait(200);    
+    wb->Display("read id");
+    wb->Read(0);
 
-    image_src_load->SetImagePreProc(PreProcImage);
-    image_src_load->SetImageShow("sorce image");
-    image_angle_dump->SetImageShow("canny angle");
-    image_dst_dump->SetImageShow("canny edge");
-    mng->SetThreadEnable(true);
+    wb->Display("set FMTR");
+    wb->Read (ADR_FMTR + REG_VIDEO_FMTREG_CORE_ID);
+    wb->Write(ADR_FMTR + REG_VIDEO_FMTREG_PARAM_WIDTH,  IMG_X_NUM, 0xff);
+    wb->Write(ADR_FMTR + REG_VIDEO_FMTREG_PARAM_HEIGHT, IMG_Y_NUM, 0xff);
+    wb->Write(ADR_FMTR + REG_VIDEO_FMTREG_CTL_CONTROL,        0x3, 0xff);
 
-    mng->SetControlCvWindow("Simulation", 0x1b);
+    wb->Display("set DEMOSIC");
+    wb->Read (ADR_DEMOS + REG_IMG_DEMOSAIC_CORE_ID);
+    wb->Write(ADR_DEMOS + REG_IMG_DEMOSAIC_PARAM_PHASE,    0, 0xff);
+    wb->Write(ADR_DEMOS + REG_IMG_DEMOSAIC_CTL_CONTROL,  0x3, 0xff);
+
+    wb->Display("set write DMA");
+    wb->Read (ADR_VDMAW + REG_VDMA_WRITE_CORE_ID);
+    wb->Write(ADR_VDMAW + REG_VDMA_WRITE_PARAM_ADDR,                    0x0000000, 0xff);
+    wb->Write(ADR_VDMAW + REG_VDMA_WRITE_PARAM_LINE_STEP,             IMG_X_NUM*3, 0xff);
+    wb->Write(ADR_VDMAW + REG_VDMA_WRITE_PARAM_H_SIZE,                IMG_X_NUM-1, 0xff);
+    wb->Write(ADR_VDMAW + REG_VDMA_WRITE_PARAM_V_SIZE,                IMG_Y_NUM-1, 0xff);
+    wb->Write(ADR_VDMAW + REG_VDMA_WRITE_PARAM_FRAME_STEP,  IMG_Y_NUM*IMG_X_NUM*3, 0xff);
+    wb->Write(ADR_VDMAW + REG_VDMA_WRITE_PARAM_F_SIZE,                        1-1, 0xff);
+    wb->Write(ADR_VDMAW + REG_VDMA_WRITE_CTL_CONTROL,                           3, 0xff);  // update & enable
+
+    wb->Wait(100000);
+    wb->Display("set HLS");
+    wb->Write(ADR_HLS + 0x08, 1, 0xff);
+    wb->Wait(100000);
     */
 
-    mng->Run(1000000);
+    mng->Run(4000000);
 //    mng->Run();
 
 #if VM_TRACE
