47c6dddbe70277b3bbbc018be4ace3356c5bc574
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2022-02-27 15:11:08 +0900

    update

---
+++ projects/ultra96v2_riscv_sample/app/bin_copy.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+scp riscv/riscv_sample.bin debian-ultra96:/home/ryuji/git-work/jelly/projects/ultra96v2_riscv_sample/app/riscv/
+scp riscv/mem.hex          debian-ultra96:/home/ryuji/git-work/jelly/projects/ultra96v2_riscv_sample/app/riscv/
+
+
+
