065ccfc0fe67c172cd1c3693dd71590c2815b3b4
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2022-05-03 13:42:04 +0900

    add docker ubuntu18.04

---
+++ docker/ubuntu-18.04/files/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USER_NAME=${LOCAL_USER:-user}
+USER_ID=${LOCAL_UID:-9001}
+GROUP_ID=${LOCAL_GID:-9001}
+
+echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
+useradd -u $USER_ID -o -m $USER_NAME --shell /bin/bash
+groupmod -g $GROUP_ID $USER_NAME
+echo "$USER_NAME:$USER_NAME" | chpasswd
+
+gpasswd -a $USER_NAME sudo
+
+/usr/sbin/sshd -D
