2c8c3e2e25cbf815db3046cb37a4ffa03e9ec02e
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2022-06-21 20:34:47 +0900

    update

--- docker/jelly_zynqmp/buildx.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker buildx build . --platform linux/arm64/v8 --output=type=docker,dest=- > jelly_zynqmp.tar
--- docker/jelly_zynqmp/push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker buildx build . --platform linux/arm64/v8 --output=type=image,push=true --tag ryuz88/jelly_zynqmp
-
