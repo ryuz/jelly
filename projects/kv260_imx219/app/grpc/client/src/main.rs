94570bc999ee7b28332215503231c7a78c6c8da9
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2022-09-06 07:07:07 +0900

    update

--- projects/kv260_imx219/app/grpc/client/src/main.rs
+++ projects/kv260_imx219/app/grpc/client/src/main.rs
@@ -1,68 +1,48 @@
-use std::io::stdin;
-
-use camera_control::{camera_control_client::CameraControlClient, OpenRequest, Empty, ImageResponse};
+use camera_control::*;
+use camera_control_client::CameraControlClient;
+use tonic::transport::Channel;
 
 pub mod camera_control {
-  tonic::include_proto!("camera_control");
+    tonic::include_proto!("camera_control");
 }
 
 use std::fs::File;
-use std::io::{self, BufRead, Write, BufReader};
+use std::io::Write;
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-// let mut client = CameraControlClient::connect("http://127.0.0.1:8080").await?;
-   let mut client = CameraControlClient::connect("http://kria:50051").await?;
-   /*
-  loop {
-    println!("\nPlease vote for a particular url");
-    let mut u = String::new();
-    let mut vote: String = String::new();
-    println!("Please provide a url: ");
-    stdin().read_line(&mut u).unwrap();
-    let u = u.trim();
-    println!("Please vote (d)own or (u)p: ");
-    stdin().read_line(&mut vote).unwrap();
-    let v = match vote.trim().to_lowercase().chars().next().unwrap() {
-      'u' => 0,
-      'd' => 1,
-      _ => break,
-    };
-    let request = tonic::Request::new(OpenRequest {
-      id: 1,
+    // let mut client = CameraControlClient::connect("http://127.0.0.1:8080").await?;
+    let mut client: CameraControlClient<Channel> = CameraControlClient::connect("http://kria:50051").await?;
+
+    let request = tonic::Request::new(SetAoiRequest {
+        id: 1,
+        width: 640,
+        height: 480,
+        x: -1,
+        y: -1,
     });
-    let response = client.open(request).await?;
-//    println!("Got: '{}' from service", response.into_inner().confirmation);
-  }
-  */
+    let _response = client.set_aoi(request).await?;
 
-  let request = tonic::Request::new(OpenRequest {id: 1,});
-  let response = client.open(request).await?;
-//    println!("Got: '{}' from service", response.into_inner().confirmation);
+    let request = tonic::Request::new(OpenRequest { id: 1 });
+    let _response = client.open(request).await?;
 
-  let empty = tonic::Request::new(Empty{});
-  let img = client.get_image(empty).await?;
-  let img = img.into_inner();
-  println!("width : {}", img.width);
-  println!("height: {}", img.height);
-  println!("len: {}", img.image.len());
-  
-  let mut file = File::create("test.ppm")?;
-  write!(file, "P3\n")?;
-  write!(file, "{} {}\n", img.width, img.height)?;
-  write!(file, "256\n")?;
-  for i in 0..img.width*img.height {
-    write!(file, "{} ", img.image[2+4*i as usize])?;
-    write!(file, "{} ", img.image[1+4*i as usize])?;
-    write!(file, "{}\n", img.image[0+4*i as usize])?;
-  }
+    let request = tonic::Request::new(GetImageRequest { id: 1 });
+    let img = client.get_image(request).await?;
+    let img = img.into_inner();
+    println!("width : {}", img.width);
+    println!("height: {}", img.height);
+    println!("len: {}", img.image.len());
 
-  Ok(())
+    let mut file = File::create("camera_image.ppm")?;
+    write!(file, "P3\n")?;
+    write!(file, "{} {}\n", img.width, img.height)?;
+    write!(file, "256\n")?;
+    for i in 0..img.width * img.height {
+        write!(file, "{} ", img.image[2 + 4 * i as usize])?;
+        write!(file, "{} ", img.image[1 + 4 * i as usize])?;
+        write!(file, "{}\n", img.image[0 + 4 * i as usize])?;
+    }
+
+    Ok(())
 }
-
-
-/*
-fn main() {
-    println!("Hello, world!");
-}
-*/
