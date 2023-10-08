d41428e375c2e5a9ee55f9a346d56151393dc175
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2023-10-08 18:04:14 +0900

    update

--- projects/tang_nano_9k/tang_nano_9k_blink_led/rtl/tang_nano_9k_blink_led.sv
+++ projects/tang_nano_9k/tang_nano_9k_blink_led/rtl/tang_nano_9k_blink_led.sv
@@ -1,15 +1,23 @@
 
+`default_nettype none
+
 module tang_nano_9k_blink_led
         (
-            input   var logic   clk,
-            output  var logic   led
+            input   var logic           reset_n,
+            input   var logic           clk,
+
+            output  var logic   [4:0]   led
         );
 
-    logic   [23:0]  counter;
+    logic   [26:0]  counter;
     always_ff @(posedge clk) begin
-        counter <= counter + 1;
+        if ( ~reset_n ) begin
+            counter <= 0;
+        end
+        else begin
+            counter <= counter + 1;
+        end
     end
-    assign led = counter[23];
+    assign led = counter[26:22];
 
 endmodule
-
