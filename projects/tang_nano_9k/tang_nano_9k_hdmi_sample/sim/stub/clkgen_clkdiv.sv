c870dd2ad2f2949248b584f49cac99e0a04c83cd
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2023-10-09 16:06:53 +0900

    add xsim

---
+++ projects/tang_nano_9k/tang_nano_9k_hdmi_sample/sim/stub/clkgen_clkdiv.sv
@@ -0,0 +1,16 @@
+
+`default_nettype none
+
+module clkgen_clkdiv
+        (
+            input   var logic   reset_n,
+            input   var logic   clk_in,
+            output  var logic   clk_out
+        );
+
+endmodule
+
+
+`default_nettype wire
+
+
---
+++ projects/tang_nano_9k/tang_nano_9k_hdmi_sample/sim/tb_top/tb_main.sv
@@ -0,0 +1,58 @@
+// ---------------------------------------------------------------------------
+//
+//                                 Copyright (C) 2015-2020 by Ryuz 
+//                                 https://github.com/ryuz/
+// ---------------------------------------------------------------------------
+
+
+`timescale 1ns / 1ps
+`default_nettype none
+
+
+module tb_main
+        (
+            input   wire                        in_reset_n,
+            input   wire                        in_clk,
+            input   wire                        clk,
+            input   wire                        clk_x5
+        );
+    
+    
+    // -----------------------------------------
+    //  top
+    // -----------------------------------------
+
+    logic           dvi_tx_clk_p;
+    logic           dvi_tx_clk_n;
+    logic   [2:0]   dvi_tx_data_p;
+    logic   [2:0]   dvi_tx_data_n;
+
+    logic   [4:0]   led_n;
+
+    tang_nano_9k_hdmi_sample
+        u_top
+            (
+                in_reset_n,
+                in_clk,
+
+                dvi_tx_clk_p,
+                dvi_tx_clk_n,
+                dvi_tx_data_p,
+                dvi_tx_data_n,
+
+                led_n
+            );
+
+
+    // for verilator
+    always_comb force u_top.clk    = clk;
+    always_comb force u_top.clk_x5 = clk_x5;
+
+
+endmodule
+
+
+`default_nettype wire
+
+
+// end of file
