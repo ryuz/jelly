b613dee5f44321ef9fdf962473345cbc7f71c5bc
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2023-10-14 14:25:07 +0900

    gowin_build.tcl に sdc ファイル対応追加

--- scripts/gowin_build.tcl
+++ scripts/gowin_build.tcl
@@ -3,8 +3,24 @@
 set base_name $::env(BASE_NAME)
 set device_name $::env(DEVICE_NAME)
 set device_version $::env(DEVICE_VERSION)
-set vlog_sources  [split $::env(VLOG_SOURCES) " "]
-set cst_sources  [split $::env(CST_SOURCES) " "]
+
+if {[info exists ::env(CST_SOURCES)]} {
+    set vlog_sources  [split $::env(VLOG_SOURCES) " "]
+} else {
+    set vlog_sources {}
+}
+
+if {[info exists ::env(SDC_SOURCES)]} {
+    set sdc_sources  [split $::env(SDC_SOURCES) " "]
+} else {
+    set sdc_sources {}
+}
+
+if {[info exists ::env(CST_SOURCES)]} {
+    set cst_sources  [split $::env(CST_SOURCES) " "]
+} else {
+    set cst_sources {}
+}
 
 if {[info exists ::env(DEVICE_VERSION)]} {
     set device_version $::env(DEVICE_VERSION)
@@ -24,6 +40,7 @@ if {[info exists ::env(USE_DONE_AS_GPIO)]} {
     set use_done_as_gpio "1"
 }
 
+
 set_device -device_version $device_version $device_name
 set_option -synthesis_tool gowinsynthesis
 set_option -output_base_name $base_name
@@ -33,6 +50,11 @@ set_option -use_done_as_gpio $use_done_as_gpio
 # verilog files
 foreach fname $vlog_sources {
     add_file -type verilog $fname
+}
+
+# sdc files
+foreach fname $sdc_sources {
+    add_file -type sdc $fname
 }
 
 # cst files
