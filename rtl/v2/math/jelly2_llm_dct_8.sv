2c92878b8cdcee65496d0a8549c055643a3740aa
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2023-12-23 12:45:11 +0900

    update jelly2_llm_dct_8

--- rtl/v2/math/jelly2_llm_dct_8.sv
+++ rtl/v2/math/jelly2_llm_dct_8.sv
@@ -101,9 +101,45 @@ module jelly2_llm_dct_8
     
     assign in_re   = st0_0_valid;
     assign in_addr = st0_0_addr;
+   
+    // buffer
+    logic                       st0_wr_bank;
+    always_ff @(posedge clk) begin
+        if ( reset ) begin
+            st0_wr_bank <= 1'b0;
+        end
+        else if ( cke ) begin
+            if ( st0_3_valid && st0_3_addr == 3'd4 ) begin
+                st0_wr_bank <= st0_wr_bank + 1'b1;
+            end
+        end
+    end
 
-    calc_t          st0_mem     [0:15];
-    
+    logic                       st0_rd_en;
+    logic   [3:0]               st0_rd_addr;
+    logic   [DATA_WIDTH-1:0]    st0_rd_dout;
+
+    jelly2_ram_simple_dualport
+            #(
+                .ADDR_WIDTH     (4),
+                .DATA_WIDTH     (DATA_WIDTH),
+                .MEM_SIZE       (16),
+                .RAM_TYPE       ("distributed"),
+                .DOUT_REGS      (0)
+            )
+        u_ram_simple_dualport_0
+            (
+                .wr_clk         (clk),
+                .wr_en          (st0_3_valid), // dont' care cke
+                .wr_addr        ({st0_wr_bank, st0_3_addr}),
+                .wr_din         (st0_3_data),
+                
+                .rd_clk         (clk),
+                .rd_en          (cke),
+                .rd_regcke      (1'b0),
+                .rd_addr        (),
+                .rd_dout        ()
+            );
 
 
 endmodule
