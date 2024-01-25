1bfef97bfc4c1657706ced16c16fffbb4ae5b0d2
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2024-01-25 18:41:01 +0900

    add build_verilator.sh

---
+++ tools/build_scripts/build_verilator.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+VERILATOR_VERSION=5.020
+
+git clone https://github.com/verilator/verilator.git -b v${VERILATOR_VERSION} verilator-${VERILATOR_VERSION}
+cd verilator-${VERILATOR_VERSION}
+
+autoconf
+./configure --prefix ${HOME}/.opt/verilator-${VERILATOR_VERSION}
+make -j2
+make install
+
