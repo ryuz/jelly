e8b4b943e4af7772c338d41e95bd4d2c2e40c319
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2024-01-27 08:52:38 +0000

    update

--- tools/dpkg/build_fclkcfg.sh
+++ tools/dpkg/build_fclkcfg.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# build
 rm -f fclkcfg-*.deb
 sudo rm -fr fclkcfg-kmod-dpkg
 git clone --recursive --depth=1 -b v1.7.2 https://github.com/ikwzm/fclkcfg-kmod-dpkg
 cd fclkcfg-kmod-dpkg
 sudo debian/rules binary
 cd ..
-#sudo dpkg -i fclkcfg-*.deb
+sudo chown $USER:$USER fclkcfg-*.deb
+
+# install
+sudo dpkg -i fclkcfg-*.deb
+
+# clean
+sudo rm -fr fclkcfg-kmod-dpkg
