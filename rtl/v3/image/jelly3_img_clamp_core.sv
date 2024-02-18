0321fb0aa7ca0cc54120fa7d8ff9f6eb05a6451a
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2024-02-18 18:11:32 +0900

    Refactor jelly3_img_clamp_core module parameters and data assignment

--- rtl/v3/image/jelly3_img_clamp_core.sv
+++ rtl/v3/image/jelly3_img_clamp_core.sv
@@ -14,8 +14,13 @@
 
 module jelly3_img_clamp_core
         #(
-            parameter   int     CALC_BITS = 10                      ,
-            parameter   type    calc_t    = logic [CALC_BITS-1:0]
+            parameter   int     N            = 3                        ,
+            parameter   int     S_DATA_BITS  = 10                       ,
+            parameter   type    s_data_t     = logic [S_DATA_BITS-1:0]  ,
+            parameter   int     M_DATA_BITS  = 10                       ,
+            parameter   type    m_data_t     = logic [M_DATA_BITS-1:0]  ,
+            parameter   int     CALC_BITS    = 10                       ,
+            parameter   type    calc_t       = logic [CALC_BITS-1:0]    
         )
         (
             input   var logic               enable,
@@ -25,18 +30,19 @@ module jelly3_img_clamp_core
             jelly3_img_if.m                 m_img
         );
 
-    localparam  int S_DATA_BITS = s_img.DATA_BITS;
-    localparam  int M_DATA_BITS = m_img.DATA_BITS;
     localparam  int DE_BITS     = s_img.DE_BITS;
     localparam  int USER_BITS   = s_img.USER_BITS;
 
-    localparam  type    s_data_t  = logic    [S_DATA_BITS-1:0];
-    localparam  type    m_data_t  = logic    [M_DATA_BITS-1:0];
     localparam  type    de_t      = logic    [DE_BITS    -1:0];
     localparam  type    user_t    = logic    [USER_BITS  -1:0];
 
+    s_data_t    [N-1:0]  s_img_data;
+    m_data_t    [N-1:0]  m_img_data;
+    assign s_img_data = s_img.data;
+    assign m_img.data = m_img_data;
+
     function automatic m_data_t clamp(input s_data_t data);
-        calc_t v;
+        automatic calc_t v = calc_t'(data);
         if ( v < min_value ) v = min_value;
         if ( v > max_value ) v = max_value;
         return m_data_t'(v);
@@ -49,7 +55,7 @@ module jelly3_img_clamp_core
             m_img.col_first <= 'x;
             m_img.col_last  <= 'x;
             m_img.de        <= 'x;
-            m_img.data      <= 'x;
+            m_img_data      <= 'x;
             m_img.user      <= 'x;
             m_img.valid     <= 1'b0;
         end
@@ -59,11 +65,15 @@ module jelly3_img_clamp_core
             m_img.col_first <= s_img.col_first      ;
             m_img.col_last  <= s_img.col_last       ;
             m_img.de        <= s_img.de             ;
-            m_img.data      <= m_data_t'(s_img.data);
             m_img.user      <= s_img.user           ;
             m_img.valid     <= s_img.valid          ;
-            if ( enable ) begin
-                m_img.data <= clamp(s_img.data);
+            for ( int i = 0; i < N; i++) begin
+                if ( enable ) begin
+                    m_img_data[i] <= clamp(s_img_data[i]);
+                end
+                else begin
+                    m_img_data[i] <= m_data_t'(s_img_data[i]);
+                end
             end
         end
     end
