e00e56ae95d79e3405b69923e1c080c63aff3c91
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2024-05-17 17:41:57 +0900

    update

--- rtl/v3/primitive/jelly3_carry_chain.sv
+++ rtl/v3/primitive/jelly3_carry_chain.sv
@@ -14,7 +14,7 @@
 
 module jelly3_carry_chain
         #(
-            parameter   int     DATA_BITS  = 7                       ,
+            parameter   int     DATA_BITS  = 8                       ,
             parameter   type    data_t     = logic [DATA_BITS-1:0]   ,
             parameter           DEVICE     = "RTL"                   ,
             parameter           SIMULATION = "false"                 ,
@@ -34,32 +34,38 @@ module jelly3_carry_chain
         );
 
 
-    if ( DATA_BITS > 4 && DATA_BITS <= 8
-            && (string'(DEVICE) == "ULTRASCALE"
-            ||  string'(DEVICE) == "ULTRASCALE_PLUS"
-            ||  string'(DEVICE) == "ULTRASCALE_PLUS_ES1"
-            ||  string'(DEVICE) == "ULTRASCALE_PLUS_ES2" ) ) begin : xilinx
-        logic           carry8_ci;
-        logic   [7:0]   carry8_s;
-        logic   [7:0]   carry8_di;
-        logic   [7:0]   carry8_o;
-        logic   [7:0]   carry8_co;
-        CARRY8
-                #(
-                    .CARRY_TYPE ("SINGLE_CY8") 
-                )
-            u_carry8
-                (
-                    .CO         (carry8_co  ),
-                    .O          (carry8_o   ),
-                    .CI         (carry8_ci  ),
-                    .CI_TOP     (1'b0       ),
-                    .DI         (carry8_di  ),
-                    .S          (carry8_s   )
-                );
-        assign carry8_ci = cin;
-        assign carry8_s  = 8'(sin);
-        assign carry8_di = 8'(din);
+    if (       string'(DEVICE) == "ULTRASCALE"
+            || string'(DEVICE) == "ULTRASCALE_PLUS"
+            || string'(DEVICE) == "ULTRASCALE_PLUS_ES1"
+            || string'(DEVICE) == "ULTRASCALE_PLUS_ES2" ) begin : xilinx
+        
+        localparam  int     CARRY8_N    = $bits(data_t) + 7 / 8;
+        localparam  int     CARRY8_BITS = CARRY8_N * 8;
+
+        logic   [CARRY8_N-1:0]      carry8_ci;
+        logic   [CARRY8_N*8-1:0]    carry8_s;
+        logic   [CARRY8_N*8-1:0]    carry8_di;
+        logic   [CARRY8_N*8-1:0]    carry8_o;
+        logic   [CARRY8_N*8-1:0]    carry8_co;
+        for ( genvar i = 0; i < CARRY8_N; i++ ) begin : carry8
+            CARRY8
+                    #(
+                        .CARRY_TYPE ("SINGLE_CY8") 
+                    )
+                u_carry8
+                    (
+                        .CO         (carry8_co[i*8 +: 8]),
+                        .O          (carry8_o [i*8 +: 8]),
+                        .CI         (carry8_ci[i]       ),
+                        .CI_TOP     (1'b0               ),
+                        .DI         (carry8_di[i*8 +: 8]),
+                        .S          (carry8_s [i*8 +: 8])
+                    );
+            assign carry8_ci[i] = (i == 0 ? cin : carry8_co[i*8 - 1]);
+        end
+
+        assign carry8_s  = CARRY8_BITS'(sin);
+        assign carry8_di = CARRY8_BITS'(din);
         assign dout = data_t'(carry8_o);
         assign cout = carry8_co[$bits(data_t)-1];
     end
