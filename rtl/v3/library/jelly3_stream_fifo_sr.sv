11c21339172c633189c9de18dc25588ecd9cdba5
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2024-06-30 14:12:27 +0900

    update

--- rtl/v3/library/jelly3_stream_fifo_sr.sv
+++ rtl/v3/library/jelly3_stream_fifo_sr.sv
@@ -14,8 +14,9 @@
 module jelly3_stream_fifo_sr
         #(
             parameter   int     PTR_BITS    = 5                     ,
-            parameter   type    size_t      = logic [PTR_BITS:0]    ,
             parameter   int     FIFO_SIZE   = 2 ** PTR_BITS         ,
+            parameter   int     SIZE_BITS   = $clog2(FIFO_SIZE + 1) ,
+            parameter   type    size_t      = logic [SIZE_BITS-1:0] ,
             parameter   int     DATA_BITS   = 8                     ,
             parameter   type    data_t      = logic [DATA_BITS-1:0] ,
             parameter           DEVICE     = "RTL"                  ,
@@ -31,13 +32,13 @@ module jelly3_stream_fifo_sr
             input   var data_t  s_data      ,
             input   var logic   s_valid     ,
             output  var logic   s_ready     ,
-            output  var size_t  s_free_count,
+            output  var size_t  s_free_size ,
 
             // master port
             output  var data_t  m_data      ,
             output  var logic   m_valid     ,
             input   var logic   m_ready     ,
-            output  var size_t  m_data_count
+            output  var size_t  m_data_size 
         );
     
     localparam  type    ptr_t = logic [PTR_BITS-1:0];
@@ -51,35 +52,38 @@ module jelly3_stream_fifo_sr
         next_ready = 1'b0       ;
         if ( m_valid && m_ready ) begin
             if ( next_ptr == '0 ) begin
-                next_ptr   = 'x;
+                next_ptr   = '0;
                 next_valid = 1'b0;
             end
             else begin
-                next_ptr   = reg_ptr - 1;
-                next_valid = 1'b1       ;
+                next_ptr--          ;
+                next_valid = 1'b1   ;
             end
         end
 
         if ( s_valid && s_ready ) begin
             if ( next_valid ) begin
-                next_ptr   = reg_ptr + 1;
-                next_valid = 1'b1       ;
+                next_ptr++          ;
+                next_valid = 1'b1   ;
             end
             else begin
-                next_ptr   = '0         ;
-                next_valid = 1'b1       ;
+                next_ptr   = '0     ;
+                next_valid = 1'b1   ;
             end
         end
 
-        next_ready = (int'(next_ptr) < FIFO_SIZE) || m_ready;
+        next_ready = (int'(next_ptr) < FIFO_SIZE-1) || m_ready;
     end
 
-    assign s_ready = reg_ready;
-    assign m_valid = reg_valid;
+    assign s_ready     = reg_ready;
+    assign s_free_size = size_t'(FIFO_SIZE) - m_data_size;
+
+    assign m_valid     = reg_valid;
+    assign m_data_size = size_t'(reg_ptr) + size_t'(reg_valid);
 
     always_ff @(posedge clk) begin
         if ( reset ) begin
-            reg_ptr   <= 'x;
+            reg_ptr   <= '0;
             reg_valid <= 1'b0;
             reg_ready <= 1'b0;
         end
