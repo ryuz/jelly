a2a716f102d8ab2e1b38c78da081dbff48a5dffc
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2025-03-02 18:00:54 +0900

    create kv260_imx219_of_measuring

--- rtl/v3/video/jelly3_video_frame_histry_mem_core.sv
+++ rtl/v3/video/jelly3_video_frame_histry_mem_core.sv
@@ -28,38 +28,35 @@ module jelly3_video_frame_histry_mem_core
 
     localparam  int     USER_BITS    = 1                        ;
     localparam  type    user_t       = logic    [USER_BITS-1:0] ;
+    localparam  int     FLAG_BITS    = 1                        ;
+    localparam  type    flag_t       = logic    [FLAG_BITS-1:0] ;
     localparam  int     DATA_BITS    = s_axi4s.DATA_BITS        ;
     localparam  type    data_t       = logic    [DATA_BITS-1:0] ;
 
-    typedef struct packed {
-        user_t  user    ;
-        logic   last    ;
-        data_t  data    ;
-    } packet_t;
 
     logic                   cke;
     assign cke = s_axi4s.tready && s_axi4s.aclken;
 
 
-    logic                   buf_s_first ;
-    logic                   buf_s_last  ;
-    logic                   buf_s_user  ;
-    packet_t                buf_s_data  ;
-    logic                   buf_s_valid ;
+    logic                   hist_s_first ;
+    user_t                  hist_s_user  ;
+    flag_t                  hist_s_flag  ;
+    data_t                  hist_s_data  ;
+    logic                   hist_s_valid ;
 
-    logic                   buf_m_first ;
-    logic                   buf_m_last  ;
-    logic       [N-1:0]     buf_m_user  ;
-    packet_t    [N-1:0]     buf_m_data  ;
-    logic       [N-1:0]     buf_m_valid ;
+    logic                   hist_m_first ;
+    logic                   hist_m_user  ;
+    logic       [N-1:0]     hist_m_flag  ;
+    data_t      [N-1:0]     hist_m_data  ;
+    logic       [N-1:0]     hist_m_valid ;
 
     jelly3_histry_buffer_mem
             #(
                 .N              (N                  ),
                 .USER_BITS      (USER_BITS          ),
                 .user_t         (user_t             ),
-                .DATA_BITS      ($bits(packet_t)    ),
-                .data_t         (packet_t           ),
+                .DATA_BITS      (DATA_BITS          ),
+                .data_t         (data_t             ),
                 .BUF_SIZE       (BUF_SIZE           ),
                 .SDP            (SDP                ),
                 .RAM_TYPE       (RAM_TYPE           ),
@@ -71,32 +68,32 @@ module jelly3_video_frame_histry_mem_core
                 .clk            (s_axi4s.aclk       ),
                 .cke            (cke                ),
 
-                .s_first        (buf_s_first        ),
-                .s_last         (buf_s_last         ),
-                .s_user         (buf_s_user         ),
-                .s_data         (buf_s_data         ),
-                .s_valid        (buf_s_valid        ),
+                .s_first        (hist_s_first       ),
+                .s_user         (hist_s_user        ),
+                .s_flag         (hist_s_flag        ),
+                .s_data         (hist_s_data        ),
+                .s_valid        (hist_s_valid       ),
 
-                .m_first        (buf_m_first        ),
-                .m_last         (buf_m_last         ),
-                .m_user         (buf_m_user         ),
-                .m_data         (buf_m_data         ),
-                .m_valid        (buf_m_valid        )
+                .m_first        (hist_m_first       ),
+                .m_user         (hist_m_user        ),
+                .m_flag         (hist_m_flag        ),
+                .m_data         (hist_m_data        ),
+                .m_valid        (hist_m_valid       )
             );
 
-    assign buf_s_first     = s_axi4s.tuser[0]   ;
-    assign buf_s_last      = s_axi4s.tlast      ;
-    assign buf_s_user      = 1'bx               ;
-    assign buf_s_data      = s_axi4s.tdata      ;
-    assign buf_s_valid     = s_axi4s.tvalid     ;
+    assign hist_s_first    = s_axi4s.tuser[0]   ;
+    assign hist_s_user     = s_axi4s.tlast      ;
+    assign hist_s_flag     = 1'bx               ;
+    assign hist_s_data     = s_axi4s.tdata      ;
+    assign hist_s_valid    = s_axi4s.tvalid     ;
 
     assign s_axi4s.tready  = !m_axi4s.tvalid || m_axi4s.tready;
 
-    assign m_axi4s.tuser   = buf_m_first    ;
-    assign m_axi4s.tlast   = buf_m_last     ;
-    assign m_axi4s.tdata   = buf_m_data     ;
-    assign m_axi4s.tvalid  = buf_m_valid    ;
-
+    assign m_axi4s.tuser   = hist_m_first    ;
+    assign m_axi4s.tlast   = hist_m_user     ;
+    assign m_axi4s.tdata   = hist_m_data     ;
+    assign m_axi4s.tvalid  = hist_m_valid    ;
+    
 endmodule
 
 `default_nettype wire
