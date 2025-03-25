057c7647611a967e73bc3f6f10db7d6529a9f360
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2025-03-25 21:50:16 +0900

    update sim

--- projects/kv260/kv260_blinking_led_ps/sim/tb_top/tb_main.sv
+++ projects/kv260/kv260_blinking_led_ps/sim/tb_top/tb_main.sv
@@ -11,8 +11,8 @@ module tb_main
             input   var logic   [2:0]   s_axi4l_awprot  ,
             input   var logic           s_axi4l_awvalid ,
             output  var logic           s_axi4l_awready ,
-            input   var logic   [127:0] s_axi4l_wdata   ,
-            input   var logic   [15:0]  s_axi4l_wstrb   ,
+            input   var logic   [63:0]  s_axi4l_wdata   ,
+            input   var logic   [7:0]   s_axi4l_wstrb   ,
             input   var logic           s_axi4l_wvalid  ,
             output  var logic           s_axi4l_wready  ,
             output  var logic   [1:0]   s_axi4l_bresp   ,
@@ -22,7 +22,7 @@ module tb_main
             input   var logic   [2:0]   s_axi4l_arprot  ,
             input   var logic           s_axi4l_arvalid ,
             output  var logic           s_axi4l_arready ,
-            output  var logic   [127:0] s_axi4l_rdata   ,
+            output  var logic   [63:0]  s_axi4l_rdata   ,
             output  var logic   [1:0]   s_axi4l_rresp   ,
             output  var logic           s_axi4l_rvalid  ,
             input   var logic           s_axi4l_rready  
@@ -41,28 +41,28 @@ module tb_main
             );
     
     // force の仕様の異なる verilator の為に always_comb を用いる
-    always_comb force u_top.u_design1.aresetn  = s_axi4l_aresetn ;
-    always_comb force u_top.u_design1.aclk     = s_axi4l_aclk    ;
-    always_comb force u_top.u_design1.awaddr   = s_axi4l_awaddr  ;
-    always_comb force u_top.u_design1.awprot   = s_axi4l_awprot  ;
-    always_comb force u_top.u_design1.awvalid  = s_axi4l_awvalid ;
-    always_comb force u_top.u_design1.wdata    = s_axi4l_wdata   ;
-    always_comb force u_top.u_design1.wstrb    = s_axi4l_wstrb   ;
-    always_comb force u_top.u_design1.wvalid   = s_axi4l_wvalid  ;
-    always_comb force u_top.u_design1.bready   = s_axi4l_bready  ;
-    always_comb force u_top.u_design1.araddr   = s_axi4l_araddr  ;
-    always_comb force u_top.u_design1.arprot   = s_axi4l_arprot  ;
-    always_comb force u_top.u_design1.arvalid  = s_axi4l_arvalid ;
-    always_comb force u_top.u_design1.rready   = s_axi4l_rready  ;
+    always_comb force u_top.u_design1.aresetn  = s_axi4l_aresetn    ;
+    always_comb force u_top.u_design1.aclk     = s_axi4l_aclk       ;
+    always_comb force u_top.u_design1.awaddr   = s_axi4l_awaddr     ;
+    always_comb force u_top.u_design1.awprot   = s_axi4l_awprot     ;
+    always_comb force u_top.u_design1.awvalid  = s_axi4l_awvalid    ;
+    always_comb force u_top.u_design1.wdata    = 128'(s_axi4l_wdata);
+    always_comb force u_top.u_design1.wstrb    = 16'(s_axi4l_wstrb) ;
+    always_comb force u_top.u_design1.wvalid   = s_axi4l_wvalid     ;
+    always_comb force u_top.u_design1.bready   = s_axi4l_bready     ;
+    always_comb force u_top.u_design1.araddr   = s_axi4l_araddr     ;
+    always_comb force u_top.u_design1.arprot   = s_axi4l_arprot     ;
+    always_comb force u_top.u_design1.arvalid  = s_axi4l_arvalid    ;
+    always_comb force u_top.u_design1.rready   = s_axi4l_rready     ;
 
-    assign s_axi4l_awready = u_top.u_design1.awready;
-    assign s_axi4l_wready  = u_top.u_design1.wready ;
-    assign s_axi4l_bresp   = u_top.u_design1.bresp  ;
-    assign s_axi4l_bvalid  = u_top.u_design1.bvalid ;
-    assign s_axi4l_arready = u_top.u_design1.arready;
-    assign s_axi4l_rdata   = u_top.u_design1.rdata  ;
-    assign s_axi4l_rresp   = u_top.u_design1.rresp  ;
-    assign s_axi4l_rvalid  = u_top.u_design1.rvalid ;
+    assign s_axi4l_awready = u_top.u_design1.awready    ;
+    assign s_axi4l_wready  = u_top.u_design1.wready     ;
+    assign s_axi4l_bresp   = u_top.u_design1.bresp      ;
+    assign s_axi4l_bvalid  = u_top.u_design1.bvalid     ;
+    assign s_axi4l_arready = u_top.u_design1.arready    ;
+    assign s_axi4l_rdata   = 64'(u_top.u_design1.rdata) ;
+    assign s_axi4l_rresp   = u_top.u_design1.rresp      ;
+    assign s_axi4l_rvalid  = u_top.u_design1.rvalid     ;
 
 
 endmodule
--- projects/kv260/kv260_blinking_led_ps/sim/tb_top/verilator_cpp/tb_verilator.cpp
+++ projects/kv260/kv260_blinking_led_ps/sim/tb_top/verilator_cpp/tb_verilator.cpp
@@ -1,16 +1,11 @@
 #include <memory>
 #include <verilated.h>
-//#include <opencv2/opencv.hpp>
 #include "Vtb_main.h"
 #include "jelly/simulator/Manager.h"
 #include "jelly/simulator/ResetNode.h"
 #include "jelly/simulator/ClockNode.h"
 #include "jelly/simulator/VerilatorNode.h"
-#include "jelly/simulator/WishboneMasterNode.h"
-//#include "jelly/simulator/Axi4sImageLoadNode.h"
-//#include "jelly/simulator/Axi4sImageDumpNode.h"
-#include "jelly/JellyRegs.h"
-
+#include "jelly/simulator/Axi4LiteMasterNode.h"
 
 namespace jsim = jelly::simulator;
 
@@ -44,9 +39,50 @@ int main(int argc, char** argv)
 
     mng->AddNode(jsim::VerilatorNode_Create(top, tfp));
 
-    mng->AddNode(jsim::ClockNode_Create(&top->clk25, 1000.0/25.0));   // 25MHz
+    mng->AddNode(jsim::ResetNode_Create(&top->s_axi4l_aresetn, 100, false));
+    mng->AddNode(jsim::ClockNode_Create(&top->s_axi4l_aclk, 1000.0/100.0));   // 100MHz
 
-    mng->Run(100000);
+    jsim::Axi4Lite axi4lite_signals =
+            {
+                &top->s_axi4l_aresetn   ,
+                &top->s_axi4l_aclk      ,
+                &top->s_axi4l_awaddr    ,
+                &top->s_axi4l_awprot    ,
+                &top->s_axi4l_awvalid   ,
+                &top->s_axi4l_awready   ,
+                &top->s_axi4l_wdata     ,
+                &top->s_axi4l_wstrb     ,
+                &top->s_axi4l_wvalid    ,
+                &top->s_axi4l_wready    ,
+                &top->s_axi4l_bresp     ,
+                &top->s_axi4l_bvalid    ,
+                &top->s_axi4l_bready    ,
+                &top->s_axi4l_araddr    ,
+                &top->s_axi4l_arprot    ,
+                &top->s_axi4l_arvalid   ,
+                &top->s_axi4l_arready   ,
+                &top->s_axi4l_rdata     ,
+                &top->s_axi4l_rresp     ,
+                &top->s_axi4l_rvalid    ,
+                &top->s_axi4l_rready    ,
+            };
+    auto axi4l = jsim::Axi4LiteMasterNode_Create(axi4lite_signals);
+    mng->AddNode(axi4l);
+
+    // リセット解除待ち
+    mng->Run(10000);
+
+    for ( int i = 0; i < 10; i++ ) {
+        // LED を ON
+        axi4l->ExecWrite(0, 1, 0xff); // 1を書く
+        axi4l->ExecRead(0);           // 読み出す
+        mng->Run(10000);
+
+        // LED を OFF
+        axi4l->ExecWrite(0, 0, 0xff); // 0を書く
+        axi4l->ExecRead(0);           // 読み出す
+        mng->Run(10000);
+    }
 
 
 #if VM_TRACE
