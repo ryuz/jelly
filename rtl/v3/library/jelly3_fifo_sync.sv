1fa87722d26fa7e869851940e668595937891ae6
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2025-08-02 11:10:27 +0900

    Add synchronous FIFO and stream FIFO modules with testbenches

--- rtl/v3/library/jelly3_fifo_sync.sv
+++ rtl/v3/library/jelly3_fifo_sync.sv
@@ -11,8 +11,8 @@
 `default_nettype none
 
 
-// asyncronous FIFO
-module jelly3_fifo_async
+// syncronous FIFO
+module jelly3_fifo_sync
         #(
             parameter   int     PTR_BITS   = 5                      ,
             localparam  int     FIFO_SIZE  = 2 ** PTR_BITS          ,
@@ -20,8 +20,6 @@ module jelly3_fifo_async
             parameter   type    size_t     = logic [SIZE_BITS-1:0]  ,
             parameter   int     DATA_BITS  = 8                      ,
             parameter   type    data_t     = logic [DATA_BITS-1:0]  ,
-            parameter   int     WR_SYNC_FF = 2                      ,
-            parameter   int     RD_SYNC_FF = 2                      ,
             parameter           RAM_TYPE   = "block"                ,
             parameter   bit     DOUT_REG   = 1'b0                   ,
             parameter           DEVICE     = "RTL"                  ,
@@ -29,17 +27,15 @@ module jelly3_fifo_async
             parameter           DEBUG      = "false"                
         )
         (
-            input   var logic       wr_reset        ,
-            input   var logic       wr_clk          ,
-            input   var logic       wr_cke          ,
+            input   var logic       reset           ,
+            input   var logic       clk             ,
+            input   var logic       cke             ,
+
             input   var logic       wr_en           ,
             input   var data_t      wr_data         ,
             output  var logic       wr_full         ,
             output  var size_t      wr_free_size    ,
             
-            input   var logic       rd_reset        ,
-            input   var logic       rd_clk          ,
-            input   var logic       rd_cke          ,
             input   var logic       rd_en           ,
             input   var logic       rd_regcke       ,
             output  var data_t      rd_data         ,
@@ -86,133 +82,90 @@ module jelly3_fifo_async
             )
         u_ram_simple_dualport
             (
-                .wr_clk      (wr_clk            ),
-                .wr_en       (ram_wr_en & wr_cke),
+                .wr_clk      (clk               ),
+                .wr_en       (ram_wr_en & cke   ),
                 .wr_addr     (ram_wr_addr       ),
                 .wr_din      (ram_wr_data       ),
 
-                .rd_clk      (rd_clk            ),
-                .rd_en       (ram_rd_en & rd_cke),
-                .rd_regcke   (rd_regcke & rd_cke),
+                .rd_clk      (clk               ),
+                .rd_en       (ram_rd_en & cke   ),
+                .rd_regcke   (rd_regcke & cke   ),
                 .rd_addr     (ram_rd_addr       ),
                 .rd_dout     (ram_rd_data       )
             );
     
-
-    
-    // ---------------------------------
-    //  CDC FIFO pointer
-    // ---------------------------------
-    
-    // write
-    ptr_t       wr_wptr;
-    ptr_t       wr_rptr;
-
-    // read
-    ptr_t       rd_wptr;
-    ptr_t       rd_rptr;
-
-    jelly3_cdc_gray
-            #(
-                .DEST_SYNC_FF       (WR_SYNC_FF     ),
-                .WIDTH              ($bits(ptr_t)   ),
-                .DEVICE             (DEVICE         ),
-                .SIMULATION         (SIMULATION     ),
-                .DEBUG              (DEBUG          )
-            )
-        jelly3_cdc_gray_w
-            (
-                .src_clk            (wr_clk         ),
-                .src_in_bin         (wr_wptr        ),
-                .dest_clk           (rd_clk         ),
-                .dest_out_bin       (rd_wptr        )
-            );
-
-    jelly3_cdc_gray
-            #(
-                .DEST_SYNC_FF       (RD_SYNC_FF     ),
-                .WIDTH              ($bits(ptr_t)   ),
-                .DEVICE             (DEVICE         ),
-                .SIMULATION         (SIMULATION     ),
-                .DEBUG              (DEBUG          )
-            )
-        jelly3_cdc_gray_r
-            (
-                .src_clk            (rd_clk         ),
-                .src_in_bin         (rd_rptr        ),
-                .dest_clk           (wr_clk         ),
-                .dest_out_bin       (wr_rptr        )
-            );
-
     
     // ---------------------------------
     //  Control
     // ---------------------------------
-    
+
+    // pointer
+    ptr_t   wr_ptr              ;
+    ptr_t   rd_ptr              ;
+
     // write side
-    ptr_t   next_wr_wptr        ;
+    ptr_t   next_wr_ptr         ;
     logic   next_wr_full        ;
     size_t  next_wr_free_size   ;
     always_comb begin
-        next_wr_wptr      = wr_wptr     ;
+        next_wr_ptr       = wr_ptr     ;
         next_wr_full      = wr_full     ;
         next_wr_free_size = wr_free_size;
         if ( ram_wr_en ) begin
-            next_wr_wptr = wr_wptr + 1'b1;
+            next_wr_ptr = wr_ptr + 1'b1;
         end
-        next_wr_full      = (next_wr_wptr[PTR_BITS] != wr_rptr[PTR_BITS]) && (next_wr_wptr[PTR_BITS-1:0] == wr_rptr[PTR_BITS-1:0]);
-        next_wr_free_size = ((wr_rptr - next_wr_wptr) + size_t'(FIFO_SIZE));
+        next_wr_full      = (next_wr_ptr[PTR_BITS] != rd_ptr[PTR_BITS]) && (next_wr_ptr[PTR_BITS-1:0] == rd_ptr[PTR_BITS-1:0]);
+        next_wr_free_size = ((rd_ptr - next_wr_ptr) + size_t'(FIFO_SIZE));
     end
     
-    always_ff @(posedge wr_clk) begin
-        if ( wr_reset ) begin
-            wr_wptr      <= 0   ;
+    always_ff @(posedge clk) begin
+        if ( reset ) begin
+            wr_ptr       <= 0   ;
             wr_full      <= 1'b1;   // リセット期間中は書き込みできないのでfullとする
             wr_free_size <= 0   ;
         end
-        else if ( wr_cke ) begin
-            wr_wptr      <= next_wr_wptr        ;
+        else if ( cke ) begin
+            wr_ptr       <= next_wr_ptr         ;
             wr_full      <= next_wr_full        ;
             wr_free_size <= next_wr_free_size   ;
         end
     end
     
     assign ram_wr_en   = wr_en & ~wr_full       ;
-    assign ram_wr_addr = wr_wptr[ADDR_BITS-1:0] ;
+    assign ram_wr_addr = wr_ptr[ADDR_BITS-1:0]  ;
     assign ram_wr_data = wr_data                ;
     
     
-    
     // read side
-    ptr_t   next_rd_rptr        ;
+    ptr_t   next_rd_ptr         ;
     logic   next_rd_empty       ;
     size_t  next_rd_data_size   ;
     always_comb begin
-        next_rd_rptr      = rd_rptr     ;
+        next_rd_ptr       = rd_ptr      ;
         next_rd_empty     = rd_empty    ;
         next_rd_data_size = rd_data_size;
         if ( ram_rd_en ) begin
-            next_rd_rptr = rd_rptr + 1'b1;
+            next_rd_ptr = rd_ptr + 1'b1;
         end
-        next_rd_empty     = (rd_wptr == next_rd_rptr);
-        next_rd_data_size = (rd_wptr - next_rd_rptr);
+        next_rd_empty     = (wr_ptr == next_rd_ptr) ;
+        next_rd_data_size = (wr_ptr - next_rd_ptr)  ;
     end
     
-    always_ff @ ( posedge rd_clk ) begin
-        if ( rd_reset ) begin
-            rd_rptr      <= 0   ;
+    always_ff @ ( posedge clk ) begin
+        if ( reset ) begin
+            rd_ptr       <= 0   ;
             rd_empty     <= 1'b1;
             rd_data_size <= 0   ;
         end
-        else if ( rd_cke ) begin
-            rd_rptr      <= next_rd_rptr        ;
+        else if ( cke ) begin
+            rd_ptr       <= next_rd_ptr         ;
             rd_empty     <= next_rd_empty       ;
             rd_data_size <= next_rd_data_size   ;
         end
     end
     
     assign ram_rd_en    = rd_en & ~rd_empty     ;
-    assign ram_rd_addr  = rd_rptr[ADDR_BITS-1:0];
+    assign ram_rd_addr  = rd_ptr[ADDR_BITS-1:0] ;
     assign rd_data      = ram_rd_data           ;
     
 endmodule
--- testbench/v3/library/tb_stream_fifo_sync/tb_main.sv
+++ testbench/v3/library/tb_stream_fifo_sync/tb_main.sv
@@ -6,8 +6,7 @@
 module tb_main
         (
             input   var logic   reset   ,
-            input   var logic   clk0    ,
-            input   var logic   clk1
+            input   var logic   clk     
         );
 
 
@@ -17,17 +16,13 @@ module tb_main
     parameter   type    size_t       = logic [SIZE_BITS-1:0];
     parameter   int     DATA_BITS    = 8                    ;
     parameter   type    data_t       = logic [DATA_BITS-1:0];
-    parameter   int     S_SYNC_FF    = 2                    ;
-    parameter   int     M_SYNC_FF    = 2                    ;
     parameter           RAM_TYPE     = "block"              ;
     parameter   bit     DOUT_REG     = 1'b0                 ;
     parameter           DEVICE       = "RTL"                ;
     parameter           SIMULATION   = "false"              ;
     parameter           DEBUG        = "false"              ;
 
-    logic   s_reset     ;
-    logic   s_clk       ;
-    logic   s_cke       ;
+    logic   cke         ;
     data_t  s_data      ;
     logic   s_valid     ;
     logic   s_ready     ;
@@ -39,62 +34,51 @@ module tb_main
     logic   m_valid     ;
     logic   m_ready     ;
     size_t  m_data_size ;
-    
-    jelly3_stream_fifo_async
+
+    always_ff @(posedge clk) begin
+        cke <= 1'({$random});
+    end
+
+    jelly3_stream_fifo_sync
             #(
                 .PTR_BITS       (PTR_BITS       ),
                 .SIZE_BITS      (SIZE_BITS      ),
                 .size_t         (size_t         ),
                 .DATA_BITS      (DATA_BITS      ),
                 .data_t         (data_t         ),
-                .S_SYNC_FF      (S_SYNC_FF      ),
-                .M_SYNC_FF      (M_SYNC_FF      ),
                 .RAM_TYPE       (RAM_TYPE       ),
                 .DOUT_REG       (DOUT_REG       ),
                 .DEVICE         (DEVICE         ),
                 .SIMULATION     (SIMULATION     ),
                 .DEBUG          (DEBUG          )
             )
-        u_stream_fifo_async
+        u_stream_fifo_sync
             (
-                .s_reset        (s_reset        ),
-                .s_clk          (s_clk          ),
-                .s_cke          (s_cke          ),
+                .reset          (reset          ),
+                .clk            (clk            ),
+                .cke            (cke            ),
+
                 .s_data         (s_data         ),
                 .s_valid        (s_valid        ),
                 .s_ready        (s_ready        ),
                 .s_free_size    (s_free_size    ),
-                .m_reset        (m_reset        ),
-                .m_clk          (m_clk          ),
-                .m_cke          (m_cke          ),
+
                 .m_data         (m_data         ),
                 .m_valid        (m_valid        ),
                 .m_ready        (m_ready        ),
                 .m_data_size    (m_data_size    )
             );
 
-    assign s_reset = reset ;
-    assign s_clk   = clk0  ;
-    always_ff @(posedge s_clk) begin
-        s_cke <= 1'({$random});
-    end
-
-    assign m_reset = reset ;
-    assign m_clk   = clk1  ;
-    always_ff @(posedge m_clk) begin
-        m_cke <= 1'({$random});
-    end
-
 
     // write
     data_t      reg_data;
     logic       reg_valid;
-    always_ff @(posedge s_clk) begin
-        if ( s_reset ) begin
+    always_ff @(posedge clk) begin
+        if ( reset ) begin
             reg_data  <= 0;
             reg_valid <= 1'b0;
         end
-        else if ( s_cke ) begin
+        else if ( cke ) begin
             if ( !(s_valid && !s_ready) ) begin
                 reg_valid <= 1'($random);
             end
@@ -117,12 +101,12 @@ module tb_main
     int         count_ok = 0;
     data_t      reg_expectation_value;
     logic       reg_ready;
-    always_ff @(posedge m_clk) begin
-        if ( m_reset ) begin
+    always_ff @(posedge clk) begin
+        if ( reset ) begin
             reg_expectation_value  <= 0;
             reg_ready              <= 1'b0;
         end
-        else if ( m_cke ) begin
+        else if ( cke ) begin
             reg_ready <= 1'($random);
             
             if ( m_valid && m_ready ) begin
@@ -145,16 +129,16 @@ module tb_main
     end
     assign m_ready = reg_ready;
 
-    always_ff @(posedge s_clk) begin
-        if ( !s_reset && s_cke ) begin
+    always_ff @(posedge clk) begin
+        if ( !reset && cke ) begin
             if ( (s_ready && s_free_size == 0) || (!s_ready && s_free_size != 0) ) begin
                 $display("%t error : s_ready = %d  s_free_size = %d", $time(), s_ready, s_free_size);
             end
         end
     end
 
-    always_ff @(posedge m_clk) begin
-        if ( !m_reset && m_cke) begin
+    always_ff @(posedge clk) begin
+        if ( !reset && cke  ) begin
             if ( (m_valid && m_data_size == 0) || (!m_valid && m_data_size != 0)  ) begin
                 $display("%t error : m_valid = %d  m_data_size = %d", $time(), m_valid, m_data_size);
             end
