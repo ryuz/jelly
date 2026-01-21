6af6c884c283ce39e69d7dc949b022223862db37
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2026-01-21 09:40:30 +0900

    Enhance Makefile and scripts for improved build process; update bin2hex.py for flexible input/output, add clean target, and modify linker script for memory alignment; refactor tb_main and tb_top for better logging and simulation control.

--- testbench/v3/jfive/tb_jfive_core/rust_app/bin2hex.py
+++ testbench/v3/jfive/tb_jfive_core/rust_app/bin2hex.py
@@ -1,7 +1,18 @@
+#!/usr/bin/env python3
+
+import argparse
 import numpy as np
 
-data = np.fromfile("rust_app.bin", dtype=np.uint32)
+parser = argparse.ArgumentParser(description='Convert binary file to hex format.')
+parser.add_argument('input_file', help='Input binary file')
+parser.add_argument('output_file', help='Output hex file')
+parser.add_argument('size', type=lambda x: int(x, 0), help='Size to pad the data to (decimal or hex)')
 
-with open("../mem.hex", "w") as f:
+args = parser.parse_args()
+
+data = np.fromfile(args.input_file, dtype=np.uint32)
+data = np.pad(data, (0, args.size - len(data)), 'constant')
+
+with open(args.output_file, "w") as f:
     for v in data:
-        f.write("%08x\n"%(v))
+        f.write(f"{v:08x}\n")
--- testbench/v3/jfive/tb_jfive_core/rust_app/src/main.rs
+++ testbench/v3/jfive/tb_jfive_core/rust_app/src/main.rs
@@ -5,21 +5,25 @@
 use core::panic::PanicInfo;
 
 
-#[panic_handler]
-fn panic(_panic: &PanicInfo<'_>) -> ! {
-    loop {}
-}
-
-
 unsafe extern "C" {
     fn asm_test() -> i32;
+    fn asm_panic() -> !;
 }
+
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    unsafe { asm_panic(); }
+//  loop {}
+}
+
+
 
 //static mut DATA : i32 = 0;
 
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn main() -> ! {
+pub unsafe extern "C" fn main() {
     println!("Start!");
     
     /*
@@ -100,7 +104,7 @@ pub unsafe extern "C" fn main() -> ! {
         a *= 1.1;
     }
 
-    loop {}
+//  loop {}
 }
 
 
@@ -108,7 +112,7 @@ pub unsafe extern "C" fn main() -> ! {
 
 
 fn write_byte(c: u8) {
-    let mmio_putc = 0x10000000  as *mut u8;
+    let mmio_putc = 0x80000000  as *mut u8;
     unsafe {
         core::ptr::write_volatile(mmio_putc, c);
     }
